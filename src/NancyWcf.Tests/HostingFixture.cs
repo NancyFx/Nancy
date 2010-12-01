5addfe1ce89f57375ad2c9a3caf7138b5196050f
Pedro Felix <pmhsfelix@gmail.com>
2010-12-01 12:00:31 +0000

    added the files that were forgeted in the last commit

---
+++ src/NancyWcf.Tests/HostingFixture.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using Nancy;
+using Xunit;
+using Xunit.Extensions;
+
+namespace NancyWcf.Tests
+{
+    public class HostingFixture
+    {
+        public class TestModule : NancyModule
+        {
+           
+            public TestModule()
+            {
+                Get["/rel"] = parameters =>
+                {
+                    return "This is the site route";
+                };
+            }
+        }
+        [Fact]
+        public void Should_be_able_to_get_from_selfhost()
+        {
+            using (var host = new WebServiceHost(new NancyWcfGenericService(GetType().Assembly), new Uri("http://localhost/base/")))
+            {
+                host.AddServiceEndpoint(typeof (NancyWcfGenericService), new WebHttpBinding(), "");
+                host.Open();
+
+                var resp = new StreamReader(
+                        WebRequest.Create("http://localhost/base/rel").GetResponse().GetResponseStream()
+                    ).ReadToEnd();
+
+                Assert.Equal("This is the site route", resp);
+            }
+        }
+    }
+}
---
+++ src/NancyWcf/NancyWcfGenericService.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Web;
+using System.Text;
+using Nancy;
+using Nancy.Routing;
+
+namespace NancyWcf
+{
+    [ServiceContract]
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
+    public class NancyWcfGenericService
+    {
+
+        private readonly NancyEngine _engine;
+
+        public NancyWcfGenericService(Assembly modulesAssembly)
+        {
+            _engine = new NancyEngine(new NancyModuleLocator(modulesAssembly), new RouteResolver());
+        }
+
+        [WebInvoke(UriTemplate="*")]
+        public Message HandleOther()
+        {
+            return HandleAll();
+        }
+
+        [WebGet(UriTemplate="*")]
+        public Message HandleGet()
+        {
+            return HandleAll();
+        }
+
+        private Message HandleAll()
+        {
+            var ctx = WebOperationContext.Current;
+            
+            var request = CreateNancyRequestFromIncomingRequest(ctx.IncomingRequest);
+            var response = _engine.HandleRequest(request);
+            SetNancyResponseToOutgoingResponse(ctx.OutgoingResponse, response);
+            return ctx.CreateTextResponse(response.Contents);
+        }
+
+        private static IRequest CreateNancyRequestFromIncomingRequest(IncomingWebRequestContext req)
+        {
+            var ruri = req.UriTemplateMatch.BaseUri.MakeRelativeUri(req.UriTemplateMatch.RequestUri);
+            return new Request(
+                req.Method,
+                "/"+ruri.ToString());
+        }
+
+        private static void SetNancyResponseToOutgoingResponse(OutgoingWebResponseContext resp, Response response)
+        {
+            resp.ContentType = response.ContentType;
+            resp.StatusCode = response.StatusCode;
+        }
+    }
+}
