5755bf02943bdb8ed5eb15b1a8c7ee5de4c9ac2b
Joao Braganca <joao.paul@braganca.name>
2010-12-03 22:12:28 -0800

    Adding support for dynamic casting of parameters

--- src/Nancy.Tests/Unit/Routing/RouteParametersFixture.cs
+++ src/Nancy.Tests/Unit/Routing/RouteParametersFixture.cs
@@ -1,4 +1,6 @@
-﻿namespace Nancy.Tests.Unit.Routing
+﻿using System;
+
+namespace Nancy.Tests.Unit.Routing
 {
     using Nancy.Routing;
     using Xunit;
@@ -18,5 +20,80 @@
             // Then
             value.ShouldEqual(10);
         }
-    }
+		[Fact]
+		public void Should_support_dynamic_casting_of_properties_to_ints()
+		{
+			//Given
+			dynamic parameters = new RouteParameters();
+			parameters.test = "10";
+
+			// When
+			var value = (int)parameters.test;
+
+			// Then
+			value.ShouldEqual(10);
+		}
+
+		[Fact]
+		public void Should_support_dynamic_casting_of_properties_to_guids()
+		{
+			//Given
+			dynamic parameters = new RouteParameters();
+			var guid = Guid.NewGuid();
+			parameters.test = guid.ToString();
+
+			// When
+			var value = (Guid)parameters.test;
+
+			// Then
+			value.ShouldEqual(guid);
+		}
+
+
+		[Fact]
+		public void Should_support_dynamic_casting_of_properties_to_timespans()
+		{
+			//Given
+			dynamic parameters = new RouteParameters();
+			parameters.test = new TimeSpan(1, 2, 3, 4).ToString();
+
+			// When
+			var value = (TimeSpan)parameters.test;
+
+			// Then
+			value.ShouldEqual(new TimeSpan(1, 2, 3, 4));
+		}
+
+		[Fact]
+		public void Should_support_dynamic_casting_of_properties_to_datetimes()
+		{
+			//Given
+			dynamic parameters = new RouteParameters();
+
+			parameters.test = new DateTime(2001, 3, 4);
+
+			// When
+			var value = (DateTime)parameters.test;
+
+			// Then
+			value.ShouldEqual(new DateTime(2001, 3, 4));
+		}
+
+
+		[Fact]
+		public void Should_support_dynamic_casting_of_nullable_properties()
+		{
+			//Given
+			dynamic parameters = new RouteParameters();
+			var guid = Guid.NewGuid();
+			parameters.test = guid.ToString();
+
+			// When
+			var value = (Guid?)parameters.test;
+
+			// Then
+			value.ShouldEqual(guid);
+		}
+
+	}
 }
--- src/Nancy/Routing/RouteParameters.cs
+++ src/Nancy/Routing/RouteParameters.cs
@@ -1,4 +1,7 @@
-﻿namespace Nancy.Routing
+﻿using System.Linq.Expressions;
+using Microsoft.CSharp.RuntimeBinder;
+
+namespace Nancy.Routing
 {
     using System;
     using System.Collections.Generic;
@@ -6,11 +9,86 @@
 
     public class RouteParameters : DynamicObject, IEquatable<RouteParameters>
     {
+		internal class DynamicRouteParameter : DynamicObject
+		{
+			private readonly object value;
+
+			public DynamicRouteParameter(object value)
+			{
+				this.value = value;
+			}
+
+			public override bool TryBinaryOperation(BinaryOperationBinder binder, object arg, out object result)
+			{
+				object resultOfCast;
+				result = null;
+
+				if (binder.Operation != ExpressionType.Equal)
+					return false;
+
+				var convert = Binder.Convert(CSharpBinderFlags.None, arg.GetType(), typeof(DynamicRouteParameter));
+				if (false == TryConvert((ConvertBinder)convert, out resultOfCast))
+				{
+					return false;
+				}
+
+				result = resultOfCast == null
+							? Equals(arg, resultOfCast)
+							: resultOfCast.Equals(arg);
+				return true;
+			}
+			public override bool TryConvert(ConvertBinder binder, out object result)
+			{
+				result = null;
+				if (value == null)
+					return true;
+				var binderType = binder.Type;
+				if (binderType == typeof(String))
+				{
+					result = Convert.ToString(value);
+					return true;
+				}
+
+				if (binderType == typeof(Guid) || binderType == typeof(Guid?))
+				{
+					Guid guid;
+					if (Guid.TryParse(Convert.ToString(value), out guid))
+					{
+						result = guid;
+						return true;
+					}
+				}
+				else if (binderType == typeof(TimeSpan) || binderType == typeof(TimeSpan?))
+				{
+					TimeSpan timespan;
+					if (TimeSpan.TryParse(Convert.ToString(value), out timespan))
+					{
+						result = timespan;
+						return true;
+					}
+				}
+				else
+				{
+					if (binderType.IsGenericType && binderType.GetGenericTypeDefinition() == typeof(Nullable<>))
+						binderType = binderType.GetGenericArguments()[0];
+					var typeCode = Type.GetTypeCode(binderType);
+					if (typeCode == TypeCode.Object) // something went wrong here
+					{
+						return false;
+					}
+					result = Convert.ChangeType(value, typeCode);
+					return true;
+				}
+				return base.TryConvert(binder, out result);
+			}
+
+		}
+
         private readonly Dictionary<string, object> dictionary = new Dictionary<string, object>();
 
         public override bool TrySetMember(SetMemberBinder binder, object value)
         {
-            dictionary[binder.Name] = value;
+			dictionary[binder.Name] = value is DynamicRouteParameter ? value : new DynamicRouteParameter(value);
             return true;
         }
 
@@ -19,7 +97,7 @@
             return dictionary.TryGetValue(binder.Name, out result);
         }
 
-        public object this[string name]
+        public dynamic this[string name]
         {
             get { return dictionary[name]; }
             set { dictionary[name] = value; }
