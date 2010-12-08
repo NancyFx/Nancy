d00ff8ce583eeb69d601656aa43da23ce0435234
Andreas Hakansson <andreas@selfinflicted.org>
2010-12-08 19:03:11 +0100

    Cleaned up the TestModule class in the WCF host test project

--- src/Nancy.Hosting.Wcf.Tests/TestModule.cs
+++ src/Nancy.Hosting.Wcf.Tests/TestModule.cs
@@ -1,13 +1,18 @@
-﻿using System.IO;
-
-namespace Nancy.Hosting.Wcf.Tests
+﻿namespace Nancy.Hosting.Wcf.Tests
 {
+    using System.IO;
+
     public class TestModule : NancyModule
     {
         public TestModule()
         {
-            Get["/rel"] = parameters => { return "This is the site route"; };
-            Post["/rel"] = parameters => { return new StreamReader(Request.Body).ReadToEnd(); };
+            Get["/rel"] = parameters => {
+                return "This is the site route";
+            };
+
+            Post["/rel"] = parameters => {
+                return new StreamReader(Request.Body).ReadToEnd();
+            };
         }
     }
 }
