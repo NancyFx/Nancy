a200b6267fb167da32589f653f73af446061c72f
Andreas Hakansson <andreas@selfinflicted.org>
2010-12-12 16:32:32 +0100

    Started Spike integration work

---
+++ src/Nancy.ViewEngines.Spark/SparkViewEngineExtensions.cs
@@ -0,0 +1,31 @@
+namespace Nancy.ViewEngines.Spark
+{
+    using System;
+    using System.IO;
+    using System.Web.Hosting;
+
+    using global::Spark;
+    using global::Spark.FileSystem;
+
+    public static class SparkViewEngineExtensions
+    {
+        public static Action<Stream> Razor(this IViewEngine source, string name) 
+        {
+            return source.Razor(name, (object)null);
+        }
+
+        public static Action<Stream> Razor<TModel>(this IViewEngine source, string name, TModel model)
+        {
+            var settings =
+                new SparkSettings().SetPageBaseType(typeof(StubSparkView));
+
+            var engine =
+                new SparkViewEngine(settings) { ViewFolder = new FileSystemViewFolder(HostingEnvironment.MapPath(@"~/Views")) };
+
+            return stream => {
+                var result = viewEngine.RenderView(name, model);
+                result.Execute(stream);
+            };
+        }
+    }
+}
