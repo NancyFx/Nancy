48443228d9a5050828620ab930c21ece42207ca9
Mindaugas Mozuras <mindaugas.mozuras@gmail.com>
2010-12-14 01:38:08 +0200

    Initial implementation of Nancy.ViewEngines.NHaml.

---
+++ src/Nancy.ViewEngines.NHaml/NHamlViewEngineExtensions.cs
@@ -0,0 +1,32 @@
+namespace Nancy.ViewEngines.NHaml
+{
+    using System;
+    using System.IO;
+    using System.Web.Hosting;
+    using global::NHaml;
+
+    public static class HamlViewEngineExtensions
+    {
+        public static Action<Stream> Haml(this IViewEngine source, string name)
+        {
+            return Haml(source, name, (object) null);
+        }
+
+        public static Action<Stream> Haml<TModel>(this IViewEngine source, string name, TModel model)
+        {
+            var templateEngine = new TemplateEngine();
+            var path = HostingEnvironment.MapPath(name);
+
+            return stream =>
+            {
+                CompiledTemplate compiledTemplate = templateEngine.Compile(path, typeof(Template<TModel>));
+
+                var writer = new StreamWriter(stream);
+                var template = (Template<TModel>)compiledTemplate.CreateInstance();
+                template.Model = model;
+                template.Render(writer);
+                writer.Flush();
+            };
+        }
+    }
+}
