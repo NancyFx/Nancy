cc4718cd33e8540a586eba346c1349d0ec3741c2
Andreas Hakansson <andreas@selfinflicted.org>
2010-12-18 23:31:10 +0100

    Cleaned up the DefaultModuleLocator

--- src/Nancy/DefaultModuleActivator.cs
+++ src/Nancy/DefaultModuleActivator.cs
@@ -4,9 +4,14 @@ namespace Nancy
 
     public class DefaultModuleActivator : IModuleActivator
     {
+        /// <summary>
+        /// Creates an instance of the specified <see cref="Type"/>.
+        /// </summary>
+        /// <param name="moduleType">The <see cref="Type"/> of the module to instantiate.</param>
+        /// <returns>A <see cref="NancyModule"/> instance.</returns>
         public virtual NancyModule CreateInstance(Type moduleType)
         {
-            if(! CanCreateInstance(moduleType))
+            if(!CanCreateInstance(moduleType))
             {
                 throw new InvalidOperationException("Cannot create an instance of type {0} as it does not inherit from NancyModule or it does not have a public parameterless constructor.");
             }
@@ -14,10 +19,14 @@ namespace Nancy
             return (NancyModule) Activator.CreateInstance(moduleType);
         }
 
+        /// <summary>
+        /// Checks whether the activator can create an instance of the specified type.
+        /// </summary>
+        /// <param name="moduleType">The <see cref="Type"/> to check.</param>
+        /// <returns><see langword="true"/> if this activator can create an instance of the specified module type, otherwise <see langword="false"/>.</returns>
         public virtual bool CanCreateInstance(Type moduleType)
         {
-            bool hasDefaultConstructor = moduleType.GetConstructor(Type.EmptyTypes) != null;
-            return hasDefaultConstructor;
+            return moduleType.GetConstructor(Type.EmptyTypes) != null;
         }
     }
 }
