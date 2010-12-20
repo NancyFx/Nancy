0c6c14a3ad670784440021c17f975336bfaf8bc3
Andreas Hakansson <andreas@selfinflicted.org>
2010-12-20 22:46:29 +0100

    Fixed tests that were failing due to localization issues + cleaned up test to match style of other tests in the solution

--- src/Nancy.Tests/Unit/NancyCookieFixture.cs
+++ src/Nancy.Tests/Unit/NancyCookieFixture.cs
@@ -1,6 +1,7 @@
 namespace Nancy.Tests.Unit
 {
     using System;
+    using System.Globalization;
     using Cookies;
     using Xunit;
 
@@ -9,33 +10,82 @@ namespace Nancy.Tests.Unit
         [Fact]
         public void Should_stringify_a_simple_name_value()
         {
-            new NancyCookie("leto", "worm").ToString().ShouldEqual("leto=worm");
+            // Given
+            var cookie = new NancyCookie("leto", "worm");
+
+            // When
+            var stringified = cookie.ToString();
+            
+            // Then
+            stringified.ShouldEqual("leto=worm");
         }
 
         [Fact]
         public void Should_stringify_an_expiry_to_gmt_and_stupid_format()
         {
+            // Given
             var date = new DateTime(2015, 10, 8, 9, 10, 11, DateTimeKind.Utc);
-            new NancyCookie("leto", "worm") { Expires = date }.ToString().ShouldEqual("leto=worm; expires=Thu, 08-Oct-2015 09:10:11 GMT");
+            var thursday = GetLocalizedAbbreviatedWeekdayName(date);
+            var october = GetLocalizedAbbreviatedMonthName(date);
+
+            // When
+            var cookie = new NancyCookie("leto", "worm") { Expires = date }.ToString();
+            
+            // Then
+            cookie.ShouldEqual(string.Format("leto=worm; expires={0}, 08-{1}-2015 09:10:11 GMT", thursday, october));
         }
 
         [Fact]
         public void Should_stringify_a_domain()
         {
-            new NancyCookie("leto", "worm") { Domain = "google.com" }.ToString().ShouldEqual("leto=worm; domain=google.com");
+            // Given
+            var cookie = new NancyCookie("leto", "worm") { Domain = "google.com" };
+
+            // When
+            var stringified = cookie.ToString();
+
+            // Then
+            stringified.ShouldEqual("leto=worm; domain=google.com");
         }
 
         [Fact]
         public void Should_stringify_a_path()
         {
-            new NancyCookie("leto", "worm") { Path = "/nancy" }.ToString().ShouldEqual("leto=worm; path=/nancy");
+            // Given
+            var cookie = new NancyCookie("leto", "worm") { Path = "/nancy" };
+
+            // When
+            var stringified = cookie.ToString();
+
+            // Then
+            stringified.ShouldEqual("leto=worm; path=/nancy");
         }
 
         [Fact]
         public void Should_stringify_everyting()
         {
+            // Given
             var date = new DateTime(2016, 11, 8, 9, 10, 11, DateTimeKind.Utc);
-            new NancyCookie("paul", "blind") { Expires = date, Path = "/frank", Domain = "gmail.com" }.ToString().ShouldEqual("paul=blind; expires=Tue, 08-Nov-2016 09:10:11 GMT; domain=gmail.com; path=/frank");
+            var tuesday = GetLocalizedAbbreviatedWeekdayName(date);
+            var november = GetLocalizedAbbreviatedMonthName(date);
+            var cookie = new NancyCookie("paul", "blind") { Expires = date, Path = "/frank", Domain = "gmail.com" };
+                
+            // When
+            var stringified = cookie.ToString();
+                
+            // Then
+            stringified.ShouldEqual(string.Format("paul=blind; expires={0}, 08-{1}-2016 09:10:11 GMT; domain=gmail.com; path=/frank", tuesday, november));
         }
+
+        public static string GetLocalizedAbbreviatedMonthName(DateTime dateTime)
+        {
+            return CultureInfo.CurrentCulture.DateTimeFormat.AbbreviatedMonthNames[dateTime.Month - 1];
+        }
+
+        public static string GetLocalizedAbbreviatedWeekdayName(DateTime dateTime)
+        {
+            return CultureInfo.CurrentCulture.DateTimeFormat.AbbreviatedDayNames[(int)dateTime.DayOfWeek];
+        }
+
     }
 }
