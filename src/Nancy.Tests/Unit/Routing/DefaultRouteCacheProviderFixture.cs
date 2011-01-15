c2d9daa86f17656ca5d3ea5026af7898f2d6935d
Steven Robbins <ste.robbins@gmail.com>
2011-01-15 16:37:34 +0000

    Some additional tests for new classes.
    Not a huge amount of functionality in them, so not many tests :-)

---
+++ src/Nancy.Tests/Unit/Routing/DefaultRouteCacheProviderFixture.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using FakeItEasy;
+
+namespace Nancy.Tests.Unit.Routing
+{
+    public class DefaultRouteCacheProviderFixture
+    {
+        private Nancy.Routing.IRouteCacheProvider _Provider;
+        private Nancy.Routing.IRouteCache _RouteCache;
+
+        /// <summary>
+        /// Initializes a new instance of the DefaultRouteCacheProviderFixture class.
+        /// </summary>
+        public DefaultRouteCacheProviderFixture()
+        {
+            _RouteCache = A.Fake<Nancy.Routing.IRouteCache>();
+            _Provider = new Nancy.Routing.DefaultRouteCacheProvider(() => _RouteCache);
+        }
+
+        [Fact]
+        public void Should_Return_Instance_Supplied_By_Func()
+        {
+            var result = _Provider.GetCache();
+
+            result.ShouldBeSameAs(_RouteCache);
+        }
+    }
+}
