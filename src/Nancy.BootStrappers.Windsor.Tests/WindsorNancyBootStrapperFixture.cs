83f096bd13b66e9da22ccbd9ea05dabb9a8e84fc
chris.nicola <chris.nicola@visioncritical.com>
2011-01-22 14:39:43 -0800

    Added WindsorNancyBootStrapper following in the footsteps of the previous bootstrappers.  All bootstrapper tests are passing.

---
+++ src/Nancy.BootStrappers.Windsor.Tests/WindsorNancyBootStrapperFixture.cs
@@ -0,0 +1,147 @@
+﻿using System.Linq;
+using Castle.MicroKernel.Registration;
+using Castle.Windsor;
+using Nancy.BootStrappers.Windsor;
+using Xunit;
+using Nancy.Routing;
+using Nancy.BootStrapper;
+using Nancy.Tests.Fakes;
+
+namespace Nancy.Tests.Unit
+{
+    public class FakeWindsorNancyBootStrapper : WindsorNancyBootStrapper
+    {
+        public bool RequestContainerConfigured { get; set; }
+
+        public bool ApplicationContainerConfigured { get; set; }
+
+        public IWindsorContainer Container { get { return _container; } }
+
+        public override void ConfigureRequestContainer(IWindsorContainer container)
+        {
+            RequestContainerConfigured = true;
+
+            container.Register(
+                Component.For<IFoo>().ImplementedBy<Foo>(),
+                Component.For<IDependency>().ImplementedBy<Dependency>());
+            base.ConfigureRequestContainer(container);
+        }
+
+        protected override void ConfigureApplicationContainer(IWindsorContainer container)
+        {
+            ApplicationContainerConfigured = true;
+            base.ConfigureApplicationContainer(container);
+        }
+    }
+
+    public class WindsorNancyBootStrapperFixture
+    {
+        private readonly FakeWindsorNancyBootStrapper _bootStrapper;
+
+        public WindsorNancyBootStrapperFixture()
+        {
+            _bootStrapper = new FakeWindsorNancyBootStrapper();
+        }
+
+        [Fact]
+        public void GetEngine_ReturnsEngine()
+        {
+            var result = _bootStrapper.GetEngine();
+
+            result.ShouldNotBeNull();
+            result.ShouldBeOfType<INancyEngine>();
+        }
+
+        [Fact]
+        public void GetAllModules_Returns_As_MultiInstance()
+        {
+            _bootStrapper.GetEngine();
+            var output1 = _bootStrapper.GetAllModules().Where(nm => nm.GetType() == typeof(Fakes.FakeNancyModuleWithBasePath)).FirstOrDefault();
+            var output2 = _bootStrapper.GetAllModules().Where(nm => nm.GetType() == typeof(Fakes.FakeNancyModuleWithBasePath)).FirstOrDefault();
+
+            output1.ShouldNotBeNull();
+            output2.ShouldNotBeNull();
+            output1.ShouldNotBeSameAs(output2);
+        }
+
+        [Fact]
+        public void GetModuleByKey_Returns_As_MultiInstance()
+        {
+            _bootStrapper.GetEngine();
+            var output1 = _bootStrapper.GetModuleByKey(typeof(Fakes.FakeNancyModuleWithBasePath).FullName);
+            var output2 = _bootStrapper.GetModuleByKey(typeof(Fakes.FakeNancyModuleWithBasePath).FullName);
+
+            output1.ShouldNotBeNull();
+            output2.ShouldNotBeNull();
+            output1.ShouldNotBeSameAs(output2);
+        }
+
+        [Fact]
+        public void GetAllModules_Configures_Child_Container()
+        {
+            _bootStrapper.GetEngine();
+            _bootStrapper.RequestContainerConfigured = false;
+
+            _bootStrapper.GetAllModules();
+
+            _bootStrapper.RequestContainerConfigured.ShouldBeTrue();
+        }
+
+        [Fact]
+        public void GetModuleByKey_Configures_Child_Container()
+        {
+            _bootStrapper.GetEngine();
+            _bootStrapper.RequestContainerConfigured = false;
+
+            _bootStrapper.GetModuleByKey(typeof(Fakes.FakeNancyModuleWithBasePath).FullName);
+
+            _bootStrapper.RequestContainerConfigured.ShouldBeTrue();
+        }
+
+        [Fact]
+        public void GetEngine_ConfigureApplicationContainer_Should_Be_Called()
+        {
+            _bootStrapper.GetEngine();
+
+            _bootStrapper.ApplicationContainerConfigured.ShouldBeTrue();
+        }
+
+        [Fact]
+        public void GetEngine_Defaults_Registered_In_Container()
+        {
+            _bootStrapper.GetEngine();
+
+            _bootStrapper.Container.Resolve<INancyModuleCatalog>();
+            _bootStrapper.Container.Resolve<IRouteResolver>();
+            _bootStrapper.Container.Resolve<ITemplateEngineSelector>();
+            _bootStrapper.Container.Resolve<INancyEngine>();
+            _bootStrapper.Container.Resolve<IModuleKeyGenerator>();
+            _bootStrapper.Container.Resolve<IRouteCache>();
+            _bootStrapper.Container.Resolve<IRouteCacheProvider>();
+        }
+
+        [Fact]
+        public void Get_Module_By_Key_Gives_Same_Request_Lifetime_Instance_To_Each_Dependency()
+        {
+            _bootStrapper.GetEngine();
+
+            var result = _bootStrapper.GetModuleByKey(new DefaultModuleKeyGenerator().GetKeyForModuleType(typeof(FakeNancyModuleWithDependency))) as Fakes.FakeNancyModuleWithDependency;
+
+            result.FooDependency.ShouldNotBeNull();
+            result.FooDependency.ShouldBeSameAs(result.Dependency.FooDependency);
+        }
+
+        [Fact]
+        public void Get_Module_By_Key_Gives_Different_Request_Lifetime_Instance_To_Each_Call()
+        {
+            _bootStrapper.GetEngine();
+
+            var result = _bootStrapper.GetModuleByKey(new DefaultModuleKeyGenerator().GetKeyForModuleType(typeof(FakeNancyModuleWithDependency))) as Fakes.FakeNancyModuleWithDependency;
+            var result2 = _bootStrapper.GetModuleByKey(new DefaultModuleKeyGenerator().GetKeyForModuleType(typeof(FakeNancyModuleWithDependency))) as Fakes.FakeNancyModuleWithDependency;
+
+            result.FooDependency.ShouldNotBeNull();
+            result2.FooDependency.ShouldNotBeNull();
+            result.FooDependency.ShouldNotBeSameAs(result2.FooDependency);
+        }
+    }
+}
---
+++ src/Nancy.BootStrappers.Windsor/WindsorNancyBootStrapper.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.Resolvers.SpecializedResolvers;
+using Castle.Windsor;
+using Nancy.BootStrapper;
+using Nancy.Routing;
+
+namespace Nancy.BootStrappers.Windsor
+{
+    public abstract class WindsorNancyBootStrapper : NancyBootStrapperBase<IWindsorContainer>,
+        INancyBootStrapperPerRequestRegistration<IWindsorContainer>, INancyModuleCatalog
+    {
+        protected IWindsorContainer _container;
+        IEnumerable<ModuleRegistration> _modulesRegistrationTypes;
+
+        #region Overrides of NancyBootStrapperBase<IWindsorContainer>
+
+        protected sealed override INancyEngine GetEngineInternal() { return _container.Resolve<INancyEngine>(); }
+
+        protected sealed override IModuleKeyGenerator GetModuleKeyGenerator() { return _container.Resolve<IModuleKeyGenerator>(); }
+
+        protected sealed override IWindsorContainer CreateContainer()
+        {
+            _container = new WindsorContainer();
+            _container.Register(Component.For<IWindsorContainer>().Instance(_container));
+            _container.Kernel.Resolver.AddSubResolver(new CollectionResolver(_container.Kernel, true));
+            return _container;
+        }
+
+        protected sealed override void RegisterDefaults(IWindsorContainer container,
+            IEnumerable<TypeRegistration> typeRegistrations)
+        {
+            _container.Register(Component.For<INancyModuleCatalog>().Instance(this));
+            var components = typeRegistrations.Select(r => Component.For(r.RegistrationType)
+                .ImplementedBy(r.ImplementationType));
+            _container.Register(components.ToArray());
+
+            container.Register(Component.For<Func<IRouteCache>>().UsingFactoryMethod(ctx =>
+            {
+                Func<IRouteCache> runc = () => _container.Resolve<IRouteCache>();
+                return runc;
+            }));
+        }
+
+        protected sealed override void RegisterModules(IEnumerable<ModuleRegistration> moduleRegistrationTypes) {
+            _modulesRegistrationTypes = moduleRegistrationTypes;
+            RegisterModulesInternal(_container, moduleRegistrationTypes);
+        }
+
+        static void RegisterModulesInternal(IWindsorContainer container, IEnumerable<ModuleRegistration> moduleRegistrationTypes) {
+            var components = moduleRegistrationTypes.Select(r => Component.For(typeof (NancyModule))
+                .ImplementedBy(r.ModuleType)
+                .Named(r.ModuleKey)
+                .LifeStyle.Transient);
+            container.Register(components.ToArray());
+        }
+
+        #endregion
+
+        #region Implementation of INancyBootStrapperPerRequestRegistration<IWindsorContainer>
+
+        public virtual void ConfigureRequestContainer(IWindsorContainer container) { }
+
+        #endregion
+
+        #region Implementation of INancyModuleCatalog
+
+        public IEnumerable<NancyModule> GetAllModules()
+        {
+            var child = GetChild();
+            ConfigureRequestContainer(child);
+            return child.ResolveAll<NancyModule>();
+        }
+
+        public NancyModule GetModuleByKey(string moduleKey)
+        {
+            var child = GetChild();
+            ConfigureRequestContainer(child);
+            return child.Resolve<NancyModule>(moduleKey);
+        }
+
+        IWindsorContainer GetChild()
+        {
+            var child = new WindsorContainer(); 
+            _container.AddChildContainer(child);
+            RegisterModulesInternal(child, _modulesRegistrationTypes); 
+            return child;
+        }
+
+        #endregion
+    }
+}
