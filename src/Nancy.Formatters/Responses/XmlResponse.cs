e595c5852fbfa6d46e5699395f43e5e106e21d2d
Andreas Hakansson <andreas@selfinflicted.org>
2011-01-25 22:51:43 +0100

    Changed coding style to fit Nancy

--- src/Nancy.Formatters/Responses/XmlResponse.cs
+++ src/Nancy.Formatters/Responses/XmlResponse.cs
@@ -9,9 +9,9 @@
     {
         public XmlResponse(TModel model, string contentType)
         {
-            Contents = GetXmlContents(model);
-            ContentType = contentType;
-            StatusCode = HttpStatusCode.OK;
+            this.Contents = GetXmlContents(model);
+            this.ContentType = contentType;
+            this.StatusCode = HttpStatusCode.OK;
         }
 
         private static Action<Stream> GetXmlContents(TModel model)
