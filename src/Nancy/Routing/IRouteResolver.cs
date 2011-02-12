a522ad168ad7d658e6020a308ae3df3a95b5050d
Steven Robbins <ste.robbins@gmail.com>
2011-02-12 18:15:29 +0000

    Added some comments

--- src/Nancy/Routing/IRouteResolver.cs
+++ src/Nancy/Routing/IRouteResolver.cs
@@ -2,8 +2,17 @@
 {
     using System;
 
+    /// <summary>
+    /// Returns a route that matches the request
+    /// </summary>
     public interface IRouteResolver
     {
+        /// <summary>
+        /// Gets the route, and the corresponding parameter dictionary from the URL
+        /// </summary>
+        /// <param name="request">Incoming request</param>
+        /// <param name="cache">Route cache</param>
+        /// <returns>Tuple - Item1 being the Route, Item2 being the parameters dictionary</returns>
         Tuple<Route, DynamicDictionary> Resolve(Request request, RouteCache cache);
     }
 }
