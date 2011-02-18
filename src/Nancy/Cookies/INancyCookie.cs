4a632ff936d9077a74a2d31fd03a67ef350a2a5d
Andreas Hakansson <andreas@selfinflicted.org>
2011-02-18 11:26:02 +0100

    Tidied up the cookie classes a bit

--- src/Nancy/Cookies/INancyCookie.cs
+++ src/Nancy/Cookies/INancyCookie.cs
@@ -12,6 +12,7 @@ namespace Nancy.Cookies
         /// <summary>
         /// When the cookie should expire
         /// </summary>
+        /// <value>A <see cref="DateTime"/> instance containing the date and time when the cookie should expire; otherwise <see langword="null"/> if it should never expire.</value>
         DateTime? Expires { get; set; }
 
         /// <summary>
--- src/Nancy/Cookies/NancyCookie.cs
+++ src/Nancy/Cookies/NancyCookie.cs
@@ -7,18 +7,34 @@ namespace Nancy.Cookies
     {
         public NancyCookie(string name, string value)
         {
-            Name = name;
-            Value = value;
+            this.Name = name;
+            this.Value = value;
         }
 
+        /// <summary>
+        /// The domain to restrict the cookie to
+        /// </summary>
         public string Domain { get; set; }
 
+        /// <summary>
+        /// When the cookie should expire
+        /// </summary>
+        /// <value>A <see cref="DateTime"/> instance containing the date and time when the cookie should expire; otherwise <see langword="null"/> if it should never expire.</value>
         public DateTime? Expires { get; set; }
 
+        /// <summary>
+        /// The name of the cookie
+        /// </summary>
         public string Name { get; private set; }
 
+        /// <summary>
+        /// The path to restrict the cookie to
+        /// </summary>
         public string Path { get; set; }
 
+        /// <summary>
+        /// The value of the cookie
+        /// </summary>
         public string Value { get; private set; }
 
         public override string ToString()
