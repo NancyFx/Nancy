c60e0b3d0847aba9c938bd5afe860343d4020ecf
Andreas Hakansson <andreas@selfinflicted.org>
2011-03-16 08:10:28 +0100

    Implemented async read and write members on the RequestStream

--- src/Nancy.Tests/Unit/IO/RequestStreamFixture.cs
+++ src/Nancy.Tests/Unit/IO/RequestStreamFixture.cs
@@ -381,5 +381,116 @@ namespace Nancy.Tests.Unit.IO
             // Then
             request.IsInMemory.ShouldBeTrue();
         }
+
+        [Fact]
+        public void Should_call_beginread_on_underlaying_stream_when_beginread_is_called()
+        {
+            // Given
+            var buffer = new byte[10];
+            AsyncCallback callback = x => { };
+            var state = new object();
+            var request = new RequestStream(this.stream, 0, 10, true);
+
+            // When
+            request.BeginRead(buffer, 0, buffer.Length, callback, state);
+
+            // Then
+            A.CallTo(() => this.stream.BeginRead(buffer, 0, buffer.Length, callback, state)).MustHaveHappened();
+        }
+
+        [Fact]
+        public void Should_return_result_from_underlaying_beginread_when_beginread_is_called()
+        {
+            // Given
+            var buffer = new byte[10];
+            var asyncResult = A.Fake<IAsyncResult>();
+            AsyncCallback callback = x => { };
+            var state = new object();
+            var request = new RequestStream(this.stream, 0, 10, true);
+            A.CallTo(() => this.stream.BeginRead(buffer, 0, buffer.Length, callback, state)).Returns(asyncResult);
+
+            // When
+            var result = request.BeginRead(buffer, 0, buffer.Length, callback, state);
+
+            // Then
+            result.ShouldBeSameAs(asyncResult);
+        }
+
+        [Fact]
+        public void Should_call_beginwrite_on_underlaying_stream_when_beginwrite_is_called()
+        {
+            // Given
+            var buffer = new byte[10];
+            AsyncCallback callback = x => { };
+            var state = new object();
+            var request = new RequestStream(this.stream, 0, 10, true);
+
+            // When
+            request.BeginWrite(buffer, 0, buffer.Length, callback, state);
+
+            // Then
+            A.CallTo(() => this.stream.BeginWrite(buffer, 0, buffer.Length, callback, state)).MustHaveHappened();
+        }
+
+        [Fact]
+        public void Should_return_result_from_underlaying_beginwrite_when_beginwrite_is_called()
+        {
+            // Given
+            var buffer = new byte[10];
+            var asyncResult = A.Fake<IAsyncResult>();
+            AsyncCallback callback = x => { };
+            var state = new object();
+            var request = new RequestStream(this.stream, 0, 10, true);
+            A.CallTo(() => this.stream.BeginWrite(buffer, 0, buffer.Length, callback, state)).Returns(asyncResult);
+
+            // When
+            var result = request.BeginWrite(buffer, 0, buffer.Length, callback, state);
+
+            // Then
+            result.ShouldBeSameAs(asyncResult);
+        }
+
+        [Fact]
+        public void Should_call_endread_on_underlaying_stream_when_endread_is_called()
+        {
+            // Given
+            var asyncResult = A.Fake<IAsyncResult>();
+            var request = new RequestStream(this.stream, 0, 10, true);
+
+            // When
+            request.EndRead(asyncResult);
+            
+            // Then
+            A.CallTo(() => this.stream.EndRead(asyncResult)).MustHaveHappened();
+        }
+
+        [Fact]
+        public void Should_return_result_from_underlaying_endread_when_endread_is_called()
+        {
+            // Given
+            var asyncResult = A.Fake<IAsyncResult>();
+            var request = new RequestStream(this.stream, 0, 10, true);
+            A.CallTo(() => this.stream.EndRead(A<IAsyncResult>.Ignored.Argument)).Returns(4);
+
+            // When
+            var result = request.EndRead(asyncResult);
+
+            // Then
+            result.ShouldEqual(4);
+        }
+
+        [Fact]
+        public void Should_call_endwrite_on_underlaying_stream_when_endwrite_is_called()
+        {
+            // Given
+            var asyncResult = A.Fake<IAsyncResult>();
+            var request = new RequestStream(this.stream, 0, 10, true);
+
+            // When
+            request.EndWrite(asyncResult);
+
+            // Then
+            A.CallTo(() => this.stream.EndWrite(asyncResult)).MustHaveHappened();
+        }
     }
 }
--- src/Nancy/IO/RequestStream.cs
+++ src/Nancy/IO/RequestStream.cs
@@ -133,11 +133,64 @@ namespace Nancy.IO
         }
 
         /// <summary>
+        /// Begins an asynchronous read operation.
+        /// </summary>
+        /// <returns>
+        /// An <see cref="T:System.IAsyncResult"/> that represents the asynchronous read, which could still be pending.
+        /// </returns>
+        /// <param name="buffer">The buffer to read the data into. </param>
+        /// <param name="offset">The byte offset in <paramref name="buffer"/> at which to begin writing data read from the stream. </param>
+        /// <param name="count">The maximum number of bytes to read. </param>
+        /// <param name="callback">An optional asynchronous callback, to be called when the read is complete. </param>
+        /// <param name="state">A user-provided object that distinguishes this particular asynchronous read request from other requests. </param>
+        public override IAsyncResult BeginRead(byte[] buffer, int offset, int count, AsyncCallback callback, object state)
+        {
+            return this.stream.BeginRead(buffer, offset, count, callback, state);
+        }
+
+        /// <summary>
+        /// Begins an asynchronous write operation.
+        /// </summary>
+        /// <returns>
+        /// An IAsyncResult that represents the asynchronous write, which could still be pending.
+        /// </returns>
+        /// <param name="buffer">The buffer to write data from. </param>
+        /// <param name="offset">The byte offset in <paramref name="buffer"/> from which to begin writing. </param>
+        /// <param name="count">The maximum number of bytes to write. </param>
+        /// <param name="callback">An optional asynchronous callback, to be called when the write is complete. </param>
+        /// <param name="state">A user-provided object that distinguishes this particular asynchronous write request from other requests.</param>
+        public override IAsyncResult BeginWrite(byte[] buffer, int offset, int count, AsyncCallback callback, object state)
+        {
+            return this.stream.BeginWrite(buffer, offset, count, callback, state);
+        }
+
+        /// <summary>
         /// Closes the current stream and releases any resources (such as sockets and file handles) associated with the current stream.
         /// </summary>
         public override void Close()
         {
             this.stream.Close();
+        }
+
+        /// <summary>
+        /// Waits for the pending asynchronous read to complete.
+        /// </summary>
+        /// <returns>
+        /// The number of bytes read from the stream, between zero (0) and the number of bytes you requested. Streams return zero (0) only at the end of the stream, otherwise, they should block until at least one byte is available.
+        /// </returns>
+        /// <param name="asyncResult">The reference to the pending asynchronous request to finish. </param>
+        public override int EndRead(IAsyncResult asyncResult)
+        {
+            return this.stream.EndRead(asyncResult);
+        }
+
+        /// <summary>
+        /// Ends an asynchronous write operation.
+        /// </summary>
+        /// <param name="asyncResult">A reference to the outstanding asynchronous I/O request. </param>
+        public override void EndWrite(IAsyncResult asyncResult)
+        {
+            this.stream.EndWrite(asyncResult);
         }
 
         /// <summary>
