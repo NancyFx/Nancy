5a1e9ebbeefe864dbde94a3e8068c4dc93077089
Steven Robbins <ste.robbins@gmail.com>
2011-03-20 20:38:08 +0000

    Additional comments

--- src/Nancy.Hosting.Owin/Fakes/FakeProducer.cs
+++ src/Nancy.Hosting.Owin/Fakes/FakeProducer.cs
@@ -22,6 +22,13 @@ namespace Nancy.Hosting.Owin.Fakes
         private int chunkSize;
         private bool autoSend;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FakeProducer"/> class. 
+        /// </summary>
+        /// <param name="sendContinuation">Whether to send a continuation to the consumer.</param>
+        /// <param name="buffer">Buffer to send.</param>
+        /// <param name="chunkSize">Chunk size to split the buffer into.</param>
+        /// <param name="autoSend">Automatically send all chunks when the body delegate is invoked.</param>
         public FakeProducer(bool sendContinuation, byte[] buffer, int chunkSize, bool autoSend)
         {
             this.sendContinuation = sendContinuation;
@@ -30,13 +37,31 @@ namespace Nancy.Hosting.Owin.Fakes
             this.autoSend = autoSend;
         }
 
+        /// <summary>
+        /// Gets a value indicating whether the body delegate has been invoked
+        /// </summary>
         public bool BodyDelegateInvoked { get; private set; }
 
+        /// <summary>
+        /// Gets a value indicating whether all chunks have been sent
+        /// </summary>
         public bool IsComplete
         {
             get { return this.currentIndex >= this.buffer.Length; }
         }
 
+        /// <summary>
+        /// Gets a value indicating whether the cancel delegate was called
+        /// </summary>
+        public bool Cancelled { get; private set; }
+
+        /// <summary>
+        /// The actual body delegate
+        /// </summary>
+        /// <param name="onNext">On next delegate</param>
+        /// <param name="onError">On error delegate</param>
+        /// <param name="onComplete">On complete delegate</param>
+        /// <returns>Cancellation delegate</returns>
         public Action BodyDelegate(Func<ArraySegment<byte>, Action, bool> onNext, Action<Exception> onError, Action onComplete)
         {
             this.onNext = onNext;
@@ -53,6 +78,10 @@ namespace Nancy.Hosting.Owin.Fakes
             return this.OnCancel;
         }
 
+        /// <summary>
+        /// Throw an exception to the producer
+        /// </summary>
+        /// <param name="e">Exception to pass</param>
         public void ThrowException(Exception e)
         {
             if (!this.BodyDelegateInvoked)
@@ -63,6 +92,9 @@ namespace Nancy.Hosting.Owin.Fakes
             this.onError.Invoke(e);
         }
 
+        /// <summary>
+        /// Send all chunks
+        /// </summary>
         public void SendAll()
         {
             if (!this.BodyDelegateInvoked)
@@ -78,6 +110,9 @@ namespace Nancy.Hosting.Owin.Fakes
             this.onComplete.Invoke();
         }
 
+        /// <summary>
+        /// Send next chunk
+        /// </summary>
         public void SendChunk()
         {
             if (!this.BodyDelegateInvoked)
@@ -120,6 +155,9 @@ namespace Nancy.Hosting.Owin.Fakes
             }
         }
 
+        /// <summary>
+        /// Invoke the onComplete delegate from the consumer
+        /// </summary>
         public void InvokeOnComplete()
         {
             if (!this.BodyDelegateInvoked)
@@ -136,8 +174,6 @@ namespace Nancy.Hosting.Owin.Fakes
 
             this.active = false;
         }
-
-        protected bool Cancelled { get; set; }
 
         public static implicit operator BodyDelegate(FakeProducer producer)
         {
