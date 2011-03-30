1a51541ad3ffa25c0bfccfb460c20cf2f26d3243
Andreas Hakansson <andreas@selfinflicted.org>
2011-03-30 15:12:48 +0200

    Updated self-hosting demo to listen to 8888 as the description text says

--- src/Nancy.Demo.Hosting.Self/Program.cs
+++ src/Nancy.Demo.Hosting.Self/Program.cs
@@ -7,7 +7,7 @@
     {
         static void Main()
         {
-            var nancyHost = new NancyHost(new Uri("http://localhost:8889/nancy/"));
+            var nancyHost = new NancyHost(new Uri("http://localhost:8888/nancy/"));
             nancyHost.Start();
 
             Console.WriteLine("Nancy now listening - navigate to http://localhost:8888/nancy/. Press enter to stop");
