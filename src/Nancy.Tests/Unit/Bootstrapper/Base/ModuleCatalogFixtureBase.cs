9c3f57677dd01827426c8c43315d218e08b7dd78
Steven Robbins <ste.robbins@gmail.com>
2011-04-06 20:41:05 +0100

    Added test base class for module catalog tests

---
+++ src/Nancy.Tests/Unit/Bootstrapper/Base/ModuleCatalogFixtureBase.cs
@@ -0,0 +1,78 @@
+namespace Nancy.Tests.Unit.Bootstrapper.Base
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Nancy.Bootstrapper;
+    using Xunit;
+
+    public abstract class ModuleCatalogFixtureBase
+    {
+        /// <summary>
+        /// Gets the catalog under test - should have ModuleTypesToRegister
+        /// registred as modules for resolution.
+        /// </summary>
+        protected abstract INancyModuleCatalog Catalog { get; }
+
+        protected IEnumerable<ModuleRegistration> ModuleTypesToRegister
+        {
+            get
+            {
+                return new[]
+                {
+                    new ModuleRegistration(typeof(Fakes.FakeNancyModuleWithBasePath), typeof(Fakes.FakeNancyModuleWithBasePath).FullName), 
+                };
+            }
+        }
+
+        [Fact]
+        public void GetModuleByKey_returns_same_instance_with_same_context()
+        {
+            var context = new NancyContext();
+            var output1 = this.Catalog.GetModuleByKey(typeof(Fakes.FakeNancyModuleWithBasePath).FullName, context);
+            var output2 = this.Catalog.GetModuleByKey(typeof(Fakes.FakeNancyModuleWithBasePath).FullName, context);
+
+            output1.ShouldNotBeNull();
+            output2.ShouldNotBeNull();
+            output1.ShouldBeSameAs(output2);
+        }
+
+        [Fact]
+        public void GetModuleByKey_returns_different_instance_with_different_context()
+        {
+            var context1 = new NancyContext();
+            var context2 = new NancyContext();
+            var output1 = this.Catalog.GetModuleByKey(typeof(Fakes.FakeNancyModuleWithBasePath).FullName, context1);
+            var output2 = this.Catalog.GetModuleByKey(typeof(Fakes.FakeNancyModuleWithBasePath).FullName, context2);
+
+            output1.ShouldNotBeNull();
+            output2.ShouldNotBeNull();
+            output1.ShouldNotBeSameAs(output2);
+        }
+
+        [Fact]
+        public void GetAllModules_returns_same_instances_with_same_context()
+        {
+            var context = new NancyContext();
+            var output1 = this.Catalog.GetAllModules(context).FirstOrDefault();
+            var output2 = this.Catalog.GetAllModules(context).FirstOrDefault();
+
+            output1.ShouldNotBeNull();
+            output2.ShouldNotBeNull();
+            output1.ShouldBeSameAs(output2);
+        }
+
+        [Fact]
+        public void GetAllModules_returns_different_instances_with_different_context()
+        {
+            var context1 = new NancyContext();
+            var context2 = new NancyContext();
+            var output1 = this.Catalog.GetAllModules(context1).FirstOrDefault();
+            var output2 = this.Catalog.GetAllModules(context2).FirstOrDefault();
+
+            output1.ShouldNotBeNull();
+            output2.ShouldNotBeNull();
+            output1.ShouldNotBeSameAs(output2);
+        }
+    }
+}
