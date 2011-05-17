c2b34effef293b3a4d32c7c5bb3a2991978a5a63
Roy Jacobs <roy.jacobs@gmail.com>
2011-05-17 14:44:34 +0200

    Added JsonBody extension to NancyContextExtensions

--- src/Nancy.Testing.Tests/ContextExtensionsTests.cs
+++ src/Nancy.Testing.Tests/ContextExtensionsTests.cs
@@ -3,40 +3,69 @@ namespace Nancy.Testing.Tests
     using System;
     using System.IO;
     using System.Text;
-    using Nancy.Tests;
+	using Nancy.Responses;
+	using Nancy.Tests;
     using Xunit;
 
     public class ContextExtensionsTests
     {
-        [Fact]
-        public void Should_use_documentwrapper_from_context_if_it_is_present()
-        {
-            var wrapper = new DocumentWrapper("<html></html>");
-            var context = new NancyContext();
-            context.Items["@@@@DOCUMENT_WRAPPER@@@@"] = wrapper; // Yucky hardcoded stringyness
+		[Fact]
+		public void Should_use_documentwrapper_from_context_if_it_is_present()
+		{
+			var wrapper = new DocumentWrapper("<html></html>");
+			var context = new NancyContext();
+			context.Items["@@@@DOCUMENT_WRAPPER@@@@"] = wrapper; // Yucky hardcoded stringyness
 
-            var result = context.DocumentBody();
+			var result = context.DocumentBody();
 
-            result.ShouldBeSameAs(wrapper);
-        }
+			result.ShouldBeSameAs(wrapper);
+		}
 
-        [Fact]
-        public void Should_create_new_wrapper_from_response_if_not_already_present()
-        {
-            var called = false;
-            var bodyBytes = Encoding.ASCII.GetBytes("<html></html>");
-            Action<Stream> bodyDelegate = (s) =>
-                {
-                    s.Write(bodyBytes, 0, bodyBytes.Length);
-                    called = true;
-                };
-            var response = new Response { Contents = bodyDelegate };
-            var context = new NancyContext() { Response = response };
+		[Fact]
+		public void Should_create_new_wrapper_from_html_response_if_not_already_present()
+		{
+			var called = false;
+			var bodyBytes = Encoding.ASCII.GetBytes("<html></html>");
+			Action<Stream> bodyDelegate = (s) =>
+			{
+				s.Write(bodyBytes, 0, bodyBytes.Length);
+				called = true;
+			};
+			var response = new Response { Contents = bodyDelegate };
+			var context = new NancyContext() { Response = response };
 
-            var result = context.DocumentBody();
+			var result = context.DocumentBody();
 
-            result.ShouldBeOfType(typeof(DocumentWrapper));
-            called.ShouldBeTrue();
-        }
-    }
+			result.ShouldBeOfType(typeof(DocumentWrapper));
+			called.ShouldBeTrue();
+		}
+
+		public class JsonModel
+		{
+			public string Dummy { get; set; }
+		}
+
+		[Fact]
+		public void Should_use_jsonresponse_from_context_if_it_is_present()
+		{
+			var model = new JsonModel() { Dummy = "Data" };
+			var context = new NancyContext();
+			context.Items["@@@@JSONRESPONSE@@@@"] = model; // Yucky hardcoded stringyness
+
+			var result = context.JsonBody<JsonModel>();
+
+			result.ShouldBeSameAs(model);
+		}
+
+		[Fact]
+		public void Should_create_new_wrapper_from_json_response_if_not_already_present()
+		{
+			var response = new JsonResponse<JsonModel>(new JsonModel() { Dummy = "Data" });
+			var context = new NancyContext() { Response = response };
+
+			var result = context.JsonBody<JsonModel>();
+
+			result.Dummy.ShouldEqual("Data");
+		}
+	}
 }
--- src/Nancy.Testing/NancyContextExtensions.cs
+++ src/Nancy.Testing/NancyContextExtensions.cs
@@ -1,40 +1,66 @@
 namespace Nancy.Testing
 {
-    using System.IO;
+	using System;
+	using System.IO;
+	using Nancy.Json;
 
     /// <summary>
     /// Defines extensions for the <see cref="NancyContext"/> type.
     /// </summary>
     public static class NancyContextExtensions
     {
-        private const string DOCUMENT_WRAPPER_KEY_NAME = "@@@@DOCUMENT_WRAPPER@@@@";
+		private const string DOCUMENT_WRAPPER_KEY_NAME = "@@@@DOCUMENT_WRAPPER@@@@";
+		private const string JSONRESPONSE_KEY_NAME = "@@@@JSONRESPONSE@@@@";
 
-        /// <summary>
-        /// Returns the HTTP response body, of the specified <see cref="NancyContext"/>, wrapped in an <see cref="DocumentWrapper"/> instance.
-        /// </summary>
-        /// <param name="context">The <see cref="NancyContext"/> instance that the HTTP response body should be retrieved from.</param>
-        /// <returns>A <see cref="DocumentWrapper"/> instance, wrapping the HTTP response body of the context.</returns>
-        public static DocumentWrapper DocumentBody(this NancyContext context)
-        {
-            // We only really want to generate this once, so we'll stick it in the context
-            // This isn't ideal, but we don't want to hide the guts of the context from the
-            // tests this will have to do.
-            if (context.Items.ContainsKey(DOCUMENT_WRAPPER_KEY_NAME))
-            {
-                return (DocumentWrapper)context.Items[DOCUMENT_WRAPPER_KEY_NAME];
-            }
+		private static T Cache<T>(NancyContext context, string key, Func<T> getData)
+		{
+			// We only really want to generate this once, so we'll stick it in the context
+			// This isn't ideal, but we don't want to hide the guts of the context from the
+			// tests this will have to do.
+			if (context.Items.ContainsKey(key))
+			{
+				return (T)context.Items[key];
+			}
 
-            DocumentWrapper wrapper;
-            using (var contentsStream = new MemoryStream())
-            {
-                context.Response.Contents.Invoke(contentsStream);
-                contentsStream.Position = 0;
-                wrapper = new DocumentWrapper(contentsStream);
-            }
+			T data = getData.Invoke();
+			context.Items[key] = data;
+			return data;
+		}
 
-            context.Items[DOCUMENT_WRAPPER_KEY_NAME] = wrapper;
+		/// <summary>
+		/// Returns the HTTP response body, of the specified <see cref="NancyContext"/>, wrapped in an <see cref="DocumentWrapper"/> instance.
+		/// </summary>
+		/// <param name="context">The <see cref="NancyContext"/> instance that the HTTP response body should be retrieved from.</param>
+		/// <returns>A <see cref="DocumentWrapper"/> instance, wrapping the HTTP response body of the context.</returns>
+		public static DocumentWrapper DocumentBody(this NancyContext context)
+		{
+			return Cache(context, DOCUMENT_WRAPPER_KEY_NAME, () =>
+			{
+				using (var contentsStream = new MemoryStream())
+				{
+					context.Response.Contents.Invoke(contentsStream);
+					contentsStream.Position = 0;
+					return new DocumentWrapper(contentsStream);
+				}
+			});
+		}
 
-            return wrapper;
-        }
-    }
+		public static TModel JsonBody<TModel>(this NancyContext context)
+		{
+			return Cache(context, JSONRESPONSE_KEY_NAME, () =>
+			{
+				using (var contentsStream = new MemoryStream())
+				{
+					context.Response.Contents.Invoke(contentsStream);
+					contentsStream.Position = 0;
+					using (var contents = new StreamReader(contentsStream))
+					{
+						var serializer = new JavaScriptSerializer();
+						var model = serializer.Deserialize<TModel>(contents.ReadToEnd());
+						return model;
+					}
+				}
+			});
+		}
+	}
 }
