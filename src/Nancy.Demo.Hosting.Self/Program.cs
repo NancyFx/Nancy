e0a841b77aff19497a44fc9acdca84898b31f56b
Steven Robbins <ste.robbins@gmail.com>
2011-06-24 15:27:41 +0100

    Made the self host demo launch the demo page

--- src/Nancy.Demo.Hosting.Self/Program.cs
+++ src/Nancy.Demo.Hosting.Self/Program.cs
@@ -1,6 +1,8 @@
 ﻿namespace Nancy.Demo.Hosting.Self
 {
     using System;
+    using System.Diagnostics;
+
     using Nancy.Hosting.Self;
 
     class Program
@@ -10,7 +12,8 @@
             var nancyHost = new NancyHost(new Uri("http://localhost:8888/nancy/"));
             nancyHost.Start();
 
-            Console.WriteLine("Nancy now listening - navigate to http://localhost:8888/nancy/. Press enter to stop");
+            Console.WriteLine("Nancy now listening - navigating to http://localhost:8888/nancy/. Press enter to stop");
+            Process.Start("http://localhost:8888/nancy/");
             Console.ReadKey();
 
             nancyHost.Stop();
