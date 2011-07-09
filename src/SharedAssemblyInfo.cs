17dfa68cc9eb2f497c0806b6cf2bb26d399a4263
Andy Pike <andy@andypike.com>
2011-07-10 00:32:33 +0100

    Updated the SharedAssemblyInfo file to reset it's version back to 0.6.0 (by running the rake build) after testing the new nuget task. Just to avoid confusion :o)

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -5,5 +5,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("Nancy")]
 [assembly: AssemblyProduct("Nancy")]
 [assembly: AssemblyCopyright("Copyright (C) Andreas Hakansson, Steven Robbins and contributors")]
-[assembly: AssemblyVersion("0.7.0")]
+[assembly: AssemblyVersion("0.6.0")]
 
