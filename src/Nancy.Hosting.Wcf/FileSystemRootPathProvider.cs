edbaa1a98fd43d59b0b68945a827903d140d2dcb
Steven Robbins <ste.robbins@gmail.com>
2011-07-24 15:13:37 +0100

    Changed FileSystemRootPath provider. Fixes #203

--- src/Nancy.Hosting.Wcf/FileSystemRootPathProvider.cs
+++ src/Nancy.Hosting.Wcf/FileSystemRootPathProvider.cs
@@ -1,12 +1,13 @@
 namespace Nancy.Hosting.Wcf
 {
-    using System;
+    using System.IO;
+    using System.Reflection;
 
     public class FileSystemRootPathProvider : IRootPathProvider
     {
         public string GetRootPath()
         {
-            return Environment.CurrentDirectory;
+            return Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
         }
     }
 }
