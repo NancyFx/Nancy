ec030fdc9dea3268a1d68b385a4b5c8451a610b7
Andreas Hakansson <andreas@selfinflicted.org>
2011-07-25 22:46:54 +0200

    Nulled out the assembly version in SharedAssemblyInfo.cs as it is set by the rake file.. also updated two invalid references to the file

--- src/Nancy.Demo.Authentication.Forms.TestingDemo/Properties/SharedAssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-using System.Reflection;
-
-[assembly: AssemblyTitle("my assembly title")]
-[assembly: AssemblyDescription("this is the assembly description")]
-[assembly: AssemblyCompany("a test company")]
-[assembly: AssemblyProduct("a product name goes here")]
-[assembly: AssemblyCopyright("Copyright (C) Andreas Hakansson and contributors")]
-[assembly: AssemblyVersion("0.4.0.0")]
--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -1,9 +1,8 @@
 using System.Reflection;
-using System.Runtime.InteropServices;
+
 [assembly: AssemblyTitle("Nancy")]
 [assembly: AssemblyDescription("A Sinatra inspired web framework for the .NET platform")]
 [assembly: AssemblyCompany("Nancy")]
 [assembly: AssemblyProduct("Nancy")]
 [assembly: AssemblyCopyright("Copyright (C) Andreas Hakansson, Steven Robbins and contributors")]
-[assembly: AssemblyVersion("0.6.0")]
-
+[assembly: AssemblyVersion("0.0.0")]
