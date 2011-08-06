d1c3389c737d042ee120dc38070026dd7db93986
Andreas Hakansson <andreas@selfinflicted.org>
2011-08-07 00:59:10 +0200

    Added the version number to SharedAssemblyInfo.cs because it is used by the Nancy-Version header at runtime

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -4,4 +4,4 @@ using System.Reflection;
 [assembly: AssemblyCompany("Nancy")]
 [assembly: AssemblyProduct("Nancy")]
 [assembly: AssemblyCopyright("Copyright (C) Andreas Hakansson, Steven Robbins and contributors")]
-[assembly: AssemblyVersion("0.0.0")]
+[assembly: AssemblyVersion("0.7.1")]
