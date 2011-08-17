0c483516d341e772842b2b9f4a27f35eef6443b3
Andreas Hakansson <andreas@selfinflicted.org>
2011-08-18 00:02:53 +0200

    Added nuget for basic authentication

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -1,7 +1,9 @@
 using System.Reflection;
+using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Nancy")]
 [assembly: AssemblyDescription("A Sinatra inspired web framework for the .NET platform")]
 [assembly: AssemblyCompany("Nancy")]
 [assembly: AssemblyProduct("Nancy")]
 [assembly: AssemblyCopyright("Copyright (C) Andreas Hakansson, Steven Robbins and contributors")]
 [assembly: AssemblyVersion("0.7.1")]
+
