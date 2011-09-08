43101d0089b4df1713ad60d405d2633472cbf2ac
Steven Robbins <ste.robbins@gmail.com>
2011-09-08 15:46:04 +0100

    Updated debug mode - doesn't seem to work though

--- src/Nancy/StaticConfiguration.cs
+++ src/Nancy/StaticConfiguration.cs
@@ -1,34 +1,66 @@
 namespace Nancy
 {
+    using System;
+    using System.Diagnostics;
+    using System.Linq;
+    using System.Reflection;
+
+    using Nancy.Bootstrapper;
+
     public static class StaticConfiguration
     {
-#if DEBUG
-        private static bool disableCaches = true;
-#else
-        private static bool disableCaches = false;
-#endif   
+        private static bool? isRunningDebug;
+
+        static StaticConfiguration()
+        {
+            DisableCaches = DisableCaches = IsRunningDebug;
+        }
 
         /// <summary>
         /// Gets or sets a value indicating whether Nancy should disable caching
         /// </summary>
-        public static bool DisableCaches
-        {
-            get { return disableCaches; }
-            set { disableCaches = value; }
-        }
+        public static bool DisableCaches { get; set; }
 
-#if DEBUG
-        private static bool disableErrorTraces = false;
-#else
-        private static bool disableErrorTraces = true;
-#endif
         /// <summary>
         /// Gets or sets a value indicating whether or not to disable traces in error messages
         /// </summary>
-        public static bool DisableErrorTraces
+        public static bool DisableErrorTraces { get; set; }
+
+        /// <summary>
+        /// Gets a value indicating whether we are running in debug mode or not.
+        /// Checks the entry assembly to see whether it has been built in debug mode.
+        /// If anything goes wrong it returns false.
+        /// </summary>
+        public static bool IsRunningDebug
         {
-            get { return disableErrorTraces; }
-            set { disableErrorTraces = value; }
+            get
+            {
+                return isRunningDebug ?? (bool)(isRunningDebug = GetDebugMode());
+            }
+        }
+
+        private static bool GetDebugMode()
+        {
+            try
+            {
+                var assembly = AppDomainAssemblyTypeScanner.TypesOf<NancyModule>(true).FirstOrDefault().Assembly;
+
+                var attributes = assembly.GetCustomAttributes(typeof(DebuggableAttribute), true);
+
+                if (attributes.Length == 0)
+                {
+                    return false;
+                }
+
+                var d = (DebuggableAttribute)attributes[0];
+
+                return d.IsJITTrackingEnabled;
+            }
+            catch (Exception)
+            {
+                // Evil catch all - don't want to blow up trying to detect debug mode!
+                return false;
+            }
         }
     }
 }
