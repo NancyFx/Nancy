2291ad2961b9f6ee6a2e77716bbadb121abe2df9
Steven Robbins <ste.robbins@gmail.com>
2011-09-09 12:08:59 +0100

    Rejiggered statics - seems to work now

--- src/Nancy/StaticConfiguration.cs
+++ src/Nancy/StaticConfiguration.cs
@@ -3,13 +3,16 @@ namespace Nancy
     using System;
     using System.Diagnostics;
     using System.Linq;
-    using System.Reflection;
 
     using Nancy.Bootstrapper;
 
     public static class StaticConfiguration
     {
         private static bool? isRunningDebug;
+
+        private static bool? disableCaches;
+
+        private static bool? disableErrorTraces;
 
         static StaticConfiguration()
         {
@@ -19,19 +22,39 @@ namespace Nancy
         /// <summary>
         /// Gets or sets a value indicating whether Nancy should disable caching
         /// </summary>
-        public static bool DisableCaches { get; set; }
+        public static bool DisableCaches
+        {
+            get
+            {
+                return disableCaches ?? (bool)(disableCaches = IsRunningDebug);
+            }
+            set
+            {
+                disableCaches = value;
+            }
+        }
 
         /// <summary>
         /// Gets or sets a value indicating whether or not to disable traces in error messages
         /// </summary>
-        public static bool DisableErrorTraces { get; set; }
+        public static bool DisableErrorTraces
+        {
+            get
+            {
+                return disableErrorTraces ?? (bool)(disableErrorTraces = IsRunningDebug);
+            }
+            set
+            {
+                disableErrorTraces = value;
+            }
+        }
 
         /// <summary>
         /// Gets a value indicating whether we are running in debug mode or not.
         /// Checks the entry assembly to see whether it has been built in debug mode.
         /// If anything goes wrong it returns false.
         /// </summary>
-        public static bool IsRunningDebug
+        private static bool IsRunningDebug
         {
             get
             {
