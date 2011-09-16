f83287d9d4f47e52da69b957b6ede6539bbecdbb
Andreas Hakansson <andreas@selfinflicted.org>
2011-09-16 10:44:36 +0200

    Updated convention implementation

--- src/Nancy/Conventions/DefaultStaticContentsConventions.cs
+++ src/Nancy/Conventions/DefaultStaticContentsConventions.cs
@@ -1,6 +1,7 @@
 namespace Nancy.Conventions
 {
     using System;
+    using System.Collections.Concurrent;
     using System.Collections.Generic;
     using System.IO;
     using System.Linq;
@@ -13,36 +14,7 @@ namespace Nancy.Conventions
         {
             conventions.StaticContentsConventions = new List<Func<NancyContext, string, Response>>
             {
-                (ctx, rootPath) => {
-
-                    var extension =
-                        Path.GetExtension(ctx.Request.Path);
-
-                    if (string.IsNullOrEmpty(extension))
-                    {
-                        return null;
-                    }
-
-                    var fileName =
-                        Path.GetFileName(ctx.Request.Path);
-
-                    var path = ctx.Request.Path
-                        .Substring(1)
-                        .Replace(fileName, string.Empty)
-                        .TrimEnd(new [] { '/' });
-
-                    if (!path.Equals(StaticConfiguration.StaticContentsDirectory, StringComparison.OrdinalIgnoreCase))
-                    {
-                        return null;
-                    }
-
-                    var filePath =
-                        Path.Combine(rootPath, StaticConfiguration.StaticContentsDirectory, fileName);
-
-                    return File.Exists(filePath) ? 
-                        new GenericFileResponse(filePath) : 
-                        null;
-                }
+                StaticContentConventionBuilder.AddDirectory("content")
             };
         }
 
@@ -87,20 +59,88 @@ namespace Nancy.Conventions
 
         public void Initialize(IApplicationPipelines pipelines)
         {
-            StaticContentsHook.Enable(pipelines, this.rootPathProvider, this.conventions);
-        }
-    }
-
-    public static class StaticContentsHook
-    {
-        public static void Enable(IApplicationPipelines pipelines, IRootPathProvider rootPathProvider, StaticContentsConventions conventions)
-        {
             pipelines.BeforeRequest.AddItemToStartOfPipeline(ctx =>
             {
                 return conventions
                     .Select(convention => convention.Invoke(ctx, rootPathProvider.GetRootPath()))
                     .FirstOrDefault(response => response != null);
             });
+        }
+    }
+
+    public class StaticContentConventionBuilder
+    {
+        private static readonly ConcurrentDictionary<string, Func<Response>> ResponseFactoryCache;
+
+        static StaticContentConventionBuilder()
+        {
+            ResponseFactoryCache = new ConcurrentDictionary<string, Func<Response>>();
+        }
+
+        public static Func<NancyContext, string, Response> AddDirectory(string contentPath, string virtualPath = null, params string[] allowedExtensions)
+        {
+            return (ctx, root) =>
+            {
+                var path =
+                    ctx.Request.Path.TrimStart(new[] { '/' });
+
+                virtualPath = virtualPath ?? contentPath;
+
+                if (!path.StartsWith(virtualPath, StringComparison.OrdinalIgnoreCase))
+                {
+                    return null;
+                }
+
+                var responseFactory =
+                    ResponseFactoryCache.GetOrAdd(path, BuildContentDelegate(root, contentPath, virtualPath, allowedExtensions));
+
+                return responseFactory.Invoke();
+            };
+        }
+
+        private static Func<string, Func<Response>> BuildContentDelegate(string applicationRootPath, string contentPath, string virtualPath, string[] allowedExtensions)
+        {
+            return requestPath =>
+            {
+                var extension = Path.GetExtension(requestPath);
+
+                if (string.IsNullOrEmpty(extension))
+                {
+                    return () => null;
+                }
+
+                if (allowedExtensions.Length != 0 && !allowedExtensions.Any(e => string.Equals(e, extension, StringComparison.OrdinalIgnoreCase)))
+                {
+                    return () => null;
+                }
+
+                if(!contentPath.Equals(virtualPath, StringComparison.OrdinalIgnoreCase))
+                {
+                    requestPath = String.Concat(contentPath, requestPath.Substring(virtualPath.Length));
+                }
+
+                if (!IsSafeToCombinePaths(applicationRootPath, requestPath))
+                {
+                    return () => null;
+                }
+
+                var fileName = Path.Combine(applicationRootPath, requestPath);
+
+                if (!File.Exists(fileName))
+                {
+                    return () => null;
+                }
+
+                return () => new GenericFileResponse(fileName);
+            };
+        }
+
+        private static bool IsSafeToCombinePaths(string contentsFolderPath, string requestedPath)
+        {
+            var combinedPath =
+                Path.GetFullPath(Path.Combine(contentsFolderPath, requestedPath));
+
+            return combinedPath.StartsWith(contentsFolderPath);
         }
     }
 }
