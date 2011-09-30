346a1379c09eb3324b3ad8b6e980f78d81735d0a
Steven Robbins <ste.robbins@gmail.com>
2011-09-30 08:43:53 +0100

    Fix for issue #258

---
+++ src/Nancy.Hosting.Owin.Tests/ResponseStreamFixture.cs
@@ -0,0 +1,18 @@
+﻿namespace Nancy.Tests
+{
+    using Nancy.Hosting.Owin;
+
+    using Xunit;
+
+    public class ResponseStreamFixture
+    {
+        [Fact]
+        public void Should_be_idempotent_when_calling_dispose()
+        {
+            var stream = new ResponseStream((arr, act) => false, () => { });
+
+            stream.Dispose();
+            stream.Dispose();
+        }
+    }
+}
