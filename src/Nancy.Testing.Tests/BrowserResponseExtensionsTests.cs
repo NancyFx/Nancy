af7316036f4d27ad1a6ef36729b3e90683fb4263
Christian Horsdal <c.horsdal@gmail.com>
2011-10-01 22:12:04 +0200

    adding a couple of failing tests, for not yet written BodyAsXml extension to BrowserResponse

---
+++ src/Nancy.Testing.Tests/BrowserResponseExtensionsTests.cs
@@ -0,0 +1,34 @@
+﻿namespace Nancy.Testing.Tests
+{
+	using System;
+	using System.Linq;
+	using Xunit;
+
+	public class BrowserResponseExtensionsTests
+	{
+		private BrowserResponse sut;
+
+		public BrowserResponseExtensionsTests()
+		{
+			var response = new Response();
+			response = "<tag />";
+
+			var context = new NancyContext() { Response = response };
+			sut = new BrowserResponse(context);			
+		}
+
+		[Fact]
+		public void Should_create_xdocument_from_xml_body()
+		{
+			var bodyAsXml = sut.BodyAsXml();
+
+			Assert.NotNull(bodyAsXml.Element("tag").FirstOrDefault());
+		}
+
+		[Fact]
+		public void Should_fail_to_create_xdocument_from_non_xml_body()
+		{
+			Assert.Throws<InvalidOperationException>(sut.BodyAsXml);
+		}
+	}
+}
