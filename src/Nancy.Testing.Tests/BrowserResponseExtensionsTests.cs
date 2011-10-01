70243caaa6a0c425b7ac13289eff233e95569462
Christian Horsdal <c.horsdal@gmail.com>
2011-10-01 22:22:09 +0200

    refining failing tests, to something, that might actually compile eventually

--- src/Nancy.Testing.Tests/BrowserResponseExtensionsTests.cs
+++ src/Nancy.Testing.Tests/BrowserResponseExtensionsTests.cs
@@ -1,34 +1,34 @@
 ﻿namespace Nancy.Testing.Tests
 {
-	using System;
-	using System.Linq;
+using System.Xml;
 	using Xunit;
 
 	public class BrowserResponseExtensionsTests
 	{
 		private BrowserResponse sut;
 
-		public BrowserResponseExtensionsTests()
+		[Fact]
+		public void Should_create_xdocument_from_xml_body()
 		{
 			var response = new Response();
 			response = "<tag />";
 
 			var context = new NancyContext() { Response = response };
-			sut = new BrowserResponse(context);			
-		}
+			sut = new BrowserResponse(context); var bodyAsXml = sut.BodyAsXml();
 
-		[Fact]
-		public void Should_create_xdocument_from_xml_body()
-		{
-			var bodyAsXml = sut.BodyAsXml();
-
-			Assert.NotNull(bodyAsXml.Element("tag").FirstOrDefault());
+			Assert.NotNull(bodyAsXml.Element("tag"));
 		}
 
 		[Fact]
 		public void Should_fail_to_create_xdocument_from_non_xml_body()
 		{
-			Assert.Throws<InvalidOperationException>(sut.BodyAsXml);
+			var response = new Response();
+			response = "hello";
+
+			var context = new NancyContext() { Response = response };
+			sut = new BrowserResponse(context);	
+
+			Assert.Throws<XmlException>(() => sut.BodyAsXml());
 		}
 	}
 }
