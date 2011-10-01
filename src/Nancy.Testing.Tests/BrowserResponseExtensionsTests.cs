eef8218c52baa38a6720a35d34c3206f33618880
Christian Horsdal <c.horsdal@gmail.com>
2011-10-01 22:25:19 +0200

    refactored tests for BodyAsXml to make read more directly

--- src/Nancy.Testing.Tests/BrowserResponseExtensionsTests.cs
+++ src/Nancy.Testing.Tests/BrowserResponseExtensionsTests.cs
@@ -10,10 +10,7 @@ using System.Xml;
 		[Fact]
 		public void Should_create_xdocument_from_xml_body()
 		{
-			var response = new Response();
-			response = "<tag />";
-
-			var context = new NancyContext() { Response = response };
+			var context = new NancyContext() { Response = "<tag />" };
 			sut = new BrowserResponse(context); var bodyAsXml = sut.BodyAsXml();
 
 			Assert.NotNull(bodyAsXml.Element("tag"));
@@ -22,10 +19,7 @@ using System.Xml;
 		[Fact]
 		public void Should_fail_to_create_xdocument_from_non_xml_body()
 		{
-			var response = new Response();
-			response = "hello";
-
-			var context = new NancyContext() { Response = response };
+			var context = new NancyContext() { Response = "hello" };
 			sut = new BrowserResponse(context);	
 
 			Assert.Throws<XmlException>(() => sut.BodyAsXml());
