070fe23ca36358980b8e3d0d0a715ceda0a1469d
Andreas Hakansson <andreas@selfinflicted.org>
2011-10-17 12:07:52 +0200

    Updated FormsAutoBootstrapper to demo per-request
    The FormsAuth demo now uses a per-request instance of the
    IUserMapper. The demo has been updated to show how to register the
    dependencies and how to register pipeline items with request scoped
    dependencies.

--- src/Nancy.Demo.Authentication.Forms/FormsAuthBootstrapper.cs
+++ src/Nancy.Demo.Authentication.Forms/FormsAuthBootstrapper.cs
@@ -1,20 +1,32 @@
 namespace Nancy.Demo.Authentication.Forms
 {
-    using Bootstrapper;
     using Nancy;
     using Nancy.Authentication.Forms;
 
     public class FormsAuthBootstrapper : DefaultNancyBootstrapper
     {
-        protected override void InitialiseInternal(TinyIoC.TinyIoCContainer container, IPipelines pipelines)
+        protected override void ConfigureApplicationContainer(TinyIoC.TinyIoCContainer container)
         {
-            base.InitialiseInternal(container, pipelines);
+        }
 
-            var formsAuthConfiguration = 
+        protected override void InitialiseInternal(TinyIoC.TinyIoCContainer container, Bootstrapper.IPipelines pipelines)
+        {
+        }
+
+        protected override void ConfigureRequestContainer(TinyIoC.TinyIoCContainer container)
+        {
+            base.ConfigureRequestContainer(container);
+
+            container.Register<IUserMapper, UserDatabase>();
+        }
+
+        protected override void InitialiseRequestInternal(TinyIoC.TinyIoCContainer requestContainer, Bootstrapper.IPipelines pipelines)
+        {
+            var formsAuthConfiguration =
                 new FormsAuthenticationConfiguration()
                 {
                     RedirectUrl = "~/login",
-                    UserMapper = container.Resolve<IUserMapper>(),
+                    UserMapper = requestContainer.Resolve<IUserMapper>(),
                 };
 
             FormsAuthentication.Enable(pipelines, formsAuthConfiguration);
