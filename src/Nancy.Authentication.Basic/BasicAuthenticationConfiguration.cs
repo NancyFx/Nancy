ac2336a4dddd95160999acd88d41d867dcf92131
Chris Sainty <chris.sainty@quids.com.au>
2011-10-18 10:42:12 +1100

    Reformatted file to use spaces instead of tabs to match the rest of the project.

--- src/Nancy.Authentication.Basic/BasicAuthenticationConfiguration.cs
+++ src/Nancy.Authentication.Basic/BasicAuthenticationConfiguration.cs
@@ -5,47 +5,47 @@ using System.Text;
 
 namespace Nancy.Authentication.Basic
 {
-	/// <summary>
-	/// Configuration options for forms authentication
-	/// </summary>
-	public class BasicAuthenticationConfiguration
-	{
-		/// <summary>
-		/// Initializes a new instance of the <see cref="BasicAuthenticationConfiguration"/> class.
+    /// <summary>
+    /// Configuration options for forms authentication
+    /// </summary>
+    public class BasicAuthenticationConfiguration
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BasicAuthenticationConfiguration"/> class.
         /// </summary>
-		/// <param name="userValidator">A valid instance of <see cref="IUserValidator"/> class</param>
-		/// <param name="realm">Basic authentication realm</param>
+        /// <param name="userValidator">A valid instance of <see cref="IUserValidator"/> class</param>
+        /// <param name="realm">Basic authentication realm</param>
         /// <param name="promptUser">Tell the browser to prompt the user for credentials</param>
-		public BasicAuthenticationConfiguration(IUserValidator userValidator, string realm, bool promptUser = true)
+        public BasicAuthenticationConfiguration(IUserValidator userValidator, string realm, bool promptUser = true)
         {
-			if (userValidator == null)
-				throw new ArgumentNullException("userValidator");
+            if (userValidator == null)
+                throw new ArgumentNullException("userValidator");
 
-			if (string.IsNullOrEmpty(realm))
-				throw new ArgumentException("realm");
+            if (string.IsNullOrEmpty(realm))
+                throw new ArgumentException("realm");
 
-			this.UserValidator = userValidator;
-			this.Realm = realm;
+            this.UserValidator = userValidator;
+            this.Realm = realm;
             this.PromptUser = promptUser;
         }
 
-		/// <summary>
-		/// Gets the user validator
-		/// </summary>
-		public IUserValidator UserValidator
-		{
-			get;
-			private set;
-		}
+        /// <summary>
+        /// Gets the user validator
+        /// </summary>
+        public IUserValidator UserValidator
+        {
+            get;
+            private set;
+        }
 
-		/// <summary>
-		/// Gets the basic authentication realm
-		/// </summary>
-		public string Realm
-		{
-			get;
-			private set;
-		}
+        /// <summary>
+        /// Gets the basic authentication realm
+        /// </summary>
+        public string Realm
+        {
+            get;
+            private set;
+        }
 
         /// <summary>
         /// Determines whether the browser should prompt for credentials
@@ -55,5 +55,5 @@ namespace Nancy.Authentication.Basic
             get;
             private set;
         }
-	}
+    }
 }
