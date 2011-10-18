1009b6bd79487ee977ef8ae36c03ba08c13a28cb
Steven Robbins <ste.robbins@gmail.com>
2011-10-18 20:40:43 +0100

    Test fixup

--- src/Nancy.Testing.Tests/BrowserResponseExtensionsTests.cs
+++ src/Nancy.Testing.Tests/BrowserResponseExtensionsTests.cs
@@ -1,7 +1,8 @@
 ﻿namespace Nancy.Testing.Tests
 {
 using System.Xml;
-	using Xunit;
+using FakeItEasy;
+using Xunit;
 
 	public class BrowserResponseExtensionsTests
 	{
@@ -11,7 +12,8 @@ using System.Xml;
 		public void Should_create_xdocument_from_xml_body()
 		{
 			var context = new NancyContext() { Response = "<tag />" };
-			sut = new BrowserResponse(context); var bodyAsXml = sut.BodyAsXml();
+            sut = new BrowserResponse(context, A.Fake<Browser>()); 
+            var bodyAsXml = sut.BodyAsXml();
 
 			Assert.NotNull(bodyAsXml.Element("tag"));
 		}
@@ -20,7 +22,7 @@ using System.Xml;
 		public void Should_fail_to_create_xdocument_from_non_xml_body()
 		{
 			var context = new NancyContext() { Response = "hello" };
-			sut = new BrowserResponse(context);	
+			sut = new BrowserResponse(context, A.Fake<Browser>());	
 
 			Assert.Throws<XmlException>(() => sut.BodyAsXml());
 		}
