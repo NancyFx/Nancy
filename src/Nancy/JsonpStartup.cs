e582bc8f17e8511a940e2a378a52415e99bf4d8f
James Hughes <james@yobriefca.se>
2011-11-01 20:01:15 +0000

    Moved Jsonp Stuff into its own static class Added enable and disable methods

--- src/Nancy/JsonpStartup.cs
+++ src/Nancy/JsonpStartup.cs
@@ -27,36 +27,7 @@ namespace Nancy
 
         public void Initialize(IPipelines pipelines)
         {
-            var item = new PipelineItem<Action<NancyContext>>("JSONP", context =>
-            {
-                bool isJson = context.Response.ContentType == "application/json";
-                bool hasCallback = context.Request.Query["callback"].HasValue;
-
-                if (isJson && hasCallback)
-                {
-                    // grab original contents for running later
-                    Action<Stream> original = context.Response.Contents;
-                    string callback = context.Request.Query["callback"].Value;
-
-                    // set content type to application/javascript so browsers can handle it by default
-                    // http://stackoverflow.com/questions/111302/best-content-type-to-serve-jsonp
-                    context.Response.ContentType = "application/javascript";
-                    context.Response.Contents = stream =>
-                    {
-                        // disposing of stream is handled elsewhere
-                        StreamWriter writer = new StreamWriter(stream)
-                        {
-                            AutoFlush = true
-                        };
-
-                        writer.Write("{0}(", callback);
-                        original(stream);
-                        writer.Write(");");
-                    };
-                }
-            });
-
-            pipelines.AfterRequest.AddItemToEndOfPipeline(item);
+            Nancy.Jsonp.Enable(pipelines);
         }
     }
 }
