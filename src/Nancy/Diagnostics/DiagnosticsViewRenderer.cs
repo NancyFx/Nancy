b7f6fbd61420607500a6e36e9b4927cd8c218a25
Steven Robbins <ste.robbins@gmail.com>
2011-12-09 12:19:49 +0000

    Made partials/master pages work and added a master page

--- src/Nancy/Diagnostics/DiagnosticsViewRenderer.cs
+++ src/Nancy/Diagnostics/DiagnosticsViewRenderer.cs
@@ -7,6 +7,10 @@ namespace Nancy.Diagnostics
 
     public class DiagnosticsViewRenderer
     {
+        private static readonly IViewResolver ViewResolver = new DiagnosticsViewResolver();
+
+        private static readonly IViewEngine Engine = new SuperSimpleViewEngineWrapper();
+
         public Response this[string name]
         {
             get { return RenderView(name, null); }
@@ -19,25 +23,11 @@ namespace Nancy.Diagnostics
 
         private static Response RenderView(string name, dynamic model)
         {
-            var engine =
-                new SuperSimpleViewEngineWrapper();
+            var fullName = string.Concat(name, ".sshtml");
 
-            name = string.Concat(name, ".sshtml");
+            var stream = GetBodyStream(fullName);
 
-            var view =
-                new EmbeddedFileResponse(typeof(DiagnosticsViewRenderer).Assembly, "Nancy.Diagnostics.Views", name);
-
-            var stream =
-                new MemoryStream();
-
-            view.Contents.Invoke(stream);
-            stream.Position = 0;
-
-            var location = new ViewLocationResult(
-                "Nancy/Diagnostics/Views",
-                name,
-                "sshtml",
-                () => new StreamReader(stream));
+            var location = GetViewLocationResult(fullName, stream);
 
             var cache = new DefaultViewCache();
 
@@ -47,9 +37,48 @@ namespace Nancy.Diagnostics
             context.Items.Add(CsrfToken.DEFAULT_CSRF_KEY, "DIAGNOSTICSTOKEN");
 
             var renderContext = 
-                new DefaultRenderContext(null, cache, new ViewLocationContext() { Context = context });
+                new DefaultRenderContext(ViewResolver, cache, new ViewLocationContext() { Context = context });
 
-            return engine.RenderView(location, model, renderContext);
+            return Engine.RenderView(location, model, renderContext);
+        }
+
+        private static Stream GetBodyStream(string name)
+        {
+            var view = new EmbeddedFileResponse(typeof(DiagnosticsViewRenderer).Assembly, "Nancy.Diagnostics.Views", name);
+
+            var stream = new MemoryStream();
+
+            view.Contents.Invoke(stream);
+            stream.Position = 0;
+            return stream;
+        }
+
+        private static ViewLocationResult GetViewLocationResult(string name, Stream bodyStream)
+        {
+            return new ViewLocationResult(
+                "Nancy/Diagnostics/Views",
+                name,
+                "sshtml",
+                () => new StreamReader(bodyStream));
+        }
+
+        internal class DiagnosticsViewResolver : IViewResolver
+        {
+            /// <summary>
+            /// Locates a view based on the provided information.
+            /// </summary>
+            /// <param name="viewName">The name of the view to locate.</param>
+            /// <param name="model">The model that will be used with the view.</param>
+            /// <param name="viewLocationContext">A <see cref="ViewLocationContext"/> instance, containing information about the context for which the view is being located.</param>
+            /// <returns>A <see cref="ViewLocationResult"/> instance if the view could be found, otherwise <see langword="null"/>.</returns>
+            public ViewLocationResult GetViewLocation(string viewName, dynamic model, ViewLocationContext viewLocationContext)
+            {
+                var fullName = string.Concat(viewName, ".sshtml");
+
+                var stream = GetBodyStream(fullName);
+
+                return GetViewLocationResult(fullName, stream);
+            }
         }
     }
 }
