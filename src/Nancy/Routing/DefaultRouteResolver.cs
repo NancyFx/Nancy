1585d7fc8ed56a53fecf1e5351440d30f7f718d6
Andreas Hakansson <andreas@selfinflicted.org>
2011-12-13 13:24:18 +0100

    Updated DefaultRouteResolver for diagnostics
    Internally the DefaultRouteResolver will now contain information about
    why routes were rejected from being selected as the route that should
    be invoked for an incoming request. An IDiagnosticsProvider buddy class
    can use this information to provide an interactive diagnostics tool.

--- src/Nancy/Routing/DefaultRouteResolver.cs
+++ src/Nancy/Routing/DefaultRouteResolver.cs
@@ -1,5 +1,6 @@
 ﻿namespace Nancy.Routing
 {
+    using System;
     using System.Collections.Generic;
     using System.Linq;
 
@@ -36,47 +37,10 @@
         /// <returns>Tuple - Item1 being the Route, Item2 being the parameters dictionary, Item3 being the prereq, Item4 being the postreq</returns>
         public ResolveResult Resolve(NancyContext context, IRouteCache routeCache)
         {
-            if (routeCache.IsEmpty())
-            {
-                context.Diagnostic.TraceLog.WriteLog(s => s.AppendLine("[RouteResolver] No Routes Available"));
-                return new ResolveResult(new NotFoundRoute(context.Request.Method, context.Request.Path), DynamicDictionary.Empty, null, null);
-            }
+            var result =
+                this.Resolve(context.Request.Path, context, routeCache);
 
-            var routesThatMatchRequestedPath = this.GetRoutesThatMatchRequestedPath(routeCache, context).ToList();
-
-            if (NoRoutesWereAbleToBeMatchedInRouteCache(routesThatMatchRequestedPath))
-            {
-                context.Diagnostic.TraceLog.WriteLog(s => s.AppendLine("[RouteResolver] No Matching Routes Available"));
-                return new ResolveResult(new NotFoundRoute(context.Request.Method, context.Request.Path), DynamicDictionary.Empty, null, null);
-            }
-
-            var routesWithCorrectRequestMethod =
-                GetRoutesWithCorrectRequestMethod(context.Request, routesThatMatchRequestedPath).ToList();
-
-            if (NoRoutesWereForTheRequestedMethod(routesWithCorrectRequestMethod))
-            {
-                var allowedMethods = routesThatMatchRequestedPath.Select(x => x.Item3.Method);
-                context.Diagnostic.TraceLog.WriteLog(s => s.AppendLine("[RouteResolver] Route Matched But Method Not Allowed"));
-                return new ResolveResult(new MethodNotAllowedRoute(context.Request.Path, context.Request.Method, allowedMethods), DynamicDictionary.Empty, null, null);
-            }
-
-            var exactMatch = GetRouteMatchesWithExactPathMatch(routesWithCorrectRequestMethod).FirstOrDefault();
-            if (exactMatch != null)
-            {
-                context.Diagnostic.TraceLog.WriteLog(s => s.AppendLine("[RouteResolver] Found exact match route"));
-
-                return this.CreateRouteAndParametersFromMatch(context, exactMatch);
-            }
-
-            var routeMatchesWithMostParameterCaptures = 
-                GetTopRouteMatches(routesWithCorrectRequestMethod);
-
-            var routeMatchToReturn = 
-                GetSingleRouteToReturn(routeMatchesWithMostParameterCaptures);
-
-            context.Diagnostic.TraceLog.WriteLog(s => s.AppendLine("[RouteResolver] Selected best match"));
-
-            return this.CreateRouteAndParametersFromMatch(context, routeMatchToReturn);
+            return result.Selected;
         }
 
         private ResolveResult CreateRouteAndParametersFromMatch(NancyContext context, RouteCandidate routeMatchToReturn)
@@ -97,67 +61,219 @@
             return this.moduleBuilder.BuildModule(module, context);
         }
 
-        private static RouteCandidate GetSingleRouteToReturn(IEnumerable<RouteCandidate> routesWithMostParameterCaptures)
-        {
-            return routesWithMostParameterCaptures.First();
-        }
-
-        private static IEnumerable<RouteCandidate> GetRouteMatchesWithExactPathMatch(IEnumerable<RouteCandidate> routesWithCorrectRequestMethod)
-        {
-            return routesWithCorrectRequestMethod.Where(
-                x => x.Item4.Parameters.GetDynamicMemberNames().Count() == 0);
-        }
-
-        private static IEnumerable<RouteCandidate> GetTopRouteMatches(IEnumerable<RouteCandidate> routesWithCorrectRequestMethod)
-
+        private static IEnumerable<RouteCandidate> GetTopRouteMatchesNew(Tuple<List<RouteCandidate>, Dictionary<string, List<RouteCandidate>>> routes)
         {
             var maxSegments = 0;
             var maxParameters = 0;
+            
             // Order is by number of path segment matches first number of parameter matches second.  
             // If two candidates have the same number of path segments the tie breaker is the parameter count.
-            foreach (var tuple in routesWithCorrectRequestMethod
+            var selectedRoutes = routes.Item1
                 .OrderBy(x => x.Item4.Parameters.GetDynamicMemberNames().Count())
-                .OrderByDescending(x => x.Item3.Path.Count(c => c.Equals('/'))))
+                .OrderByDescending(x => x.Item3.Path.Count(c => c.Equals('/')));
+
+            foreach (var tuple in selectedRoutes)
             {
-                var segments = tuple.Item3.Path.Count(c => c == '/');
-                var parameters = tuple.Item4.Parameters.GetDynamicMemberNames().Count();
+                var segments = 
+                    tuple.Item3.Path.Count(c => c == '/');
+                
+                var parameters = 
+                    tuple.Item4.Parameters.GetDynamicMemberNames().Count();
+                
                 if (segments < maxSegments || parameters < maxParameters)
+                {
                     yield break;
+                }
+
                 maxSegments = segments;
                 maxParameters = parameters;
+                
                 yield return tuple;
             }
         }
 
-        private static bool NoRoutesWereForTheRequestedMethod(IEnumerable<RouteCandidate> routesWithCorrectRequestMethod)
+        private ResolveResults Resolve(string path, NancyContext context, IRouteCache routeCache)
         {
-            return !routesWithCorrectRequestMethod.Any();
+            if (routeCache.IsEmpty())
+            {
+                context.Diagnostic.TraceLog.WriteLog(s => s.AppendLine("[DefaultRouteResolver] No routes available"));
+                return new ResolveResults
+                {
+                    Selected = new ResolveResult(new NotFoundRoute(context.Request.Method, path), DynamicDictionary.Empty, null, null)
+                };
+            }
+            
+            var routes =
+                routeCache.GetRouteCandidates();
+
+            // Condition
+            routes =
+                routes.Filter(context, "Invalid condition", (ctx, route) =>{
+                    var validCondition =
+                        ((route.Item3.Condition == null) || (route.Item3.Condition(ctx)));
+
+                    return new Tuple<bool, RouteCandidate>(
+                        validCondition,
+                        route
+                    );
+                });
+
+            if (!routes.Item1.Any())
+            {
+                context.Diagnostic.TraceLog.WriteLog(s => s.AppendLine("[DefaultRouteResolver] No route had a valid condition"));
+                return new ResolveResults
+                {
+                    Selected = new ResolveResult(new NotFoundRoute(context.Request.Method, path), DynamicDictionary.Empty, null, null),
+                    Rejected = routes.Item2
+                };
+            }
+
+            // Path
+            routes =
+                routes.Filter(context, "Path did not match", (ctx, route) => {
+                    var validationResult = 
+                        this.routePatternMatcher.Match(path, route.Item3.Path);
+
+                    var routeToReturn =
+                        (validationResult.IsMatch) ? new RouteCandidate(route.Item1, route.Item2, route.Item3, validationResult) : route;
+
+                    return new Tuple<bool, RouteCandidate>(
+                        validationResult.IsMatch,
+                        routeToReturn
+                    );
+                });
+
+            if (!routes.Item1.Any())
+            {
+                context.Diagnostic.TraceLog.WriteLog(s => s.AppendLine("[DefaultRouteResolver] No route matched the requested path"));
+                return new ResolveResults
+                {
+                    Selected = new ResolveResult(new NotFoundRoute(context.Request.Method, path), DynamicDictionary.Empty, null, null),
+                    Rejected = routes.Item2
+                };
+            }
+
+            // Method
+            routes =
+                routes.Filter(context, "Request method did not match", (ctx, route) =>{
+                    var routeMethod = 
+                        route.Item3.Method.ToUpperInvariant();
+
+                    var requestMethod = 
+                        ctx.Request.Method.ToUpperInvariant();
+                    
+                    var methodIsValid =
+                        routeMethod.Equals(requestMethod) || (routeMethod.Equals("GET") && requestMethod.Equals("HEAD"));
+
+                    return new Tuple<bool, RouteCandidate>(
+                        methodIsValid,
+                        route
+                    );
+                });
+
+            if (!routes.Item1.Any())
+            {
+                var allowedMethods = routes.Item1.Select(x => x.Item3.Method);
+                context.Diagnostic.TraceLog.WriteLog(s => s.AppendLine("[DefaultRouteResolver] Route Matched But Method Not Allowed"));
+                return new ResolveResults
+                {
+                    Selected = new ResolveResult(new MethodNotAllowedRoute(path, context.Request.Method, allowedMethods), DynamicDictionary.Empty, null, null),
+                    Rejected = routes.Item2
+                };
+            }
+
+            // Exact match
+            var exactMatchResults =
+                routes.Filter(context, "No exact match", (ctx, route) =>{
+                    var routeIsExactMatch =
+                        !route.Item4.Parameters.GetDynamicMemberNames().Any();
+
+                    return new Tuple<bool, RouteCandidate>(
+                        routeIsExactMatch,
+                        route
+                    );
+                });
+
+            if (exactMatchResults.Item1.Any())
+            {
+                context.Diagnostic.TraceLog.WriteLog(s => s.AppendLine("[DefaultRouteResolver] Found exact match route"));
+                return new ResolveResults
+                {
+                    Selected = this.CreateRouteAndParametersFromMatch(context, exactMatchResults.Item1.First()),
+                    Rejected = exactMatchResults.Item2
+                };
+            }
+
+            // First match out of multiple candidates
+            var selected =
+                GetTopRouteMatchesNew(routes).First();
+
+            context.Diagnostic.TraceLog.WriteLog(s => s.AppendLine("[DefaultRouteResolver] Selected best match"));
+            return new ResolveResults
+            {
+                Selected = this.CreateRouteAndParametersFromMatch(context, selected),
+                Rejected = exactMatchResults.Item2
+            };
         }
 
-        private static IEnumerable<RouteCandidate> GetRoutesWithCorrectRequestMethod(Request request, IEnumerable<RouteCandidate> routesThatMatchRequestedPath)
+        private class ResolveResults
         {
-            return from route in routesThatMatchRequestedPath
-                   let routeMethod = route.Item3.Method.ToUpperInvariant()
-                   let requestMethod = request.Method.ToUpperInvariant()
-                   where routeMethod.Equals(requestMethod) || (routeMethod.Equals("GET") && requestMethod.Equals("HEAD"))                    
-                   select route;
+            public ResolveResults()
+            {
+                this.Rejected = new Dictionary<string, List<RouteCandidate>>();
+            }
+
+            public ResolveResult Selected { get; set; }
+
+            public Dictionary<string, List<RouteCandidate>> Rejected { get; set; }
+        }
+    }
+
+    public static class RouteCandidateExtensions
+    {
+        public static Tuple<List<RouteCandidate>, Dictionary<string, List<RouteCandidate>>> GetRouteCandidates(this IRouteCache cache)
+        {
+            var result = new Tuple<List<RouteCandidate>, Dictionary<string, List<RouteCandidate>>>(
+                new List<RouteCandidate>(),
+                new Dictionary<string, List<RouteCandidate>>()
+            );
+
+            foreach (var cacheEntry in cache)
+            {
+                foreach (var candidate in cacheEntry.Value.Select(cacheEntryRoutes => new RouteCandidate(cacheEntry.Key, cacheEntryRoutes.Item1, cacheEntryRoutes.Item2, null)))
+                {
+                    result.Item1.Add(candidate);
+                }
+            }
+
+            return result;
         }
 
-        private static bool NoRoutesWereAbleToBeMatchedInRouteCache(IEnumerable<RouteCandidate> routesThatMatchRequestedPath)
+        public static Tuple<List<RouteCandidate>, Dictionary<string, List<RouteCandidate>>> Filter(this Tuple<List<RouteCandidate>, Dictionary<string, List<RouteCandidate>>> routes, NancyContext context, string rejectReason, Func<NancyContext, RouteCandidate, Tuple<bool, RouteCandidate>> condition)
         {
-            return !routesThatMatchRequestedPath.Any();
-        }
+            var result = new Tuple<List<RouteCandidate>, Dictionary<string, List<RouteCandidate>>>(
+                new List<RouteCandidate>(),
+                routes.Item2
+            );
 
-        private IEnumerable<RouteCandidate> GetRoutesThatMatchRequestedPath(IRouteCache routeCache, NancyContext context)
-        {
-            return from cacheEntry in routeCache
-                   from cacheEntryRoutes in cacheEntry.Value
-                   let routeIndex = cacheEntryRoutes.Item1
-                   let routeDescription = cacheEntryRoutes.Item2
-                   where ((routeDescription.Condition == null) || (routeDescription.Condition(context)))
-                   let result = this.routePatternMatcher.Match(context.Request.Path, routeDescription.Path)
-                   where result.IsMatch
-                   select new RouteCandidate(cacheEntry.Key, routeIndex, routeDescription, result);
+            foreach (var conditionResult in routes.Item1.Select(route => condition.Invoke(context, route)))
+            {
+                if (conditionResult.Item1)
+                {
+                    result.Item1.Add(conditionResult.Item2);
+                }
+                else
+                {
+                    if (!result.Item2.ContainsKey(rejectReason))
+                    {
+                        result.Item2.Add(rejectReason, new List<RouteCandidate>());
+                    }
+
+                    result.Item2[rejectReason].Add(conditionResult.Item2);
+                }
+            }
+
+            return result;
         }
     }
 }
