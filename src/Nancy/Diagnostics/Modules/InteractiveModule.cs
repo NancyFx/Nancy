e397cc6d6d5d494dcb7a59cac35aed4819916d60
Andreas Hakansson <andreas@selfinflicted.org>
2011-12-19 09:58:08 +0100

    Started adding info about which providers that were located

--- src/Nancy/Diagnostics/Modules/InteractiveModule.cs
+++ src/Nancy/Diagnostics/Modules/InteractiveModule.cs
@@ -23,7 +23,7 @@
                 {
                     var providers = this.interactiveDiagnostics
                                         .AvailableDiagnostics
-                                        .Select(p => new { p.Name, p.Description })
+                                        .Select(p => new { p.Name, p.Description, Type = p.GetType().Name, p.GetType().Namespace, Assembly = p.GetType().Assembly.GetName().Name })
                                         .ToArray();
 
                     return Response.AsJson(providers);
