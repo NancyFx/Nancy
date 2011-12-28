34538c23363c853f846fb396c646197887aa0f1d
Andreas Hakansson <andreas@selfinflicted.org>
2011-12-28 15:10:34 +0100

    Started setting up Fluent Validation tests

---
+++ src/Nancy.Validation.FluentValidation.Tests/EmailAdapterFixture.cs
@@ -0,0 +1,22 @@
+﻿namespace Nancy.Validation.FluentValidation.Tests
+{
+    using global::FluentValidation.Internal;
+    using global::FluentValidation.Validators;
+
+    public class EmailAdapterFixture
+    {
+        public EmailAdapterFixture()
+        {
+            //var member =
+            //    typeof (ClassUnderTest).GetProperty("Email");
+
+            //var rule = 
+            //    new PropertyRule(member, )
+        }
+
+        private class ClassUnderTest
+        {
+            public string Email { get; set; }
+        }
+    }
+}
