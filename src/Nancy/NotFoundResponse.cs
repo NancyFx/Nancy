12fd2bf8b794f016e28a9f62a04d31d790ff2fbf
Steven Robbins <ste.robbins@gmail.com>
2011-12-30 08:25:43 +0000

    Fixed not found response

--- src/Nancy/NotFoundResponse.cs
+++ src/Nancy/NotFoundResponse.cs
@@ -1,12 +1,9 @@
 ﻿namespace Nancy
 {
-    using System.Net;
-
     public class NotFoundResponse : Response
     {
         public NotFoundResponse()
         {
-            this.Contents = GetStringContents(string.Empty);
             this.ContentType = "text/html";
             this.StatusCode = HttpStatusCode.NotFound;
         }
