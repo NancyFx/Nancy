b6dd6352abe17c01ca962f602b45d84f6b76840d
Steven Robbins <ste.robbins@gmail.com>
2012-01-07 21:15:47 +0000

    Fix for #428

--- src/Nancy/Security/Csrf.cs
+++ src/Nancy/Security/Csrf.cs
@@ -1,4 +1,6 @@
-﻿namespace Nancy.Security
+﻿using Nancy.Bootstrapper;
+
+namespace Nancy.Security
 {
     using System;
     using Cookies;
@@ -10,6 +12,63 @@
     /// </summary>
     public static class Csrf
     {
+        private const string CsrfHookName = "CsrfPostHook";
+
+        /// <summary>
+        /// Enables Csrf token generation.
+        /// This is enabled automatically so there should be no reason to call this manually.
+        /// </summary>
+        /// <param name="pipelines">Application pipelines</param>
+        public static void Enable(IPipelines pipelines)
+        {
+            var postHook = new PipelineItem<Action<NancyContext>>(
+                CsrfHookName,
+                context =>
+                {
+                    if (context.Response == null || context.Response.Cookies == null)
+                    {
+                        return;
+                    }
+
+                    if (context.Items.ContainsKey(CsrfToken.DEFAULT_CSRF_KEY))
+                    {
+                        context.Response.Cookies.Add(new NancyCookie(CsrfToken.DEFAULT_CSRF_KEY,
+                                                                     (string)context.Items[CsrfToken.DEFAULT_CSRF_KEY],
+                                                                     true));
+                        return;
+                    }
+
+                    if (context.Request.Cookies.ContainsKey(CsrfToken.DEFAULT_CSRF_KEY))
+                    {
+                        context.Items[CsrfToken.DEFAULT_CSRF_KEY] =
+                            HttpUtility.UrlDecode(context.Request.Cookies[CsrfToken.DEFAULT_CSRF_KEY]);
+                        return;
+                    }
+
+                    var token = new CsrfToken
+                    {
+                        CreatedDate = DateTime.Now,
+                    };
+                    token.CreateRandomBytes();
+                    token.CreateHmac(CsrfStartup.CryptographyConfiguration.HmacProvider);
+                    var tokenString = CsrfStartup.ObjectSerializer.Serialize(token);
+
+                    context.Items[CsrfToken.DEFAULT_CSRF_KEY] = tokenString;
+                    context.Response.Cookies.Add(new NancyCookie(CsrfToken.DEFAULT_CSRF_KEY, tokenString, true));
+                });
+
+            pipelines.AfterRequest.AddItemToEndOfPipeline(postHook);
+        }
+
+        /// <summary>
+        /// Disable csrf token generation
+        /// </summary>
+        /// <param name="pipelines">Application pipelines</param>
+        public static void Disable(IPipelines pipelines)
+        {
+            pipelines.AfterRequest.RemoveByName(CsrfHookName);
+        }
+
         /// <summary>
         /// Creates a new csrf token for this response with an optional salt.
         /// Only necessary if a particular route requires a new token for each request.
