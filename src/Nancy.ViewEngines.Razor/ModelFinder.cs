8386150170a41271fba5611bc33cb3c0d2583280
Andreas Hakansson <andreas@selfinflicted.org>
2012-01-09 11:14:57 +0100

    Added missing XML comments

--- src/Nancy.ViewEngines.Razor/ModelFinder.cs
+++ src/Nancy.ViewEngines.Razor/ModelFinder.cs
@@ -1,10 +1,17 @@
 ﻿namespace Nancy.ViewEngines.Razor
 {
     using System.Web.Razor.Parser;
+    using System.Web.Razor.Parser.SyntaxTree;
 
+    /// <summary>
+    /// Razor parser visitor to retrieve the name of the model type.
+    /// </summary>
     public class ModelFinder : ParserVisitor
     {
-
+        /// <summary>
+        /// Visits the current Razor parser span and looks for the model.
+        /// </summary>
+        /// <param name="span">The <see cref="Span"/> to visit.</param>
         public override void VisitSpan(System.Web.Razor.Parser.SyntaxTree.Span span)
         {
             var modelSpan = span as ModelSpan;
@@ -17,6 +24,10 @@
             this.ModelTypeName = modelSpan.ModelTypeName;
         }
 
+        /// <summary>
+        /// Gets or sets the name of the model type.
+        /// </summary>
+        /// <value>A <see cref="string"/> containing the name of the model.</value>
         public string ModelTypeName { get; set; }
     }
 }
