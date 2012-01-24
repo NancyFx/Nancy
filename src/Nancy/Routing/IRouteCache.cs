c51c9cb53810ce8a4998b599f06eecd2ccc32782
Andreas Hakansson <andreas@selfinflicted.org>
2012-01-24 23:16:18 +0100

    Added missing xml comments

--- src/Nancy/Routing/IRouteCache.cs
+++ src/Nancy/Routing/IRouteCache.cs
@@ -8,6 +8,10 @@ namespace Nancy.Routing
     /// </summary>
     public interface IRouteCache : IDictionary<string, List<Tuple<int, RouteDescription>>>
     {
+        /// <summary>
+        /// Gets a boolean value that indicates of the cache is empty or not.
+        /// </summary>
+        /// <returns><see langword="true"/> if the cache is empty, otherwise <see langword="false"/>.</returns>
         bool IsEmpty();
     }
 }
--- src/Nancy/Routing/RouteCache.cs
+++ src/Nancy/Routing/RouteCache.cs
@@ -5,10 +5,19 @@
     using Nancy.Bootstrapper;
     using System;
 
+    /// <summary>
+    /// Caches information about all the available routes that was discovered by the bootstrapper.
+    /// </summary>
     public class RouteCache : Dictionary<string, List<Tuple<int, RouteDescription>>>, IRouteCache
     {
         private readonly IModuleKeyGenerator moduleKeyGenerator;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RouteCache"/> class.
+        /// </summary>
+        /// <param name="moduleCatalog">The <see cref="INancyModuleCatalog"/> that should be used by the cache.</param>
+        /// <param name="moduleKeyGenerator">The <see cref="IModuleKeyGenerator"/> used to generate module keys.</param>
+        /// <param name="contextFactory">The <see cref="INancyContextFactory"/> that should be used to create a context instance.</param>
         public RouteCache(INancyModuleCatalog moduleCatalog, IModuleKeyGenerator moduleKeyGenerator, INancyContextFactory contextFactory)
         {
             this.moduleKeyGenerator = moduleKeyGenerator;
@@ -17,6 +26,15 @@
             {
                 this.BuildCache(moduleCatalog.GetAllModules(context));
             }
+        }
+
+        /// <summary>
+        /// Gets a boolean value that indicates of the cache is empty or not.
+        /// </summary>
+        /// <returns><see langword="true"/> if the cache is empty, otherwise <see langword="false"/>.</returns>
+        public bool IsEmpty()
+        {
+            return this.Values.SelectMany(r => r).Count() == 0;
         }
 
         private void BuildCache(IEnumerable<NancyModule> modules)
@@ -38,11 +56,6 @@
             }
 
             this[moduleKey].AddRange(routes.Select((r, i) => new Tuple<int, RouteDescription>(i, r)));
-        }
-
-        public bool IsEmpty()
-        {
-            return this.Values.SelectMany(r => r).Count() == 0;
         }
     }
 }
