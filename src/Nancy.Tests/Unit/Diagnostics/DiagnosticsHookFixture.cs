41094e85bbb3aca8882b437a7a7daed0a03d5d36
Steven Robbins <ste.robbins@gmail.com>
2012-01-25 16:02:27 +0000

    Additional fixture work

--- src/Nancy.Tests/Unit/Diagnostics/DiagnosticsHookFixture.cs
+++ src/Nancy.Tests/Unit/Diagnostics/DiagnosticsHookFixture.cs
@@ -1,9 +1,13 @@
 ﻿namespace Nancy.Tests.Unit.Diagnostics
 {
     using System;
+    using System.Collections.Generic;
     using System.Linq;
+    using System.Security.Cryptography;
+    using System.Text;
 
     using Nancy.Cookies;
+    using Nancy.Cryptography;
     using Nancy.Diagnostics;
     using Nancy.Testing;
 
@@ -12,6 +16,16 @@
     public class DiagnosticsHookFixture
     {
         private const string DiagsCookieName = "__ncd";
+
+        private readonly CryptographyConfiguration cryptoConfig;
+
+        private readonly IObjectSerializer objectSerializer;
+
+        public DiagnosticsHookFixture()
+        {
+            this.cryptoConfig = CryptographyConfiguration.Default;
+            this.objectSerializer = new DefaultObjectSerializer();
+        }
 
         [Fact]
         public void Should_return_info_page_if_password_null()
@@ -58,15 +72,10 @@
 
             var result = browser.Get("/_Nancy", with =>
                 {
-                    with.Cookie(DiagsCookieName, this.GetAuthCookie("password"));
+                    with.Cookie(DiagsCookieName, this.GetSessionCookieValue("password"));
                 });
 
             result.Body["#notSureWhatToPutHereYet"].ShouldExistOnce();
-        }
-
-        private string GetAuthCookie(string password, DateTime? expiry = null)
-        {
-            throw new NotImplementedException();
         }
 
         [Fact]
@@ -78,7 +87,7 @@
 
             var result = browser.Get("/_Nancy", with =>
             {
-                with.Cookie(DiagsCookieName, this.GetAuthCookie("password", DateTime.Now.AddMinutes(-10)));
+                with.Cookie(DiagsCookieName, this.GetSessionCookieValue("password", DateTime.Now.AddMinutes(-10)));
             });
 
             result.Body["#login"].ShouldExistOnce();
@@ -93,7 +102,7 @@
 
             var result = browser.Get("/_Nancy", with =>
             {
-                with.Cookie(DiagsCookieName, this.GetAuthCookie("wrongPassword"));
+                with.Cookie(DiagsCookieName, this.GetSessionCookieValue("wrongPassword"));
             });
 
             result.Body["#login"].ShouldExistOnce();
@@ -140,7 +149,7 @@
             var expiryDate = DateTime.Now.AddMinutes(5);
             var result = browser.Get("/_Nancy", with =>
                 {
-                    with.Cookie(DiagsCookieName, this.GetAuthCookie("password", expiryDate));
+                    with.Cookie(DiagsCookieName, this.GetSessionCookieValue("password", expiryDate));
                 });
 
             result.Cookies.Any(c => c.Name == DiagsCookieName).ShouldBeTrue();
@@ -148,18 +157,66 @@
                 .Expiry.ShouldNotEqual(expiryDate);
         }
 
-        private DiagnosticsSessionCookie DecodeCookie(INancyCookie nancyCookie)
+        private string GetSessionCookieValue(string password, DateTime? expiry = null)
+        {
+            var salt = DiagnosticsSession.GenerateRandomSalt();
+            var hash = DiagnosticsSession.GenerateSaltedHash(password, salt);
+            var session = new DiagnosticsSession
+                {
+                    Hash = hash,
+                    Salt = salt,
+                    Expiry = expiry.HasValue ? expiry.Value : DateTime.Now.AddMinutes(15),
+                };
+
+            var serializedSession = this.objectSerializer.Serialize(session);
+        }
+
+        private DiagnosticsSession DecodeCookie(INancyCookie nancyCookie)
         {
             throw new NotImplementedException();
         }
     }
 
-    internal class DiagnosticsSessionCookie
+    internal class DiagnosticsSession
     {
-        public string Password { get; set; }
+        public byte[] Hash { get; set; }
 
         public byte[] Salt { get; set; }
 
         public DateTime Expiry { get; set; }
+
+        public static byte[] GenerateRandomSalt()
+        {
+            var provider = new RNGCryptoServiceProvider();
+
+            var buffer = new byte[32];
+            provider.GetBytes(buffer);
+
+            return buffer;
+        }
+
+        public static byte[] GenerateSaltedHash(byte[] plainText, byte[] salt)
+        {
+            var algorithm = new SHA256Managed();
+
+            var plainTextWithSaltBytes = new byte[plainText.Length + salt.Length];
+
+            for (var i = 0; i < plainText.Length; i++)
+            {
+                plainTextWithSaltBytes[i] = plainText[i];
+            }
+
+            for (var i = 0; i < salt.Length; i++)
+            {
+                plainTextWithSaltBytes[plainText.Length + i] = salt[i];
+            }
+
+            return algorithm.ComputeHash(plainTextWithSaltBytes);
+        }
+
+        public static byte[] GenerateSaltedHash(string plainText, byte[] salt)
+        {
+            return GenerateSaltedHash(Encoding.UTF8.GetBytes(plainText), salt);
+        }
     }
 }
