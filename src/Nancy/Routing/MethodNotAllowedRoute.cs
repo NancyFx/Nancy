eef9e7e1b78d14d05dba194c1dfdb69b4059cc62
Andreas Hakansson <andreas@selfinflicted.org>
2012-03-09 13:24:46 +0100

    Added missing XML comments to MethodNotAllowedRoute

--- src/Nancy/Routing/MethodNotAllowedRoute.cs
+++ src/Nancy/Routing/MethodNotAllowedRoute.cs
@@ -8,6 +8,13 @@
     /// <remarks>This is equal to sending back the 405 HTTP status code.</remarks>
     public class MethodNotAllowedRoute : Route
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MethodNotAllowedRoute"/> type, for the
+        /// specifed <paramref name="path"/>, <paramref name="method"/> and <paramref name="allowedMethods"/>.
+        /// </summary>
+        /// <param name="path">The path of the route.</param>
+        /// <param name="method">The HTTP method of the route.</param>
+        /// <param name="allowedMethods">The HTTP methods that can be used to invoke the route.</param>
         public MethodNotAllowedRoute(string path, string method, IEnumerable<string> allowedMethods)
             : base(method, path, null, x => CreateMethodNotAllowedResponse(allowedMethods))
         {
