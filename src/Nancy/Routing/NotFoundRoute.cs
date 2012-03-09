c010e59d91a8e3e4edeea814e067b94791a85cc9
Andreas Hakansson <andreas@selfinflicted.org>
2012-03-09 13:15:43 +0100

    Added missing XML comments to NotFoundRoute

--- src/Nancy/Routing/NotFoundRoute.cs
+++ src/Nancy/Routing/NotFoundRoute.cs
@@ -6,6 +6,12 @@
     /// <remarks>This is equal to sending back the 404 HTTP status code.</remarks>
     public class NotFoundRoute : Route
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NotFoundRoute"/> type, for the
+        /// specified <paramref name="path"/> and <paramref name="method"/>.
+        /// </summary>
+        /// <param name="method">The HTTP method of the route.</param>
+        /// <param name="path">The path of the route.</param>
         public NotFoundRoute(string method, string path)
             : base(method, path, null, x => new NotFoundResponse())
         {
