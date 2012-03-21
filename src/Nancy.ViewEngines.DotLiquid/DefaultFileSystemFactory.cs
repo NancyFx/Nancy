7dc97e231e107a0cbb8cdf4dcdcde98a0a724a53
Andreas Hakansson <andreas@selfinflicted.org>
2012-03-21 11:12:50 +0100

    Updated the DotLiquid engine
    * Added unit tests for the LiquidNancyFileSystem implementation and
    also updated it to use template locations
    
    * Removed tests from DotLiquidViewEngine that were testing the
    LiquidNancyFileSystem through the DotLiquidViewEngine implementation
    
    * Introduced IFileSystemFactory and DefaultFileSystemFactory which is
    used to create a filesystem for the provided context. Updated the
    DotLiquidViewEngine to take a ctor dependency on the interface and
    updated the Initialize method to use the factory
    
    * Added DotLiquidViewEngineStartup to make sure that the
    DefaultFileSystemFactory is registered in the container
    
    * Added unit test to ensure that the factory is used in the Initialize
    method of the engine
    
    * Added support to LiquidNancyFileSystem so the name of the template
    can .liquid extension, but still works without it. Also made it support
    both forward- and backslashed in the location path. Made all matching
    logic case-insensitive
    
    * Removed convention that stripped underscores from the template name. It
    made no sense to have it

---
+++ src/Nancy.ViewEngines.DotLiquid/DefaultFileSystemFactory.cs
@@ -0,0 +1,31 @@
+﻿namespace Nancy.ViewEngines.DotLiquid
+{
+    using Conventions;
+    using global::DotLiquid.FileSystems;
+
+    /// <summary>
+    /// Default implementation of the <see cref="IFileSystemFactory"/> interface.
+    /// </summary>
+    /// <remarks>This implementation always returns instances of the <see cref="LiquidNancyFileSystem"/> type.</remarks>
+    public class DefaultFileSystemFactory : IFileSystemFactory
+    {
+        private readonly ViewLocationConventions conventions;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DefaultFileSystemFactory"/> class
+        /// </summary>
+        public DefaultFileSystemFactory()
+        {
+        }
+
+        /// <summary>
+        /// Gets a <see cref="IFileSystem"/> instance for the provided <paramref name="context"/>.
+        /// </summary>
+        /// <param name="context">The context that the filesystem should be created for.</param>
+        /// <returns>An <see cref="IFileSystem"/> instance.</returns>
+        public IFileSystem GetFileSystem(ViewEngineStartupContext context)
+        {
+            return new LiquidNancyFileSystem(context);
+        }
+    }
+}
--- src/Nancy.ViewEngines.DotLiquid/LiquidNancyFileSystem.cs
+++ src/Nancy.ViewEngines.DotLiquid/LiquidNancyFileSystem.cs
@@ -1,22 +1,45 @@
 ﻿namespace Nancy.ViewEngines.DotLiquid
 {
+    using System;
+    using System.Text.RegularExpressions;
+    using global::DotLiquid;
+    using global::DotLiquid.FileSystems;
     using liquid = global::DotLiquid;    
-    using System.IO;
     using System.Linq;
 
-    public class LiquidNancyFileSystem : liquid.FileSystems.IFileSystem
+    /// <summary>
+    /// <see cref="IFileSystem"/> implementation around the Nancy templates.
+    /// </summary>
+    public class LiquidNancyFileSystem : IFileSystem
     {
+        private readonly Regex extensionExpression;
         private readonly ViewEngineStartupContext nancyContext;
 
-        public LiquidNancyFileSystem(ViewEngineStartupContext viewEngineStartupContext)
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LiquidNancyFileSystem"/> class,
+        /// with the provided <paramref name="context"/>.
+        /// </summary>
+        /// <param name="context">The context that the engine can operate in.</param>
+        public LiquidNancyFileSystem(ViewEngineStartupContext context)
         {
-            this.nancyContext = viewEngineStartupContext;
+            this.nancyContext = context;
+            this.extensionExpression = new Regex(".liquid", RegexOptions.IgnoreCase | RegexOptions.IgnoreCase);
         }
 
-        public string ReadTemplateFile(liquid.Context context, string templateName)
+        /// <summary>
+        /// Reads the content of the template specified by the <paramref name="templateName"/> parameter.
+        /// </summary>
+        /// <param name="context">The <see cref="Context"/> of the call.</param>
+        /// <param name="templateName">The name of the template to read.</param>
+        /// <exception cref="liquid.Exceptions.FileSystemException">The specified template could not be located.</exception>
+        /// <returns>The content of the template.</returns>
+        public string ReadTemplateFile(Context context, string templateName)
         {
-            // Find the template referenced by the passed templateName
-            ViewLocationResult viewLocation = nancyContext.ViewLocationResults.FirstOrDefault(v => ReplaceTroubleChars(v.Name) == ReplaceTroubleChars(templateName));
+            var neutralTemplateName =
+                this.GetNeutralTemplateName(templateName);
+
+            var viewLocation = nancyContext.ViewLocationResults
+                .FirstOrDefault(v => GetLocationQualifiedName(v).Equals(neutralTemplateName, StringComparison.OrdinalIgnoreCase));
 
             if (viewLocation != null)
             {
@@ -26,9 +49,22 @@
             throw new liquid.Exceptions.FileSystemException("Template file {0} not found", new[] { templateName });
         }
 
-        private string ReplaceTroubleChars(string templateName)
+        private string GetLocationQualifiedName(ViewLocationResult viewLocationResult)
         {
-            return templateName.Replace(@"""", "").Replace("'", "").Replace("_","");
+            return string.Concat(viewLocationResult.Location, "/", this.GetNeutralTemplateName(viewLocationResult.Name));
+        }
+
+        private string GetNeutralTemplateName(string templateName)
+        {
+            templateName = templateName
+                .Replace(@"""", "")
+                .Replace("'", "")
+                .Replace(@"\", "/");
+
+            templateName =
+                this.extensionExpression.Replace(templateName, string.Empty);
+
+            return templateName;
         }
     }
 }
