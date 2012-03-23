cbc5abc7513b53dd257ccc47fb72bd436f41fbdb
Francisco Ruiz A <frankra@gmail.com>
2012-03-23 21:06:13 +0000

    submitting changes again

--- src/Nancy.Testing/BrowserContextExtensions.cs
+++ src/Nancy.Testing/BrowserContextExtensions.cs
@@ -1,4 +1,6 @@
-﻿namespace Nancy.Testing
+﻿using Nancy.Authentication.Forms;
+
+namespace Nancy.Testing
 {
     using System;
     using System.Text;
@@ -123,6 +125,25 @@
         public static void AjaxRequest(this BrowserContext browserContext)
         {
             browserContext.Header("X-Requested-With", "XMLHttpRequest");
+        }
+
+        /// <summary>
+        /// Adds forms authentication cookie to the headers of the <see cref="Browser"/>.
+        /// </summary>
+        /// <param name="browserContext">The <see cref="BrowserContext"/> that the data should be added to.</param>
+        /// <param name="userId">The user identifier</param>
+        /// <param name="formsAuthenticationConfiguration">Current configuration.</param>
+        public static void FormsAuth(this BrowserContext browserContext, Guid userId, FormsAuthenticationConfiguration formsAuthenticationConfiguration)
+        {
+            var encryptedId = formsAuthenticationConfiguration.CryptographyConfiguration.EncryptionProvider.Encrypt(userId.ToString());
+
+            var hmacBytes = formsAuthenticationConfiguration.CryptographyConfiguration.HmacProvider.GenerateHmac(encryptedId);
+
+            var hmacString = Convert.ToBase64String(hmacBytes);
+
+            var cookieContents = String.Format("{1}{0}", encryptedId, hmacString);
+
+            Cookie(browserContext, FormsAuthentication.FormsAuthenticationCookieName, cookieContents);
         }
     }
 }
