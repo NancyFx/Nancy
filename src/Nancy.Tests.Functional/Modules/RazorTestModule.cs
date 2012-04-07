ec9fa692ca85d05e70c635815e62b4e3a2e8184c
Steven Robbins <ste.robbins@gmail.com>
2012-04-07 11:08:10 +0100

    Added functional test to test viewbag from razor views

---
+++ src/Nancy.Tests.Functional/Modules/RazorTestModule.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nancy.Tests.Functional.Modules
+{
+    public class RazorTestModule : NancyModule
+    {
+        public RazorTestModule()
+        {
+            Get["/razor-viewbag"] = _ =>
+                {
+                    this.ViewBag.Name = "Bob";
+
+                    return View["RazorPage"];
+                };
+        }
+    }
+}
