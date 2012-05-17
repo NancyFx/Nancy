cec7d08739baba7cd26285cb02c1b25efd7c9de3
Andreas Hakansson <andreas@selfinflicted.org>
2012-05-17 14:51:00 +0200

    Added NegotiatedResponse
    Will be used for responses that should have its format negotiated
    between the client and server

---
+++ src/Nancy/Responses/NegotiatedResponse.cs
@@ -0,0 +1,23 @@
+﻿namespace Nancy.Responses
+{
+    /// <summary>
+    /// Response that indicates that the response format should be negotiated between the client and the server.
+    /// </summary>
+    public class NegotiatedResponse : Response
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NegotiatedResponse"/> response for the
+        /// provided <paramref name="value"/>.
+        /// </summary>
+        /// <param name="value">The response value that should be negotiated.</param>
+        public NegotiatedResponse(dynamic value)
+        {
+            Value = value;
+        }
+
+        /// <summary>
+        /// Gets or sets the value that should be negotiated.
+        /// </summary>
+        public dynamic Value { get; set; }
+    }
+}
