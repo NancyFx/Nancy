c5e506304b2b21b65fd99e35e81f1d0150c5561c
Andreas Hakansson <andreas@selfinflicted.org>
2012-05-17 14:15:08 +0200

    Sorted out route invoker types
    - Added missing XML comments
    - Moved the DefaultRouteInvoker to its own file

--- src/Nancy/Routing/DefaultRouteInvoker.cs
+++ src/Nancy/Routing/DefaultRouteInvoker.cs
@@ -1,9 +1,9 @@
-﻿namespace Nancy.Routing
+namespace Nancy.Routing
 {
     /// <summary>
-    /// Defines the functionality for invoking a <see cref="Route"/> and returning a <see cref="Response"/>
+    /// Default route invoker implementation-
     /// </summary>
-    public interface IRouteInvoker
+    public class DefaultRouteInvoker : IRouteInvoker
     {
         /// <summary>
         /// Invokes the specified <paramref name="route"/> with the provided <paramref name="parameters"/>.
@@ -11,6 +11,12 @@
         /// <param name="route">The route that should be invoked.</param>
         /// <param name="parameters">The parameters that the route should be invoked with.</param>
         /// <returns>A <see cref="Response"/> intance that represents the result of the invoked route.</returns>
-        Response Invoke(Route route, DynamicDictionary parameters);
+        public Response Invoke(Route route, DynamicDictionary parameters)
+        {
+            var result =
+                route.Invoke(parameters);
+
+            return result;
+        }
     }
 }
