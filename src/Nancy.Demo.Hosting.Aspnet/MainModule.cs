4e6c907125ce60e600d63fd9dd625c79ede51896
Andreas Hakansson <andreas@selfinflicted.org>
2012-05-18 11:40:47 +0200

    Added PngSerializer as a Poof of Concept

--- src/Nancy.Demo.Hosting.Aspnet/MainModule.cs
+++ src/Nancy.Demo.Hosting.Aspnet/MainModule.cs
@@ -1,6 +1,10 @@
 namespace Nancy.Demo.Hosting.Aspnet
 {
     using System;
+    using System.Collections.Generic;
+    using System.Drawing;
+    using System.Drawing.Imaging;
+    using System.IO;
     using System.Linq;
     using Nancy.Demo.Hosting.Aspnet.Models;
     using Nancy.Routing;
@@ -191,6 +195,53 @@ namespace Nancy.Demo.Hosting.Aspnet
                 return View["FileUpload", new { Posted = fileDetails }];
             };
 
+        }
+    }
+
+    public class PngSerializer : ISerializer
+    {
+        private readonly IRootPathProvider rootPathProvider;
+
+        public PngSerializer(IRootPathProvider rootPathProvider)
+        {
+            this.rootPathProvider = rootPathProvider;
+        }
+
+        /// <summary>
+        /// Whether the serializer can serialize the content type
+        /// </summary>
+        /// <param name="contentType">Content type to serialise</param>
+        /// <returns>True if supported, false otherwise</returns>
+        public bool CanSerialize(string contentType)
+        {
+            return contentType.Equals("image/png", StringComparison.OrdinalIgnoreCase);
+        }
+
+        /// <summary>
+        /// Gets the list of extensions that the serializer can handle.
+        /// </summary>
+        /// <value>An <see cref="IEnumerable{T}"/> of extensions if any are available, otherwise an empty enumerable.</value>
+        public IEnumerable<string> Extensions
+        {
+            get { yield return "png"; }
+        }
+
+        /// <summary>
+        /// Serialize the given model with the given contentType
+        /// </summary>
+        /// <param name="contentType">Content type to serialize into</param>
+        /// <param name="model">Model to serialize</param>
+        /// <param name="outputStream">Output stream to serialize to</param>
+        /// <returns>Serialised object</returns>
+        public void Serialize<TModel>(string contentType, TModel model, Stream outputStream)
+        {
+            var path =
+                Path.Combine(this.rootPathProvider.GetRootPath(), "content/face.png");
+
+            var face = 
+                Image.FromFile(path);
+
+            face.Save(outputStream, ImageFormat.Png);
         }
     }
 }
