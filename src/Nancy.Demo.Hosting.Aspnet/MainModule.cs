d8a43c98c253b65012e6cef046b96caa7e36ac09
Andreas Hakansson <andreas@selfinflicted.org>
2012-07-12 21:55:53 +0200

    Updated demo app to use negotiation syntax

--- src/Nancy.Demo.Hosting.Aspnet/MainModule.cs
+++ src/Nancy.Demo.Hosting.Aspnet/MainModule.cs
@@ -14,12 +14,12 @@ namespace Nancy.Demo.Hosting.Aspnet
                 return View["routes", routeCacheProvider.GetCache()];
             };
 
-            Get["/negotiated"] = parameters =>{
-
+            Get["/negotiated"] = parameters => {
                 return Negotiate
                     .WithModel(new RatPack {FirstName = "Nancy "})
                     .WithMediaRangeModel("text/html", new RatPack {FirstName = "Nancy fancy pants"})
-                    .WithView("negotiatedview");
+                    .WithView("negotiatedview")
+                    .WithHeader("X-Custom", "SomeValue");
             };
 
             Get["/filtered", r => true] = x => {
