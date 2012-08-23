c6fae38f14a872f569551ed052f769176d3de829
Prabir Shrestha <mail@prabir.me>
2012-08-22 22:36:11 -0300

    Update src/Nancy.Hosting.Owin/TaskHelpers.cs
    updated TaskHelpers.cs license text

--- src/Nancy.Hosting.Owin/TaskHelpers.cs
+++ src/Nancy.Hosting.Owin/TaskHelpers.cs
@@ -1,6 +1,4 @@
-﻿// Copyright (c) Microsoft Corporation. All rights reserved. See License.txt in the project root for license information.
-
-
+﻿// Copyright (c) Microsoft Corporation. All rights reserved. Apache License 2.0
 
 namespace System.Threading.Tasks
 {
