650d1b0273827ca1e0175fdaf4852732620af9f4
Svetoslav Milenov <sloncho@gmail.com>
2012-09-20 08:44:04 -0500

    XML comments added to ModelBindingException class

--- src/Nancy/ModelBinding/ModelBindingException.cs
+++ src/Nancy/ModelBinding/ModelBindingException.cs
@@ -11,26 +11,44 @@ namespace Nancy.ModelBinding
         private const string ExceptionMessage = "Unable to bind to type: {0}";
         private const string PropertyMessage = "; Property: {0}";
 
+        /// <summary>
+        /// Gets the model property name, which caused the exception
+        /// </summary>
         public virtual string PropertyName { get; private set; }
 
+        /// <summary>
+        /// Gets the model type, which caused the exception
+        /// </summary>
         public virtual Type BoundType { get; private set; }
 
+        /// <summary>
+        /// Gets a message with the model type and property name, which caused the exception
+        /// </summary>
         public override string Message
         {
-            get { var message = String.Format(ExceptionMessage, BoundType);
-            if (PropertyName != null)
+            get
             {
-                message += String.Format(PropertyMessage, PropertyName);
-            }
+                var message = String.Format(ExceptionMessage, BoundType);
+                if (PropertyName != null)
+                {
+                    message += String.Format(PropertyMessage, PropertyName);
+                }
                 return message;
             }
         }
 
+        /// <summary>
+        /// Initializes a new instance of the ModelBindingException class with a specified model type,
+        /// property name and the original exception, which caused the problem
+        /// </summary>
+        /// <param name="boundType">the model type to bind to</param>
+        /// <param name="propertyName">the property name, which failed to bind</param>
+        /// <param name="innerException">the original exception, thrown while binding the property</param>
         public ModelBindingException(Type boundType, string propertyName = null, Exception innerException = null)
             : base(null, innerException)
         {
-            PropertyName = propertyName;
-            BoundType = boundType;
+            this.PropertyName = propertyName;
+            this.BoundType = boundType;
         }
 
         protected ModelBindingException(SerializationInfo info, StreamingContext context)
