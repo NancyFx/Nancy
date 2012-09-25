c0dc5b1b4bfe0429fe66051d3d4696b6fa74ce58
Nico Francois <nico.francois@dot-i.com>
2012-09-25 10:20:12 +0200

    Fixed formatting to Visual Studio defaults (indents with 4 spaces).

--- src/Nancy.Hosting.Self.Tests/IsCaseInstensitiveBaseOfFixture.cs
+++ src/Nancy.Hosting.Self.Tests/IsCaseInstensitiveBaseOfFixture.cs
@@ -4,171 +4,171 @@ namespace Nancy.Hosting.Self.Tests
     using Xunit;
     using Nancy.Tests;
 
-	public class IsCaseInstensitiveBaseOfFixture
-	{
-		private readonly Uri baseUri = new Uri("http://host/path/path/file");
-		private readonly Uri baseSlashUri = new Uri("http://host/path/path/");
+    public class IsCaseInstensitiveBaseOfFixture
+    {
+        private readonly Uri baseUri = new Uri("http://host/path/path/file");
+        private readonly Uri baseSlashUri = new Uri("http://host/path/path/");
         private readonly Uri baseLocalHostUri = new Uri("http://localhost/path/path/");
 
-		[Fact]
-		public void url_should_be_base_of_sub_directory()
-		{
-            // Given, When
-			var isBaseOf = baseUri.IsCaseInsensitiveBaseOf(new Uri("http://host/path/path/file/"));
-
-            // Then
-            isBaseOf.ShouldBeTrue();
-		}
-
-		[Fact]
-		public void url_should_be_base_of_sub_path_with_fragment()
-		{
-            // Given, When
-            var isBaseOf = baseUri.IsCaseInsensitiveBaseOf(new Uri("http://host/path/path/file#fragment"));
-
-            // Then
-            isBaseOf.ShouldBeTrue();
-		}
-
-		[Fact]
-		public void url_should_be_base_of_path_with_more_dirs()
-		{
-            // Given, When
-            var isBaseOf = baseUri.IsCaseInsensitiveBaseOf(new Uri("http://host/path/path/file/MoreDir/"));
-
-            // Then
-            isBaseOf.ShouldBeTrue();
-		}
-
-		[Fact]
-		public void url_should_be_base_of_sub_path_with_file_and_query()
-		{
-            // Given, When
-            var isBaseOf = baseUri.IsCaseInsensitiveBaseOf(new Uri("http://host/path/path/file/OtherFile?Query"));
-
-            // Then
-            isBaseOf.ShouldBeTrue();
-		}
-
-		[Fact]
-		public void url_should_be_base_of_path_with_extra_slash()
-		{
+        [Fact]
+        public void url_should_be_base_of_sub_directory()
+        {
             // Given, When
             var isBaseOf = baseUri.IsCaseInsensitiveBaseOf(new Uri("http://host/path/path/file/"));
 
             // Then
             isBaseOf.ShouldBeTrue();
-		}
+        }
 
-		[Fact]
-		public void url_should_be_base_of_sub_file()
-		{
+        [Fact]
+        public void url_should_be_base_of_sub_path_with_fragment()
+        {
+            // Given, When
+            var isBaseOf = baseUri.IsCaseInsensitiveBaseOf(new Uri("http://host/path/path/file#fragment"));
+
+            // Then
+            isBaseOf.ShouldBeTrue();
+        }
+
+        [Fact]
+        public void url_should_be_base_of_path_with_more_dirs()
+        {
+            // Given, When
+            var isBaseOf = baseUri.IsCaseInsensitiveBaseOf(new Uri("http://host/path/path/file/MoreDir/"));
+
+            // Then
+            isBaseOf.ShouldBeTrue();
+        }
+
+        [Fact]
+        public void url_should_be_base_of_sub_path_with_file_and_query()
+        {
+            // Given, When
+            var isBaseOf = baseUri.IsCaseInsensitiveBaseOf(new Uri("http://host/path/path/file/OtherFile?Query"));
+
+            // Then
+            isBaseOf.ShouldBeTrue();
+        }
+
+        [Fact]
+        public void url_should_be_base_of_path_with_extra_slash()
+        {
+            // Given, When
+            var isBaseOf = baseUri.IsCaseInsensitiveBaseOf(new Uri("http://host/path/path/file/"));
+
+            // Then
+            isBaseOf.ShouldBeTrue();
+        }
+
+        [Fact]
+        public void url_should_be_base_of_sub_file()
+        {
             // Given, When
             var isBaseOf = baseUri.IsCaseInsensitiveBaseOf(new Uri("http://host/path/path/file"));
 
             // Then
             isBaseOf.ShouldBeTrue();
-		}
+        }
 
-		[Fact]
-		public void url_should_not_be_base_of_other_scheme()
-		{
+        [Fact]
+        public void url_should_not_be_base_of_other_scheme()
+        {
             // Given, When
             var isBaseOf = baseUri.IsCaseInsensitiveBaseOf(new Uri("https://host/path/path/file"));
 
             // Then
             isBaseOf.ShouldBeFalse();
-		}
+        }
 
-		[Fact]
-		public void url_should_not_be_base_of_other_host()
-		{
+        [Fact]
+        public void url_should_not_be_base_of_other_host()
+        {
             // Given, When
             var isBaseOf = baseUri.IsCaseInsensitiveBaseOf(new Uri("http://otherhost/path/path/file"));
 
             // Then
             isBaseOf.ShouldBeFalse();
-		}
+        }
 
-		[Fact]
-		public void url_should_not_be_base_of_other_port()
-		{
+        [Fact]
+        public void url_should_not_be_base_of_other_port()
+        {
             // Given, When
             var isBaseOf = baseUri.IsCaseInsensitiveBaseOf(new Uri("http://otherhost:8080/path/path/file"));
 
             // Then
             isBaseOf.ShouldBeFalse();
-		}
+        }
 
-		[Fact]
-		public void url_should_be_base_of_host_with_different_casing()
-		{
+        [Fact]
+        public void url_should_be_base_of_host_with_different_casing()
+        {
             // Given, When
             var isBaseOf = baseUri.IsCaseInsensitiveBaseOf(new Uri("http://Host/path/path/file"));
 
             // Then
             isBaseOf.ShouldBeTrue();
-		}
+        }
 
-		[Fact]
-		public void url_should_be_base_of_exact_path_without_trailing_slash()
-		{
+        [Fact]
+        public void url_should_be_base_of_exact_path_without_trailing_slash()
+        {
             // Given, When
             var isBaseOf = baseSlashUri.IsCaseInsensitiveBaseOf(new Uri("http://host/path/path"));
 
             // Then
             isBaseOf.ShouldBeTrue();
-		}
+        }
 
-		[Fact]
-		public void url_should_be_base_of_exact_path_without_trailing_slash_with_query()
-		{
+        [Fact]
+        public void url_should_be_base_of_exact_path_without_trailing_slash_with_query()
+        {
             // Given, When
             var isBaseOf = baseSlashUri.IsCaseInsensitiveBaseOf(new Uri("http://host/path/path?query"));
 
             // Then
             isBaseOf.ShouldBeTrue();
-		}
+        }
 
-		[Fact]
-		public void url_should_be_base_of_exact_path_without_trailing_slash_with_fragment()
-		{
+        [Fact]
+        public void url_should_be_base_of_exact_path_without_trailing_slash_with_fragment()
+        {
             // Given, When
             var isBaseOf = baseSlashUri.IsCaseInsensitiveBaseOf(new Uri("http://host/path/path#Fragment"));
 
             // Then
             isBaseOf.ShouldBeTrue();
-		}
+        }
 
-		[Fact]
-		public void url_should_not_be_base_of_other_path()
-		{
+        [Fact]
+        public void url_should_not_be_base_of_other_path()
+        {
             // Given, When
             var isBaseOf = baseSlashUri.IsCaseInsensitiveBaseOf(new Uri("http://host/path/path2/"));
 
             // Then
             isBaseOf.ShouldBeFalse();
-		}
+        }
 
-		[Fact]
-		public void url_should_be_base_of_same_path_with_different_host_casing()
-		{
+        [Fact]
+        public void url_should_be_base_of_same_path_with_different_host_casing()
+        {
             // Given, When
             var isBaseOf = baseSlashUri.IsCaseInsensitiveBaseOf(new Uri("http://Host/path/path/"));
 
             // Then
             isBaseOf.ShouldBeTrue();
-		}
-        
-		[Fact]
-		public void url_should_be_base_of_same_path_with_different_path_casing()
-		{
+        }
+
+        [Fact]
+        public void url_should_be_base_of_same_path_with_different_path_casing()
+        {
             // Given, When
             var isBaseOf = baseSlashUri.IsCaseInsensitiveBaseOf(new Uri("http://host/Path/PATH/"));
 
             // Then
-			isBaseOf.ShouldBeTrue();
-		}
+            isBaseOf.ShouldBeTrue();
+        }
 
         [Fact]
         public void url_should_be_base_of_same_path_with_different_host_using_localhost_wildcard()
@@ -179,5 +179,5 @@ namespace Nancy.Hosting.Self.Tests
             // Then
             isBaseOf.ShouldBeTrue();
         }
-	}
+    }
 }
--- src/Nancy.Hosting.Self/UriExtensions.cs
+++ src/Nancy.Hosting.Self/UriExtensions.cs
@@ -7,26 +7,26 @@ namespace Nancy.Hosting.Self
     /// <summary>
     /// Extension methods for working with <see cref="Uri"/> instances.
     /// </summary>
-	public static class UriExtensions
-	{
-		public static bool IsCaseInsensitiveBaseOf(this Uri source, Uri value)
-		{
-			var uriComponents = source.Host == "localhost" ? (UriComponents.Port | UriComponents.Scheme) : (UriComponents.HostAndPort | UriComponents.Scheme);
-			if (Uri.Compare(source, value, uriComponents, UriFormat.Unescaped, StringComparison.InvariantCultureIgnoreCase) != 0)
-			{
-			    return false;
-			}
+    public static class UriExtensions
+    {
+        public static bool IsCaseInsensitiveBaseOf(this Uri source, Uri value)
+        {
+            var uriComponents = source.Host == "localhost" ? (UriComponents.Port | UriComponents.Scheme) : (UriComponents.HostAndPort | UriComponents.Scheme);
+            if (Uri.Compare(source, value, uriComponents, UriFormat.Unescaped, StringComparison.InvariantCultureIgnoreCase) != 0)
+            {
+                return false;
+            }
 
-			var sourceSegments = source.Segments;
-			var valueSegments = value.Segments;
+            var sourceSegments = source.Segments;
+            var valueSegments = value.Segments;
 
-			return sourceSegments.ZipCompare( valueSegments, (s1, s2) => s1.Length == 0 || SegmentEquals(s1, s2));
-		}
+            return sourceSegments.ZipCompare(valueSegments, (s1, s2) => s1.Length == 0 || SegmentEquals(s1, s2));
+        }
 
         public static string MakeAppLocalPath(this Uri appBaseUri, Uri fullUri)
-	    {
-	        return string.Concat("/", appBaseUri.Segments.ZipFill(fullUri.Segments,(x, y) => x != null && AppendSlashIfNeeded(x) == AppendSlashIfNeeded(y) ? null : y).Join());
-	    }
+        {
+            return string.Concat("/", appBaseUri.Segments.ZipFill(fullUri.Segments, (x, y) => x != null && AppendSlashIfNeeded(x) == AppendSlashIfNeeded(y) ? null : y).Join());
+        }
 
         private static string AppendSlashIfNeeded(string segment)
         {
@@ -39,9 +39,9 @@ namespace Nancy.Hosting.Self
         }
 
         private static bool SegmentEquals(string segment1, string segment2)
-		{
-			return String.Equals(AppendSlashIfNeeded(segment1), AppendSlashIfNeeded(segment2), StringComparison.InvariantCultureIgnoreCase);
-		}
+        {
+            return String.Equals(AppendSlashIfNeeded(segment1), AppendSlashIfNeeded(segment2), StringComparison.InvariantCultureIgnoreCase);
+        }
 
         private static bool ZipCompare(this IEnumerable<string> source1, IEnumerable<string> source2, Func<string, string, bool> comparison)
         {
@@ -78,52 +78,52 @@ namespace Nancy.Hosting.Self
 
             return true;
         }
-        
+
         private static IEnumerable<string> ZipFill(this IEnumerable<string> source1, IEnumerable<string> source2, Func<string, string, string> selector)
-		{
-			using (var enumerator1 = source1.GetEnumerator())
-			{
+        {
+            using (var enumerator1 = source1.GetEnumerator())
+            {
                 using (var enumerator2 = source2.GetEnumerator())
-			    {
-				    var has1 = enumerator1.MoveNext();
-				    var has2 = enumerator2.MoveNext();
+                {
+                    var has1 = enumerator1.MoveNext();
+                    var has2 = enumerator2.MoveNext();
 
-				    while (has1 || has2)
-				    {
-					    var value1 = has1 ? enumerator1.Current : null;
-					    var value2 = has2 ? enumerator2.Current : null;
-					    var value = selector(value1, value2);
+                    while (has1 || has2)
+                    {
+                        var value1 = has1 ? enumerator1.Current : null;
+                        var value2 = has2 ? enumerator2.Current : null;
+                        var value = selector(value1, value2);
 
-					    if (value != null)
-					    {
-					        yield return value;
-					    }
+                        if (value != null)
+                        {
+                            yield return value;
+                        }
 
-					    if (has1)
-					    {
-					        has1 = enumerator1.MoveNext();
-					    }
+                        if (has1)
+                        {
+                            has1 = enumerator1.MoveNext();
+                        }
 
-					    if (has2)
-					    {
-					        has2 = enumerator2.MoveNext();
-					    }
-				    }
+                        if (has2)
+                        {
+                            has2 = enumerator2.MoveNext();
+                        }
+                    }
 
-			    }
+                }
             }
-		}
+        }
 
-		private static string Join(this IEnumerable<string> source)
-		{
-			var builder = new StringBuilder();
+        private static string Join(this IEnumerable<string> source)
+        {
+            var builder = new StringBuilder();
 
-			foreach (var value in source)
-			{
-				builder.Append(value);
-			}
+            foreach (var value in source)
+            {
+                builder.Append(value);
+            }
 
-			return builder.ToString();
-		}
-	}
+            return builder.ToString();
+        }
+    }
 }
