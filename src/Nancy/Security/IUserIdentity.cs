8d7210f29a95e00913cc6285faf95def22900956
Andreas Hakansson <andreas@selfinflicted.org>
2012-10-11 07:47:26 +0200

    Cleaned up IUserIdentity.cs

--- src/Nancy/Security/IUserIdentity.cs
+++ src/Nancy/Security/IUserIdentity.cs
@@ -1,13 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace Nancy.Security
+﻿namespace Nancy.Security
 {
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Defines the core functionality of an identity
+    /// </summary>
     public interface IUserIdentity
     {
+        /// <summary>
+        /// The username of the authenticated user.
+        /// </summary>
+        /// <value>A <see cref="string"/> containing the username.</value>
         string UserName { get; set; }
+
+        /// <summary>
+        /// The claims of the authenticated user.
+        /// </summary>
+        /// <value>An <see cref="IEnumerable{T}"/>, containing the claims.</value>
         IEnumerable<string> Claims { get; set; } 
     }
 }
