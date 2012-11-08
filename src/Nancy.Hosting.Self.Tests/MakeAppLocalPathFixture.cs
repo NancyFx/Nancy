36d165847cbd58ca626329b19e816680af8ca030
inTagger <intagger@gmail.com>
2012-11-09 01:19:36 +0400

    Small code-formatting changes in tests file: tabs replaced with spaces (existed file contained mixed tabs and spaces and displayed ugly in GitHub's web viewer).

--- src/Nancy.Hosting.Self.Tests/MakeAppLocalPathFixture.cs
+++ src/Nancy.Hosting.Self.Tests/MakeAppLocalPathFixture.cs
@@ -1,88 +1,90 @@
 namespace Nancy.Hosting.Self.Tests
 {
     using System;
+
     using Nancy.Tests;
+
     using Xunit;
 
-	public class MakeAppLocalPathFixture
-	{
-		[Fact]
-		public void Should_return_path_as_local_path()
-		{
-            // Given
-		    var uri = new Uri("http://host/base/");
-
-            // When
-            var result = uri.MakeAppLocalPath(new Uri("http://host/base/rel"));
-			
-            // Then
-			result.ShouldEqual("/rel");
-		}
-
-		[Fact]
-		public void Should_return_root_path_with_trailing_slash_as_slash()
-		{
-            // Given
-            var uri = new Uri("http://host/base/");
-            
-            // When
-            var result = uri.MakeAppLocalPath(new Uri("http://host/base/"));
-
-            // Then
-            result.ShouldEqual("/");
-		}
-
-		[Fact]
-		public void Should_return_root_path_without_trailing_slash_as_slash()
-		{
+    public class MakeAppLocalPathFixture
+    {
+        [Fact]
+        public void Should_return_path_as_local_path()
+        {
             // Given
             var uri = new Uri("http://host/base/");
 
             // When
-            var result = uri.MakeAppLocalPath(new Uri("http://host/base"));
+            string result = uri.MakeAppLocalPath(new Uri("http://host/base/rel"));
+
+            // Then
+            result.ShouldEqual("/rel");
+        }
+
+        [Fact]
+        public void Should_return_root_path_with_trailing_slash_as_slash()
+        {
+            // Given
+            var uri = new Uri("http://host/base/");
+
+            // When
+            string result = uri.MakeAppLocalPath(new Uri("http://host/base/"));
 
             // Then
             result.ShouldEqual("/");
-		}
+        }
 
-		[Fact]
-		public void Should_return_path_with_same_casing_as_full_uri()
-		{
+        [Fact]
+        public void Should_return_root_path_without_trailing_slash_as_slash()
+        {
             // Given
             var uri = new Uri("http://host/base/");
 
             // When
-            var result = uri.MakeAppLocalPath(new Uri("http://host/base/ReL"));
+            string result = uri.MakeAppLocalPath(new Uri("http://host/base"));
+
+            // Then
+            result.ShouldEqual("/");
+        }
+
+        [Fact]
+        public void Should_return_path_with_same_casing_as_full_uri()
+        {
+            // Given
+            var uri = new Uri("http://host/base/");
+
+            // When
+            string result = uri.MakeAppLocalPath(new Uri("http://host/base/ReL"));
 
             // Then
             result.ShouldEqual("/ReL");
-		}
+        }
 
-		[Fact]
-		public void Should_support_extended_site_root()
-		{
+        [Fact]
+        public void Should_support_extended_site_root()
+        {
             // Given
             var uri = new Uri("http://host/");
 
             // When
-            var result = uri.MakeAppLocalPath(new Uri("http://host/rel/file"));
+            string result = uri.MakeAppLocalPath(new Uri("http://host/rel/file"));
 
             // Then
             result.ShouldEqual("/rel/file");
-		}
+        }
 
-		[Fact]
-		public void Should_support_site_root_without_trailing_slash()
-		{
+        [Fact]
+        public void Should_support_site_root_without_trailing_slash()
+        {
             // Given
             var uri = new Uri("http://host/");
 
             // When
-            var result = uri.MakeAppLocalPath(new Uri("http://host"));
+            string result = uri.MakeAppLocalPath(new Uri("http://host"));
 
             // Then
             result.ShouldEqual("/");
-		}
+        }
 
         [Fact]
         public void Should_return_path_with_case_insensitive_base_uri_comparison()
@@ -91,10 +93,10 @@ namespace Nancy.Hosting.Self.Tests
             var uri = new Uri("http://host/base/");
 
             // When
-            var result = uri.MakeAppLocalPath(new Uri("http://host/Base/rel"));
+            string result = uri.MakeAppLocalPath(new Uri("http://host/Base/rel"));
 
             // Then
             result.ShouldEqual("/rel");
         }
-	}
+    }
 }
