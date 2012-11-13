23350d3571bba52c989ef70e410fe7afbdde16dc
inTagger <intagger@gmail.com>
2012-11-13 16:15:00 +0400

    Removed unused namespaces.

--- src/Nancy.Hosting.Aspnet/NancyHandler.cs
+++ src/Nancy.Hosting.Aspnet/NancyHandler.cs
@@ -1,8 +1,6 @@
 namespace Nancy.Hosting.Aspnet
 {
-    using System;
     using System.Collections.Generic;
-    using System.Diagnostics;
     using System.Globalization;
     using System.Linq;
     using System.Web;
