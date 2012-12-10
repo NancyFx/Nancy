5d5d50ee15b02c3018462ab9c19ecb42566ac944
Jonathan Channon <jonathan.channon@gmail.com>
2012-12-10 15:32:56 +0000

    without default conventions so here they are

---
+++ src/Nancy/Conventions/CultureConventions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+
+namespace Nancy.Conventions
+{
+    public class CultureConventions : IEnumerable<Func<NancyContext, CultureInfo>>
+    {
+        private readonly IEnumerable<Func<NancyContext, CultureInfo>> conventions;
+
+        public CultureConventions(IEnumerable<Func<NancyContext, CultureInfo>> conventions)
+        {
+            this.conventions = conventions;
+        }
+
+        public IEnumerator<Func<NancyContext, CultureInfo>> GetEnumerator()
+        {
+            return conventions.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ src/Nancy/Conventions/DefaultCultureConventions.cs
@@ -0,0 +1,103 @@
+﻿
+namespace Nancy.Conventions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.Linq;
+    using System.Text;
+    using System.Threading;
+    using Nancy.Session;
+
+    public class DefaultCultureConventions : IConvention
+    {
+        public void Initialise(NancyConventions conventions)
+        {
+            this.ConfigureDefaultConventions(conventions);
+
+        }
+
+        public Tuple<bool, string> Validate(NancyConventions conventions)
+        {
+            if (conventions.CultureConventions == null)
+            {
+                return Tuple.Create(false, "The culture conventions cannot be null.");
+            }
+
+            return (conventions.CultureConventions.Count > 0) ?
+               Tuple.Create(true, string.Empty) :
+               Tuple.Create(false, "The culture conventions cannot be empty.");
+        }
+
+
+        private void ConfigureDefaultConventions(NancyConventions conventions)
+        {
+            conventions.CultureConventions = new List<Func<NancyContext, CultureInfo>>
+            {
+                (ctx) =>
+                    {
+                        if (ctx.Request.Form["CurrentCulture"] != null)
+                        {
+                            return (CultureInfo) ctx.Request.Form["CurrentCulture"];
+                        }
+
+                        return null;
+                    },
+
+                (ctx) =>
+                    {
+                        var firstParameter =
+                            ctx.Request.Url.Path.Split(new char[] {'/'}, StringSplitOptions.RemoveEmptyEntries).
+                                FirstOrDefault();
+
+                        if (firstParameter != null && IsValidCultureInfoName(firstParameter))
+                        {
+                            return new CultureInfo(firstParameter);
+                        }
+
+                        return null;
+                    },
+               (ctx) =>
+                   {
+                       if (!(ctx.Request.Session is NullSessionProvider) && ctx.Request.Session["CurrentCulutre"] != null)
+                       {
+                           return (CultureInfo)ctx.Request.Session["CurrentCulture"];
+                       }
+
+                       return null;
+                   },
+
+                (ctx) =>
+                    {
+                        string cookieCulture = null;
+
+                        if (ctx.Request.Cookies.TryGetValue("CurrentCulture", out cookieCulture))
+                        {
+                          return  new CultureInfo(cookieCulture);
+                        }
+
+                        return null;
+                    },
+                    
+             
+                (ctx) => Thread.CurrentThread.CurrentCulture
+            };
+
+        }
+
+        private static bool IsValidCultureInfoName(string name)
+        {
+            var validCulture = false;
+            foreach (CultureInfo culture in CultureInfo.GetCultures(CultureTypes.SpecificCultures))
+            {
+                if (culture.Name == name)
+                {
+                    validCulture = true;
+                    break;
+                }
+            }
+            return validCulture;
+        }
+
+    }
+}
