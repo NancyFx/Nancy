fc2daf759ea53556a85cdeaa94b6e8a3f031e16b
Jonathan Channon <jonathan.channon@gmail.com>
2012-12-22 23:47:44 +0000

    Put all conventions indexes to relate to new index and added tests for finding views based on culture

--- src/Nancy.Tests/Unit/Conventions/DefaultViewLocationConventionsFixture.cs
+++ src/Nancy.Tests/Unit/Conventions/DefaultViewLocationConventionsFixture.cs
@@ -1,3 +1,6 @@
+using System.Globalization;
+using Xunit.Extensions;
+
 namespace Nancy.Tests.Unit.Conventions
 {
     using System;
@@ -73,7 +76,7 @@ namespace Nancy.Tests.Unit.Conventions
         public void Should_be_valid_when_view_conventions_is_not_empty()
         {
             // Given
-            this.conventions.ViewLocationConventions = 
+            this.conventions.ViewLocationConventions =
                 new List<Func<string, dynamic, ViewLocationContext, string>>
                 {
                     (viewName, model, viewLocationContext) => {
@@ -126,12 +129,31 @@ namespace Nancy.Tests.Unit.Conventions
 
             // When
             var result = convention.Invoke(
-                "viewname", 
-                null, 
+                "viewname",
+                null,
                 new ViewLocationContext { ModulePath = string.Empty });
 
             // Then
             result.ShouldEqual("viewname");
+        }
+
+        [Theory]
+        [InlineData("en-GB")]
+        [InlineData("de-DE")]
+        public void Should_define_convention_that_returns_viewname_with_culture(string culture)
+        {
+            // Given
+            this.viewLocationConventions.Initialise(this.conventions);
+            var convention = this.conventions.ViewLocationConventions[14];
+
+            // When
+            var result = convention.Invoke(
+                "viewname",
+                null,
+                new ViewLocationContext { ModulePath = string.Empty, Context = new NancyContext() { Culture = new CultureInfo(culture) } });
+
+            // Then
+            result.ShouldEqual(string.Concat("viewname-", culture));
         }
 
         [Fact]
@@ -151,6 +173,25 @@ namespace Nancy.Tests.Unit.Conventions
             result.ShouldEqual("views/viewname");
         }
 
+        [Theory]
+        [InlineData("en-GB")]
+        [InlineData("de-DE")]
+        public void Should_define_convention_that_returns_viewname_in_views_folder_with_culture(string culture)
+        {
+            // Given
+            this.viewLocationConventions.Initialise(this.conventions);
+            var convention = this.conventions.ViewLocationConventions[12];
+
+            // When
+            var result = convention.Invoke(
+                "viewname",
+                null,
+                new ViewLocationContext { ModulePath = string.Empty, Context = new NancyContext() { Culture = new CultureInfo(culture) } });
+
+            // Then
+            result.ShouldEqual(string.Concat("views/viewname-", culture));
+        }
+
         [Fact]
         public void Should_define_convention_that_returns_viewname_in_modulepath_subfolder_of_views_folder()
         {
@@ -166,6 +207,25 @@ namespace Nancy.Tests.Unit.Conventions
 
             // Then
             result.ShouldEqual("views/modulepath/viewname");
+        }
+
+        [Theory]
+        [InlineData("en-GB")]
+        [InlineData("de-DE")]
+        public void Should_define_convention_that_returns_viewname_in_modulepath_subfolder_of_views_folder_with_culture(string culture)
+        {
+            // Given
+            this.viewLocationConventions.Initialise(this.conventions);
+            var convention = this.conventions.ViewLocationConventions[4];
+
+            // When
+            var result = convention.Invoke(
+                "viewname",
+                null,
+                new ViewLocationContext { ModulePath = "modulepath", Context = new NancyContext() { Culture = new CultureInfo(culture) } });
+
+            // Then
+            result.ShouldEqual(string.Concat("views/modulepath/viewname-", culture));
         }
 
         [Fact]
@@ -185,12 +245,31 @@ namespace Nancy.Tests.Unit.Conventions
             result.ShouldEqual("views/modulepath/viewname");
         }
 
+        [Theory]
+        [InlineData("en-GB")]
+        [InlineData("de-DE")]
+        public void Should_define_convention_that_returns_viewname_in_modulepath_subfolder_of_views_folder_when_modulepath_contains_leading_slash_with_culture(string culture)
+        {
+            // Given
+            this.viewLocationConventions.Initialise(this.conventions);
+            var convention = this.conventions.ViewLocationConventions[4];
+
+            // When
+            var result = convention.Invoke(
+                "viewname",
+                null,
+                new ViewLocationContext { ModulePath = "/modulepath", Context = new NancyContext() { Culture = new CultureInfo(culture) } });
+
+            // Then
+            result.ShouldEqual(string.Concat("views/modulepath/viewname-", culture));
+        }
+
         [Fact]
         public void Should_return_empty_result_convention_that_returns_viewname_in_modulepath_subfolder_of_views_folder_when_modulepath_is_empty()
         {
             // Given
             this.viewLocationConventions.Initialise(this.conventions);
-            var convention = this.conventions.ViewLocationConventions[0];
+            var convention = this.conventions.ViewLocationConventions[1];
 
             // When
             var result = convention.Invoke(
@@ -202,8 +281,10 @@ namespace Nancy.Tests.Unit.Conventions
             result.ShouldEqual(string.Empty);
         }
 
-        [Fact]
-        public void Should_return_empty_result_convention_that_returns_viewname_in_modulepath_subfolder_of_views_folder_when_modulepath_is_null()
+        [Theory]
+        [InlineData("en-GB")]
+        [InlineData("de-DE")]
+        public void Should_return_empty_result_convention_that_returns_viewname_in_modulepath_subfolder_of_views_folder_when_modulepath_is_empty_with_culture(string culture)
         {
             // Given
             this.viewLocationConventions.Initialise(this.conventions);
@@ -213,7 +294,43 @@ namespace Nancy.Tests.Unit.Conventions
             var result = convention.Invoke(
                 "viewname",
                 null,
+                new ViewLocationContext { ModulePath = string.Empty, Context = new NancyContext() { Culture = new CultureInfo(culture) } });
+
+            // Then
+            result.ShouldEqual(string.Empty);
+        }
+
+        [Fact]
+        public void Should_return_empty_result_convention_that_returns_viewname_in_modulepath_subfolder_of_views_folder_when_modulepath_is_null()
+        {
+            // Given
+            this.viewLocationConventions.Initialise(this.conventions);
+            var convention = this.conventions.ViewLocationConventions[1];
+
+            // When
+            var result = convention.Invoke(
+                "viewname",
+                null,
                 new ViewLocationContext { ModulePath = null });
+
+            // Then
+            result.ShouldEqual(string.Empty);
+        }
+
+        [Theory]
+        [InlineData("en-GB")]
+        [InlineData("de-DE")]
+        public void Should_return_empty_result_convention_that_returns_viewname_in_modulepath_subfolder_of_views_folder_when_modulepath_is_null_with_culture(string culture)
+        {
+            // Given
+            this.viewLocationConventions.Initialise(this.conventions);
+            var convention = this.conventions.ViewLocationConventions[0];
+
+            // When
+            var result = convention.Invoke(
+                "viewname",
+                null,
+                new ViewLocationContext { ModulePath = null, Context = new NancyContext() { Culture = new CultureInfo(culture) } });
 
             // Then
             result.ShouldEqual(string.Empty);
@@ -236,12 +353,31 @@ namespace Nancy.Tests.Unit.Conventions
             result.ShouldEqual("modulepath/viewname");
         }
 
+        [Theory]
+        [InlineData("en-GB")]
+        [InlineData("de-DE")]
+        public void Should_define_convention_that_returns_viewname_in_modulepath_folder_with_culture(string culture)
+        {
+            // Given
+            this.viewLocationConventions.Initialise(this.conventions);
+            var convention = this.conventions.ViewLocationConventions[6];
+
+            // When
+            var result = convention.Invoke(
+                "viewname",
+                null,
+                new ViewLocationContext { ModulePath = "modulepath", Context = new NancyContext() { Culture = new CultureInfo(culture) } });
+
+            // Then
+            result.ShouldEqual(string.Concat("modulepath/viewname-", culture));
+        }
+
         [Fact]
         public void Should_return_empty_result_for_convention_that_returns_viewname_in_modulepath_folder_when_modulepath_is_empty()
         {
             // Given
             this.viewLocationConventions.Initialise(this.conventions);
-            var convention = this.conventions.ViewLocationConventions[2];
+            var convention = this.conventions.ViewLocationConventions[5];
 
             // When
             var result = convention.Invoke(
@@ -253,18 +389,56 @@ namespace Nancy.Tests.Unit.Conventions
             result.ShouldEqual(string.Empty);
         }
 
+        [Theory]
+        [InlineData("en-GB")]
+        [InlineData("de-DE")]
+        public void Should_return_empty_result_for_convention_that_returns_viewname_in_modulepath_folder_when_modulepath_is_empty_with_culture(string culture)
+        {
+            // Given
+            this.viewLocationConventions.Initialise(this.conventions);
+            var convention = this.conventions.ViewLocationConventions[4];
+
+            // When
+            var result = convention.Invoke(
+                "viewname",
+                null,
+                new ViewLocationContext { ModulePath = string.Empty, Context = new NancyContext() { Culture = new CultureInfo(culture) } });
+
+            // Then
+            result.ShouldEqual(string.Empty);
+        }
+
         [Fact]
         public void Should_return_empty_result_for_convention_that_returns_viewname_in_modulepath_folder_when_modulepath_is_null()
         {
             // Given
             this.viewLocationConventions.Initialise(this.conventions);
-            var convention = this.conventions.ViewLocationConventions[2];
+            var convention = this.conventions.ViewLocationConventions[5];
 
             // When
             var result = convention.Invoke(
                 "viewname",
                 null,
                 new ViewLocationContext { ModulePath = null });
+
+            // Then
+            result.ShouldEqual(string.Empty);
+        }
+
+        [Theory]
+        [InlineData("en-GB")]
+        [InlineData("de-DE")]
+        public void Should_return_empty_result_for_convention_that_returns_viewname_in_modulepath_folder_when_modulepath_is_null_with_culture(string culture)
+        {
+            // Given
+            this.viewLocationConventions.Initialise(this.conventions);
+            var convention = this.conventions.ViewLocationConventions[4];
+
+            // When
+            var result = convention.Invoke(
+                "viewname",
+                null,
+                new ViewLocationContext { ModulePath = null, Context = new NancyContext() { Culture = new CultureInfo(culture) } });
 
             // Then
             result.ShouldEqual(string.Empty);
@@ -287,6 +461,25 @@ namespace Nancy.Tests.Unit.Conventions
             result.ShouldEqual("modulepath/viewname");
         }
 
+        [Theory]
+        [InlineData("en-GB")]
+        [InlineData("de-DE")]
+        public void Should_define_convention_that_returns_viewname_in_modulepath_folder_when_modulepath_contains_leading_slash_with_culture(string culture)
+        {
+            // Given
+            this.viewLocationConventions.Initialise(this.conventions);
+            var convention = this.conventions.ViewLocationConventions[6];
+
+            // When
+            var result = convention.Invoke(
+                "viewname",
+                null,
+                new ViewLocationContext { ModulePath = "/modulepath", Context = new NancyContext() { Culture = new CultureInfo(culture) } });
+
+            // Then
+            result.ShouldEqual(string.Concat("modulepath/viewname-", culture));
+        }
+
         [Fact]
         public void Should_define_convention_that_returns_viewname_in_modulename_subfolder_of_views_folder()
         {
@@ -302,6 +495,25 @@ namespace Nancy.Tests.Unit.Conventions
 
             // Then
             result.ShouldEqual("views/modulename/viewname");
+        }
+
+        [Theory]
+        [InlineData("en-GB")]
+        [InlineData("de-DE")]
+        public void Should_define_convention_that_returns_viewname_in_modulename_subfolder_of_views_folder_with_culture(string culture)
+        {
+            // Given
+            this.viewLocationConventions.Initialise(this.conventions);
+            var convention = this.conventions.ViewLocationConventions[8];
+
+            // When
+            var result = convention.Invoke(
+                "viewname",
+                null,
+                new ViewLocationContext { ModuleName = "modulename", Context = new NancyContext() { Culture = new CultureInfo(culture) } });
+
+            // Then
+            result.ShouldEqual(string.Concat("views/modulename/viewname-", culture));
         }
 
         [Fact]
@@ -321,6 +533,25 @@ namespace Nancy.Tests.Unit.Conventions
             result.ShouldEqual("modulename/viewname");
         }
 
+        [Theory]
+        [InlineData("en-GB")]
+        [InlineData("de-DE")]
+        public void Should_define_convention_that_returns_viewname_in_modulename_folder_with_culture(string culture)
+        {
+            // Given
+            this.viewLocationConventions.Initialise(this.conventions);
+            var convention = this.conventions.ViewLocationConventions[10];
+
+            // When
+            var result = convention.Invoke(
+                "viewname",
+                null,
+                new ViewLocationContext { ModuleName = "modulename", Context = new NancyContext() { Culture = new CultureInfo(culture) } });
+
+            // Then
+            result.ShouldEqual(string.Concat("modulename/viewname-", culture));
+        }
+
         [Fact]
         public void Should_define_convention_that_returns_viewname_in_modulename_folder_in_modulepath_folder()
         {
@@ -336,6 +567,25 @@ namespace Nancy.Tests.Unit.Conventions
 
             // Then
             result.ShouldEqual("modulepath/modulename/viewname");
+        }
+
+        [Theory]
+        [InlineData("en-GB")]
+        [InlineData("de-DE")]
+        public void Should_define_convention_that_returns_viewname_in_modulename_folder_in_modulepath_folder_with_culture(string culture)
+        {
+            // Given
+            this.viewLocationConventions.Initialise(this.conventions);
+            var convention = this.conventions.ViewLocationConventions[2];
+
+            // When
+            var result = convention.Invoke(
+                "viewname",
+                null,
+                new ViewLocationContext { ModuleName = "modulename", ModulePath = "modulepath", Context = new NancyContext() { Culture = new CultureInfo(culture) } });
+
+            // Then
+            result.ShouldEqual(string.Concat("modulepath/modulename/viewname-", culture));
         }
 
         [Fact]
@@ -355,6 +605,25 @@ namespace Nancy.Tests.Unit.Conventions
             result.ShouldEqual("views/modulepath/modulename/viewname");
         }
 
+        [Theory]
+        [InlineData("en-GB")]
+        [InlineData("de-DE")]
+        public void Should_define_convention_that_returns_viewname_in_modulename_folder_in_modulepath_folder_in_views_folder_with_culture(string culture)
+        {
+            // Given
+            this.viewLocationConventions.Initialise(this.conventions);
+            var convention = this.conventions.ViewLocationConventions[0];
+
+            // When
+            var result = convention.Invoke(
+                "viewname",
+                null,
+                new ViewLocationContext { ModuleName = "modulename", ModulePath = "modulepath", Context = new NancyContext() { Culture = new CultureInfo(culture) } });
+
+            // Then
+            result.ShouldEqual(string.Concat("views/modulepath/modulename/viewname-", culture));
+        }
+
         [Fact]
         public void Should_define_convention_that_returns_viewname_in_modulename_folder_in_modulepath_folder_when_modulepath_contains_leading_slash()
         {
@@ -372,6 +641,25 @@ namespace Nancy.Tests.Unit.Conventions
             result.ShouldEqual("modulepath/modulename/viewname");
         }
 
+        [Theory]
+        [InlineData("en-GB")]
+        [InlineData("de-DE")]
+        public void Should_define_convention_that_returns_viewname_in_modulename_folder_in_modulepath_folder_when_modulepath_contains_leading_slash_with_culture(string culture)
+        {
+            // Given
+            this.viewLocationConventions.Initialise(this.conventions);
+            var convention = this.conventions.ViewLocationConventions[2];
+
+            // When
+            var result = convention.Invoke(
+                "viewname",
+                null,
+                new ViewLocationContext { ModuleName = "modulename", ModulePath = "/modulepath", Context = new NancyContext() { Culture = new CultureInfo(culture) } });
+
+            // Then
+            result.ShouldEqual(string.Concat("modulepath/modulename/viewname-", culture));
+        }
+
         [Fact]
         public void Should_define_convention_that_returns_viewname_in_modulename_folder_in_modulepath_folder_in_views_folder_when_modulepath_contains_leading_slash()
         {
@@ -387,6 +675,25 @@ namespace Nancy.Tests.Unit.Conventions
 
             // Then
             result.ShouldEqual("views/modulepath/modulename/viewname");
+        }
+
+        [Theory]
+        [InlineData("en-GB")]
+        [InlineData("de-DE")]
+        public void Should_define_convention_that_returns_viewname_in_modulename_folder_in_modulepath_folder_in_views_folder_when_modulepath_contains_leading_slash_with_culture(string culture)
+        {
+            // Given
+            this.viewLocationConventions.Initialise(this.conventions);
+            var convention = this.conventions.ViewLocationConventions[0];
+
+            // When
+            var result = convention.Invoke(
+                "viewname",
+                null,
+                new ViewLocationContext { ModuleName = "modulename", ModulePath = "/modulepath", Context = new NancyContext() { Culture = new CultureInfo(culture) } });
+
+            // Then
+            result.ShouldEqual(string.Concat("views/modulepath/modulename/viewname-", culture));
         }
     }
 }
