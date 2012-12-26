47e2e1603b745dd34bf96bae18db4e735cc4a7de
Richard Cirerol <richard@codeprogression.com>
2012-12-26 10:55:44 -0700

    Adding tracing smoke test
    Accept header tracing in defaultrouteinvoker throws error when no accept
    headers are sent. Adding functional regression test

---
+++ src/Nancy.Tests.Functional/Modules/RazorWithTracingTestModule.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nancy.Tests.Functional.Modules
+{
+    public class RazorWithTracingTestModule : NancyModule
+    {
+        public RazorWithTracingTestModule()
+        {
+            StaticConfiguration.EnableRequestTracing = true;
+            Get["/tracing/razor-viewbag"] = _ =>
+                {
+                    this.ViewBag.Name = "Bob";
+
+                    return View["RazorPage"];
+                };
+        }
+    }
+}
---
+++ src/Nancy.Tests.Functional/Tests/TracingSmokeTests.cs
@@ -0,0 +1,39 @@
+﻿namespace Nancy.Tests.Functional.Tests
+{
+    using System;
+    using Bootstrapper;
+    using Modules;
+    using Testing;
+    using Xunit;
+
+    public class TracingSmokeTests
+    {
+        private readonly INancyBootstrapper bootstrapper;
+
+        private readonly Browser browser;
+
+        public TracingSmokeTests()
+        {
+            this.bootstrapper = new ConfigurableBootstrapper(
+                    configuration => configuration.Modules(new Type[] { typeof(RazorWithTracingTestModule) }));
+
+            this.browser = new Browser(bootstrapper);
+        }
+
+        [Fact]
+        public void Should_render_content_from_viewbag()
+        {
+            // Given
+            // When
+            var response = browser.Get(
+                @"/tracing/razor-viewbag",
+                with =>
+                {
+                    with.HttpRequest();
+                });
+
+            // Then
+            Assert.True(response.Body.AsString().Contains(@"Hello Bob"));
+        }
+    }
+}
