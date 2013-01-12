2b0fbb2afcb435846fe7107f7707d9efab0d7a1f
Steven Robbins <ste.robbins@gmail.com>
2013-01-12 08:49:53 +0000

    Added configuration object to fix #832 and fix #902
    Configuration object allows enabling/disabling the localhost
    rewrite and also to specify a callback for unhandled
    exceptions

---
+++ src/Nancy.Hosting.Self/HostConfiguration.cs
@@ -0,0 +1,38 @@
+﻿namespace Nancy.Hosting.Self
+{
+    using System;
+    using System.Diagnostics;
+
+    /// <summary>
+    /// Host configuration for the self host
+    /// </summary>
+    public sealed class HostConfiguration
+    {
+        /// <summary>
+        /// Gets or sets a property that determines if localhost uris are 
+        /// rewritten to htp://+:port/ style uris to allow for listening on 
+        /// all ports, but requiring either a url reservation, or admin
+        /// access
+        /// </summary>
+        public bool RewriteLocalhost { get; set; }
+
+        /// <summary>
+        /// Gets or sets a property that provides a callback to be called
+        /// if there's an unhandled exception in the self host.
+        /// Note: this will *not* be called for normal nancy Exceptions
+        /// that are handled by the Nancy handlers.
+        /// Defaults to writing to debug out.
+        /// </summary>
+        public Action<Exception> UnhandledExceptionCallback { get; set; }
+
+        public HostConfiguration()
+        {
+            this.RewriteLocalhost = true;
+            this.UnhandledExceptionCallback = e =>
+                {
+                    var message = String.Format("---\n{0}\n---\n", e);
+                    Debug.Write(message);
+                };
+        }
+    }
+}
