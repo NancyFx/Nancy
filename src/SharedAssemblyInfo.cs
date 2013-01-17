2361a9a52d3f56f20edb21ff2362132e63956f9b
Steven Robbins <ste.robbins@gmail.com>
2013-01-17 17:32:26 +0000

    Revved version

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -5,8 +5,8 @@ using System.Reflection;
 [assembly: AssemblyCompany("Nancy")]
 [assembly: AssemblyProduct("Nancy")]
 [assembly: AssemblyCopyright("Copyright (C) Andreas Hakansson, Steven Robbins and contributors")]
-[assembly: AssemblyVersion("0.15.1")]
-[assembly: AssemblyInformationalVersion("0.15.1")]
+[assembly: AssemblyVersion("0.15.2")]
+[assembly: AssemblyInformationalVersion("0.15.2")]
 
 
 
