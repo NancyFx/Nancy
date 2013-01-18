2f4df1c28a7aa4ebbd0968cbe5c4f003ca0459f3
Steven Robbins <ste.robbins@gmail.com>
2013-01-18 14:37:15 +0000

    Added machine config encryption

---
+++ src/Nancy.Encryption.MachineKey/MachineKeyEncryptionProvider.cs
@@ -0,0 +1,45 @@
+﻿namespace Nancy.Encryption.MachineKey
+{
+    using System;
+    using System.Text;
+    using System.Web.Security;
+
+    using Nancy.Cryptography;
+
+    /// <summary>
+    /// An encryption provider that uses the ASP.Net MachineKey
+    /// </summary>
+    public class MachineKeyEncryptionProvider : IEncryptionProvider
+    {
+        /// <summary>
+        /// Encrypt and base64 encode the string
+        /// </summary>
+        /// <param name="data">Data to encrypyt</param>
+        /// <returns>Encrypted string</returns>
+        public string Encrypt(string data)
+        {
+            var input = Encoding.UTF8.GetBytes(data);
+
+            return MachineKey.Encode(input, MachineKeyProtection.Encryption);
+        }
+
+        /// <summary>
+        /// Decrypt string
+        /// </summary>
+        /// <param name="data">Data to decrypt</param>
+        /// <returns>Decrypted string</returns>
+        public string Decrypt(string data)
+        {
+            try
+            {
+                var bytes = MachineKey.Decode(data, MachineKeyProtection.Encryption);
+
+                return Encoding.UTF8.GetString(bytes);
+            }
+            catch (Exception)
+            {
+                return string.Empty;
+            }
+        }
+    }
+}
