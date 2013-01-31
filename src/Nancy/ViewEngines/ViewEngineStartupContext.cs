4860177cc179fd3d2bd885c155ced31fcdd69acb
Steven Robbins <ste.robbins@gmail.com>
2013-01-31 12:48:40 +0000

    More test fixups

--- src/Nancy/ViewEngines/ViewEngineStartupContext.cs
+++ src/Nancy/ViewEngines/ViewEngineStartupContext.cs
@@ -5,16 +5,16 @@
 
     public class ViewEngineStartupContext
     {
-        private readonly IViewLocator viewLocator;
-
         public ViewEngineStartupContext(IViewCache viewCache, IViewLocator viewLocator, IEnumerable<string> extensions)
         {
-            this.viewLocator = viewLocator;
+            this.ViewLocator = viewLocator;
             this.Extensions = extensions;
             this.ViewCache = viewCache;
         }
 
         public IViewCache ViewCache { get; private set; }
+
+        public IViewLocator ViewLocator { get; private set; }
 
         public IEnumerable<string> Extensions { get; private set; }
     }
