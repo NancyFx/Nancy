4d51a39b1af43536b897f1fb7b1c7562bfc2b476
Steven Robbins <ste.robbins@gmail.com>
2013-02-05 07:23:17 +0000

    Added functional tests to ci builds

--- rakefile.rb
+++ rakefile.rb
@@ -81,7 +81,7 @@ end
 
 desc "Executes xUnit tests"
 xunit :xunit => [:compile] do |xunit|
-    tests = FileList["src/**/#{CONFIGURATION}/*.Tests.dll"].exclude(/obj\//)
+    tests = FileList["src/**/#{CONFIGURATION}/*.Tests.*.dll"].exclude(/obj\//)
 
     xunit.command = "tools/xunit/xunit.console.clr4.x86.exe"
     xunit.assemblies = tests
@@ -89,7 +89,7 @@ end
 
 desc "Executes xUnit tests using Mono"
 xunit :xunitmono => [] do |xunit|
-    tests = FileList["src/**/#{CONFIGURATIONMONO}/*.Tests.dll"].exclude(/obj\//)
+    tests = FileList["src/**/#{CONFIGURATIONMONO}/*.Tests.*.dll"].exclude(/obj\//)
 
     xunit.command = "tools/xunit/xunitmono.sh"
     xunit.assemblies = tests
