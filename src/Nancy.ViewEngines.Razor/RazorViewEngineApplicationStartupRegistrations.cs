20be16180cd5c8ffb8f26624c4eb84b2b464ab86
Andreas Hakansson <andreas@selfinflicted.org>
2013-02-07 12:00:34 +0100

    Updated razor registrations to support overriding

--- src/Nancy.ViewEngines.Razor/RazorViewEngineApplicationStartupRegistrations.cs
+++ src/Nancy.ViewEngines.Razor/RazorViewEngineApplicationStartupRegistrations.cs
@@ -1,35 +1,15 @@
 namespace Nancy.ViewEngines.Razor
 {
-    using System.Collections.Generic;
     using Bootstrapper;
 
     /// <summary>
     /// Default dependency registrations for the <see cref="RazorViewEngine"/> class.
     /// </summary>
-    public class RazorViewEngineApplicationRegistrations : IApplicationRegistrations
+    public class RazorViewEngineApplicationRegistrations : ApplicationRegistrations
     {
-        /// <summary>
-        /// Gets the type registrations to register for this startup task
-        /// </summary>
-        public IEnumerable<TypeRegistration> TypeRegistrations
+        public RazorViewEngineApplicationRegistrations()
         {
-            get { return new[] { new TypeRegistration(typeof(IRazorConfiguration), typeof(DefaultRazorConfiguration)), }; }
-        }
-
-        /// <summary>
-        /// Gets the collection registrations to register for this startup task
-        /// </summary>
-        public IEnumerable<CollectionTypeRegistration> CollectionTypeRegistrations
-        {
-            get { return null; }
-        }
-
-        /// <summary>
-        /// Gets the instance registrations to register for this startup task
-        /// </summary>
-        public IEnumerable<InstanceRegistration> InstanceRegistrations
-        {
-            get { return null; }
+            this.RegisterWithDefault<IRazorConfiguration>(typeof(DefaultRazorConfiguration));
         }
     }
 }
