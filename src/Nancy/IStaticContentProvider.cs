9e9c3d33e87794a1a84a6edb6d3dfe9ae954115c
Steven Robbins <ste.robbins@gmail.com>
2013-02-14 08:34:05 +0000

    Added IStaticContentProvider and implementations

---
+++ src/Nancy/IStaticContentProvider.cs
@@ -0,0 +1,15 @@
+﻿namespace Nancy
+{
+    /// <summary>
+    /// Provides static content delivery
+    /// </summary>
+    public interface IStaticContentProvider
+    {
+        /// <summary>
+        /// Gets the static content response, if possible.
+        /// </summary>
+        /// <param name="context">Current context</param>
+        /// <returns>Response if serving content, null otherwise</returns>
+        Response GetContent(NancyContext context);
+    }
+}
