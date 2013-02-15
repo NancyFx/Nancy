4fab680bde938b24bfd76d1e4199c0341759a090
Steven Robbins <ste.robbins@gmail.com>
2013-02-15 16:44:01 +0000

    Ported across tests

--- src/Nancy.Tests/Unit/Routing/DefaultRouteResolverFixture.cs
+++ src/Nancy.Tests/Unit/Routing/DefaultRouteResolverFixture.cs
@@ -1,6 +1,145 @@
 ﻿namespace Nancy.Tests.Unit.Routing
 {
+    using Nancy.Testing;
+
+    using Xunit;
+
     public class DefaultRouteResolverFixture
     {
+        private readonly Browser browser;
+
+        public DefaultRouteResolverFixture()
+        {
+            this.browser = new Browser(with => with.Module<TestModule>());
+        }
+
+        [Fact]
+        public void Should_resolve_root()
+        {
+            var result = this.browser.Get("/");
+
+            result.Body.AsString().ShouldEqual("Root");
+        }
+
+        [Fact]
+        public void Should_resolve_single_literal()
+        {
+            var result = this.browser.Get("/foo");
+
+            result.Body.AsString().ShouldEqual("SingleLiteral");
+        }
+
+        [Fact]
+        public void Should_resolve_multi_literal()
+        {
+            var result = this.browser.Get("/foo/bar/baz");
+
+            result.Body.AsString().ShouldEqual("MultipleLiteral");
+        }
+
+        [Fact]
+        public void Should_resolve_single_capture()
+        {
+            var result = this.browser.Get("/foo/testing/plop");
+
+            result.Body.AsString().ShouldEqual("Captured testing");
+        }
+
+        [Fact]
+        public void Should_resolve_optional_capture_with_optional_specified()
+        {
+            var result = this.browser.Get("/moo/hoo/moo");
+
+            result.Body.AsString().ShouldEqual("OptionalCapture hoo");
+        }
+
+        [Fact]
+        public void Should_resolve_optional_capture_with_optional_not_specified()
+        {
+            var result = this.browser.Get("/moo/moo");
+
+            result.Body.AsString().ShouldEqual("OptionalCapture default");
+        }
+
+        [Fact]
+        public void Should_resolve_optional_capture_with_default_with_optional_specified()
+        {
+            var result = this.browser.Get("/boo/badger/laa");
+
+            result.Body.AsString().ShouldEqual("OptionalCaptureWithDefault badger");
+        }
+
+        [Fact]
+        public void Should_resolve_optional_capture_with_default_with_optional_not_specified()
+        {
+            var result = this.browser.Get("/boo/laa");
+
+            result.Body.AsString().ShouldEqual("OptionalCaptureWithDefault test");
+        }
+
+        [Fact]
+        public void Should_capture_greedy_on_end()
+        {
+            var result = this.browser.Get("/bleh/this/is/some/stuff");
+
+            result.Body.AsString().ShouldEqual("GreedyOnEnd this/is/some/stuff");
+        }
+
+        [Fact]
+        public void Should_capture_greedy_in_middle()
+        {
+            var result = this.browser.Get("/bleh/this/is/some/stuff/bar");
+
+            result.Body.AsString().ShouldEqual("GreedyInMiddle this/is/some/stuff");
+        }
+
+        [Fact]
+        public void Should_capture_greedy_and_normal_capture()
+        {
+            var result = this.browser.Get("/greedy/this/is/some/stuff/badger/blah");
+
+            result.Body.AsString().ShouldEqual("GreedyAndCapture this/is/some/stuff blah");
+        }
+
+        [Fact]
+        public void Should_capture_regex()
+        {
+            var result = this.browser.Get("/regex/123/moo");
+
+            result.Body.AsString().ShouldEqual("RegEx 123 moo");
+        }
+
+        public class TestModule : NancyModule
+        {
+            public TestModule()
+            {
+                Get["/"] = _ => "Root";
+
+                Get["/foo"] = _ => "SingleLiteral";
+
+                Get["/foo/bar/baz"] = _ => "MultipleLiteral";
+
+                Get["/foo/{bar}/plop"] = _ => "Captured " + _.bar;
+
+                Get["/moo/baa"] = _ => "Dummy";
+
+                Get["/moo/baa/cheese"] = _ => "Dummy";
+
+                Get["/moo/{test?}/moo"] = _ => "OptionalCapture " + _.test.Default("default");
+
+                Get["/boo/{woo?test}/laa"] = _ => "OptionalCaptureWithDefault " + _.woo;
+
+                Get["/bleh/{test*}"] = _ => "GreedyOnEnd " + _.test;
+
+                Get["/bleh/{test*}/bar"] = _ => "GreedyInMiddle " + _.test;
+
+                Get["/greedy/{test*}/badger/{woo}"] = _ => "GreedyAndCapture " + _.test + " " + _.woo;
+
+                Get[@"/regex/(?<foo>\d{2,4})/{bar}"] = x =>
+                {
+                    return string.Format("RegEx {0} {1}", x.foo, x.bar);
+                };
+            }
+        }
     }
 }
