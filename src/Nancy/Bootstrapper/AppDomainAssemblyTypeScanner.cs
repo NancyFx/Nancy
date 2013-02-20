c1c793a44f1c6bc0f6ab323527f2a6e0fb1977d1
Andreas Hakansson <andreas@selfinflicted.org>
2013-02-20 12:27:59 +0100

    Removed unused using statement

--- src/Nancy/Bootstrapper/AppDomainAssemblyTypeScanner.cs
+++ src/Nancy/Bootstrapper/AppDomainAssemblyTypeScanner.cs
@@ -2,7 +2,6 @@ namespace Nancy.Bootstrapper
 {
     using System;
     using System.Collections.Generic;
-    using System.Diagnostics;
     using System.IO;
     using System.Linq;
     using System.Reflection;
