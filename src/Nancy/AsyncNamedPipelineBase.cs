497c08b8a8701de7dadd0b09910c9f2cdfae948d
Steven Robbins <ste.robbins@gmail.com>
2013-03-01 10:40:56 +0000

    Ported across async Before/After pipelines - currently non-building

---
+++ src/Nancy/AsyncNamedPipelineBase.cs
@@ -0,0 +1,332 @@
+﻿namespace Nancy
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public abstract class AsyncNamedPipelineBase<TAsyncDelegate, TSyncDelegate>
+    {
+        /// <summary>
+        /// Pipeline items to execute
+        /// </summary>
+        protected readonly List<PipelineItem<TAsyncDelegate>> pipelineItems;
+
+        protected AsyncNamedPipelineBase()
+        {
+            this.pipelineItems = new List<PipelineItem<TAsyncDelegate>>();
+        }
+
+        protected AsyncNamedPipelineBase(int capacity)
+        {
+            this.pipelineItems = new List<PipelineItem<TAsyncDelegate>>(capacity);
+        }
+
+        /// <summary>
+        /// Gets the current pipeline items
+        /// </summary>
+        public IEnumerable<PipelineItem<TAsyncDelegate>> PipelineItems
+        {
+            get { return this.pipelineItems.AsReadOnly(); }
+        }
+
+        /// <summary>
+        /// Gets the current pipeline item delegates
+        /// </summary>
+        public IEnumerable<TAsyncDelegate> PipelineDelegates
+        {
+            get { return this.pipelineItems.Select(pipelineItem => pipelineItem.Delegate); }
+        }
+
+        /// <summary>
+        /// Add an item to the start of the pipeline
+        /// </summary>
+        /// <param name="item">Item to add</param>
+        public virtual void AddItemToStartOfPipeline(TAsyncDelegate item)
+        {
+            this.AddItemToStartOfPipeline((PipelineItem<TAsyncDelegate>)item);
+        }
+
+        /// <summary>
+        /// Add an item to the start of the pipeline
+        /// </summary>
+        /// <param name="item">Item to add</param>
+        public virtual void AddItemToStartOfPipeline(TSyncDelegate item)
+        {
+            this.AddItemToStartOfPipeline(this.Wrap(item));
+        }
+
+        /// <summary>
+        /// Add an item to the start of the pipeline
+        /// </summary>
+        /// <param name="item">Item to add</param>
+        /// <param name="replaceInPlace">
+        /// Whether to replace an existing item with the same name in its current place,
+        /// rather than at the position requested. Defaults to false.
+        /// </param>
+        public virtual void AddItemToStartOfPipeline(PipelineItem<TAsyncDelegate> item, bool replaceInPlace = false)
+        {
+            this.InsertItemAtPipelineIndex(0, item, replaceInPlace);
+        }
+
+        /// <summary>
+        /// Add an item to the start of the pipeline
+        /// </summary>
+        /// <param name="item">Item to add</param>
+        /// <param name="replaceInPlace">
+        /// Whether to replace an existing item with the same name in its current place,
+        /// rather than at the position requested. Defaults to false.
+        /// </param>
+        public virtual void AddItemToStartOfPipeline(PipelineItem<TSyncDelegate> item, bool replaceInPlace = false)
+        {
+            this.AddItemToStartOfPipeline(this.Wrap(item), replaceInPlace);
+        }
+
+        /// <summary>
+        /// Add an item to the end of the pipeline
+        /// </summary>
+        /// <param name="item">Item to add</param>
+        public virtual void AddItemToEndOfPipeline(TAsyncDelegate item)
+        {
+            this.AddItemToEndOfPipeline((PipelineItem<TAsyncDelegate>)item);
+        }
+
+        /// <summary>
+        /// Add an item to the end of the pipeline
+        /// </summary>
+        /// <param name="item">Item to add</param>
+        public virtual void AddItemToEndOfPipeline(TSyncDelegate item)
+        {
+            this.AddItemToEndOfPipeline(this.Wrap(item));
+        }
+
+        /// <summary>
+        /// Add an item to the end of the pipeline
+        /// </summary>
+        /// <param name="item">Item to add</param>
+        /// <param name="replaceInPlace">
+        /// Whether to replace an existing item with the same name in its current place,
+        /// rather than at the position requested. Defaults to false.
+        /// </param>
+        public virtual void AddItemToEndOfPipeline(PipelineItem<TAsyncDelegate> item, bool replaceInPlace = false)
+        {
+            var existingIndex = this.RemoveByName(item.Name);
+
+            if (replaceInPlace && existingIndex != -1)
+            {
+                this.InsertItemAtPipelineIndex(existingIndex, item);
+            }
+            else
+            {
+                this.pipelineItems.Add(item);
+            }
+        }
+
+        /// <summary>
+        /// Add an item to the end of the pipeline
+        /// </summary>
+        /// <param name="item">Item to add</param>
+        /// <param name="replaceInPlace">
+        /// Whether to replace an existing item with the same name in its current place,
+        /// rather than at the position requested. Defaults to false.
+        /// </param>
+        public virtual void AddItemToEndOfPipeline(PipelineItem<TSyncDelegate> item, bool replaceInPlace = false)
+        {
+            this.AddItemToEndOfPipeline(this.Wrap(item), replaceInPlace);
+        }
+
+        /// <summary>
+        /// Add an item to a specific place in the pipeline.
+        /// </summary>
+        /// <param name="index">Index to add at</param>
+        /// <param name="item">Item to add</param>
+        public virtual void InsertItemAtPipelineIndex(int index, TAsyncDelegate item)
+        {
+            this.InsertItemAtPipelineIndex(index, (PipelineItem<TAsyncDelegate>)item);
+        }
+
+        /// <summary>
+        /// Add an item to a specific place in the pipeline.
+        /// </summary>
+        /// <param name="index">Index to add at</param>
+        /// <param name="item">Item to add</param>
+        public virtual void InsertItemAtPipelineIndex(int index, TSyncDelegate item)
+        {
+            this.InsertItemAtPipelineIndex(index, this.Wrap(item));
+        }
+
+        /// <summary>
+        /// Add an item to a specific place in the pipeline.
+        /// </summary>
+        /// <param name="index">Index to add at</param>
+        /// <param name="item">Item to add</param>
+        /// <param name="replaceInPlace">
+        /// Whether to replace an existing item with the same name in its current place,
+        /// rather than at the position requested. Defaults to false.
+        /// </param>
+        public virtual void InsertItemAtPipelineIndex(int index, PipelineItem<TAsyncDelegate> item, bool replaceInPlace = false)
+        {
+            var existingIndex = this.RemoveByName(item.Name);
+
+            var newIndex = (replaceInPlace && existingIndex != -1) ? existingIndex : index;
+
+            this.pipelineItems.Insert(newIndex, item);
+        }
+
+        /// <summary>
+        /// Add an item to a specific place in the pipeline.
+        /// </summary>
+        /// <param name="index">Index to add at</param>
+        /// <param name="item">Item to add</param>
+        /// <param name="replaceInPlace">
+        /// Whether to replace an existing item with the same name in its current place,
+        /// rather than at the position requested. Defaults to false.
+        /// </param>
+        public virtual void InsertItemAtPipelineIndex(int index, PipelineItem<TSyncDelegate> item, bool replaceInPlace = false)
+        {
+            this.InsertItemAtPipelineIndex(index, this.Wrap(item), replaceInPlace);
+        }
+
+        /// <summary>
+        /// Insert an item before a named item.
+        /// If the named item does not exist the item is inserted at the start of the pipeline.
+        /// </summary>
+        /// <param name="name">Name of the item to insert before</param>
+        /// <param name="item">Item to insert</param>
+        public virtual void InsertBefore(string name, TAsyncDelegate item)
+        {
+            this.InsertBefore(name, (PipelineItem<TAsyncDelegate>)item);
+        }
+
+        /// <summary>
+        /// Insert an item before a named item.
+        /// If the named item does not exist the item is inserted at the start of the pipeline.
+        /// </summary>
+        /// <param name="name">Name of the item to insert before</param>
+        /// <param name="item">Item to insert</param>
+        public virtual void InsertBefore(string name, TSyncDelegate item)
+        {
+            this.InsertBefore(name, this.Wrap(item));
+        }
+
+        /// <summary>
+        /// Insert an item before a named item.
+        /// If the named item does not exist the item is inserted at the start of the pipeline.
+        /// </summary>
+        /// <param name="name">Name of the item to insert before</param>
+        /// <param name="item">Item to insert</param>
+        public virtual void InsertBefore(string name, PipelineItem<TAsyncDelegate> item)
+        {
+            var existingIndex =
+                this.pipelineItems.FindIndex(i => String.Equals(name, i.Name, StringComparison.InvariantCulture));
+
+            if (existingIndex == -1)
+            {
+                existingIndex = 0;
+            }
+
+            this.InsertItemAtPipelineIndex(existingIndex, item);
+        }
+
+        /// <summary>
+        /// Insert an item before a named item.
+        /// If the named item does not exist the item is inserted at the start of the pipeline.
+        /// </summary>
+        /// <param name="name">Name of the item to insert before</param>
+        /// <param name="item">Item to insert</param>
+        public virtual void InsertBefore(string name, PipelineItem<TSyncDelegate> item)
+        {
+            this.InsertBefore(name, this.Wrap(item));
+        }
+
+        /// <summary>
+        /// Insert an item after a named item.
+        /// If the named item does not exist the item is inserted at the end of the pipeline.
+        /// </summary>
+        /// <param name="name">Name of the item to insert after</param>
+        /// <param name="item">Item to insert</param>
+        public virtual void InsertAfter(string name, TAsyncDelegate item)
+        {
+            this.InsertAfter(name, (PipelineItem<TAsyncDelegate>)item);
+        }
+
+        /// <summary>
+        /// Insert an item after a named item.
+        /// If the named item does not exist the item is inserted at the end of the pipeline.
+        /// </summary>
+        /// <param name="name">Name of the item to insert after</param>
+        /// <param name="item">Item to insert</param>
+        public virtual void InsertAfter(string name, TSyncDelegate item)
+        {
+            this.InsertAfter(name, this.Wrap(item));
+        }
+
+        /// <summary>
+        /// Insert an item after a named item.
+        /// If the named item does not exist the item is inserted at the end of the pipeline.
+        /// </summary>
+        /// <param name="name">Name of the item to insert after</param>
+        /// <param name="item">Item to insert</param>
+        public virtual void InsertAfter(string name, PipelineItem<TAsyncDelegate> item)
+        {
+            var existingIndex =
+                this.pipelineItems.FindIndex(i => String.Equals(name, i.Name, StringComparison.InvariantCulture));
+
+            if (existingIndex == -1)
+            {
+                existingIndex = this.pipelineItems.Count;
+            }
+
+            existingIndex++;
+
+            if (existingIndex > this.pipelineItems.Count)
+            {
+                this.AddItemToEndOfPipeline(item);
+            }
+            else
+            {
+                this.InsertItemAtPipelineIndex(existingIndex, item);
+            }
+        }
+
+        /// <summary>
+        /// Insert an item after a named item.
+        /// If the named item does not exist the item is inserted at the end of the pipeline.
+        /// </summary>
+        /// <param name="name">Name of the item to insert after</param>
+        /// <param name="item">Item to insert</param>
+        public virtual void InsertAfter(string name, PipelineItem<TSyncDelegate> item)
+        {
+            this.InsertAfter(name, this.Wrap(item));
+        }
+
+        /// <summary>
+        /// Remove a named pipeline item
+        /// </summary>
+        /// <param name="name">Name</param>
+        /// <returns>Index of item that was removed or -1 if nothing removed</returns>
+        public virtual int RemoveByName(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                return -1;
+            }
+
+            var existingIndex =
+                this.pipelineItems.FindIndex(i => String.Equals(name, i.Name, StringComparison.InvariantCulture));
+
+            if (existingIndex != -1)
+            {
+                this.pipelineItems.RemoveAt(existingIndex);
+            }
+
+            return existingIndex;
+        }
+
+        /// <summary>
+        /// Wraps a sync delegate into it's async form
+        /// </summary>
+        /// <param name="syncDelegate">Sync delegate instance</param>
+        /// <returns>Async delegate instance</returns>
+        protected abstract TAsyncDelegate Wrap(TSyncDelegate syncDelegate);
+    }
+}
--- src/Nancy/BeforePipeline.cs
+++ src/Nancy/BeforePipeline.cs
@@ -1,45 +1,41 @@
 ﻿namespace Nancy
 {
     using System;
-    using System.Linq;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
 
-    /// <summary>
-    /// <para>
-    /// A simple pipeline for pre-request hooks.
-    /// Hooks will be executed until either a hook returns a response, or every
-    /// hook has been executed.
-    /// </para>
-    /// <para>
-    /// Can be implictly cast to/from the pre-request hook delegate signature
-    /// (Func NancyContext, Response) for assigning to NancyEngine or for building
-    /// composite pipelines.
-    /// </para>
-    /// </summary>
-    public class BeforePipeline : NamedPipelineBase<Func<NancyContext, Response>> 
+    public class BeforePipeline : AsyncNamedPipelineBase<Func<NancyContext, Task<Response>>, Func<NancyContext, Response>>
     {
         public BeforePipeline()
         {
         }
 
-        public BeforePipeline(int capacity) : base(capacity)
+        public BeforePipeline(int capacity)
+            : base(capacity)
         {
         }
 
-        public static implicit operator Func<NancyContext, Response>(BeforePipeline pipeline)
+        public static implicit operator Func<NancyContext, Task<Response>>(BeforePipeline pipeline)
         {
             return pipeline.Invoke;
         }
 
-        public static implicit operator BeforePipeline(Func<NancyContext, Response> func)
+        public static implicit operator BeforePipeline(Func<NancyContext, Task<Response>> func)
         {
             var pipeline = new BeforePipeline();
             pipeline.AddItemToEndOfPipeline(func);
             return pipeline;
         }
 
-        public static BeforePipeline operator +(BeforePipeline pipeline, Func<NancyContext, Response> func)
+        public static BeforePipeline operator +(BeforePipeline pipeline, Func<NancyContext, Task<Response>> func)
         {
             pipeline.AddItemToEndOfPipeline(func);
+            return pipeline;
+        }
+
+        public static BeforePipeline operator +(BeforePipeline pipeline, Func<NancyContext, Response> action)
+        {
+            pipeline.AddItemToEndOfPipeline(action);
             return pipeline;
         }
 
@@ -53,29 +49,132 @@
             return pipelineToAddTo;
         }
 
-        /// <summary>
-        /// Invoke the pipeline. Each item will be invoked in turn until either an
-        /// item returns a Response, or all items have beene invoked.
-        /// </summary>
-        /// <param name="context">
-        /// The current context to pass to the items.
-        /// </param>
-        /// <returns>
-        /// Response from an item invocation, or null if no response was generated.
-        /// </returns>
-        public Response Invoke(NancyContext context)
+        public Task<Response> Invoke(NancyContext context)
         {
-            Response returnValue = null;
+            var tcs = new TaskCompletionSource<Response>();
 
-            using (var enumerator = this.PipelineDelegates.GetEnumerator())
+            var enumerator = this.PipelineDelegates.GetEnumerator();
+
+            if (enumerator.MoveNext())
             {
-                while (returnValue == null && enumerator.MoveNext())
-                {
-                    returnValue = enumerator.Current.Invoke(context);
-                }
+                ExecuteTasksWithSingleResultInternal(context, enumerator, tcs);
             }
 
-            return returnValue;
+            return tcs.Task;
+        }
+
+        private static void ExecuteTasksWithSingleResultInternal(NancyContext context, IEnumerator<Func<NancyContext, Task<Response>>> enumerator, TaskCompletionSource<Response> tcs)
+        {
+            // Endless loop to try and optimise the "main" use case of 
+            // our tasks just being delegates wrapped in a task.
+            //
+            // If they finish executing before returning them we will
+            // just loop around this while loop running them one by one,
+            // as soon as we have to return, or a task is actually async,
+            // then we will bale out and set a continuation.
+            while (true)
+            {
+                var current = enumerator.Current.Invoke(context);
+
+                if (current.Status == TaskStatus.Created)
+                {
+                    current.Start();
+                }
+
+                if (current.IsCompleted || current.IsFaulted)
+                {
+                    var resultTask = current;
+
+                    // Task has already completed, so don't bother with continuations
+                    if (ContinueExecution(current.IsFaulted, current.Result, current.Exception))
+                    {
+                        if (enumerator.MoveNext())
+                        {
+                            continue;
+                        }
+
+                        resultTask = null;
+                    }
+
+                    ExecuteTasksSingleResultFinished(resultTask, tcs);
+
+                    break;
+                }
+
+                // Task hasn't finished - set a continuation and bail out of the loop
+                current.ContinueWith(ExecuteTasksWithSingleResultContinuation(context, enumerator, tcs), TaskContinuationOptions.ExecuteSynchronously);
+                break;
+            }
+        }
+
+        private static Action<Task<Response>> ExecuteTasksWithSingleResultContinuation(NancyContext context, IEnumerator<Func<NancyContext, Task<Response>>> enumerator, TaskCompletionSource<Response> tcs)
+        {
+            return t =>
+            {
+                if (ContinueExecution(t.IsFaulted, t.Result, t.Exception))
+                {
+                    if (enumerator.MoveNext())
+                    {
+                        ExecuteTasksWithSingleResultInternal(context, enumerator, tcs);
+                    }
+                    else
+                    {
+                        ExecuteTasksSingleResultFinished(null, tcs);
+                    }
+                }
+                else
+                {
+                    ExecuteTasksSingleResultFinished(t, tcs);
+                }
+            };
+        }
+
+        private static void ExecuteTasksSingleResultFinished(Task<Response> task, TaskCompletionSource<Response> tcs)
+        {
+            if (task == null)
+            {
+                tcs.SetResult(default(Response));
+                return;
+            }
+
+            if (task.IsFaulted)
+            {
+                tcs.SetException(task.Exception);
+            }
+            else
+            {
+                tcs.SetResult(task.Result);
+            }
+        }
+
+        private static bool ContinueExecution(bool isFaulted, Response result, AggregateException exception)
+        {
+            return result == null;
+        }
+
+        /// <summary>
+        /// Wraps a sync delegate into it's async form
+        /// </summary>
+        /// <param name="syncDelegate">Sync delegate instance</param>
+        /// <returns>Async delegate instance</returns>
+        protected override Func<NancyContext, Task<Response>> Wrap(Func<NancyContext, Response> syncDelegate)
+        {
+            return ctx =>
+            {
+                var tcs = new TaskCompletionSource<Response>();
+                try
+                {
+                    var result = syncDelegate.Invoke(ctx);
+
+                    tcs.SetResult(result);
+                }
+                catch (Exception e)
+                {
+                    tcs.SetException(e);
+                }
+
+                return tcs.Task;
+            };
         }
     }
 } 
