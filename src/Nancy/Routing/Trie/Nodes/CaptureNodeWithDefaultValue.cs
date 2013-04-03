a00284e551275c3eca80cc9d8b734ea65c155997
Phillip Haydon <phillip.haydon@gmail.com>
2013-04-03 23:12:30 +0800

    updated TriedNodeFactory references to use interface

--- src/Nancy/Routing/Trie/Nodes/CaptureNodeWithDefaultValue.cs
+++ src/Nancy/Routing/Trie/Nodes/CaptureNodeWithDefaultValue.cs
@@ -19,7 +19,7 @@ namespace Nancy.Routing.Trie.Nodes
             get { return 1000; }
         }
 
-        public CaptureNodeWithDefaultValue(TrieNode parent, string segment, TrieNodeFactory nodeFactory)
+        public CaptureNodeWithDefaultValue(TrieNode parent, string segment, ITrieNodeFactory nodeFactory)
             : base(parent, segment, nodeFactory)
         {
             this.ExtractParameterNameAndDefaultValue();
