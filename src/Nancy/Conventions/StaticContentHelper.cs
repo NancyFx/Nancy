8e8d2165b3148f778e460d3d660b073377d7712a
David Spörri <davepermen@hotmail.com>
2013-04-08 01:11:17 +0200

    Added StaticContent helpers
    A tiny DSL for adding static files and directories in the Bootstrapper

---
+++ src/Nancy/Conventions/StaticContentHelper.cs
@@ -0,0 +1,25 @@
+﻿namespace Nancy.Conventions
+{
+    using System;
+
+    /// <summary>
+    /// Nancy static convention helper
+    /// </summary>
+    public static class StaticContentHelper
+    {
+        /// <summary>
+        /// Extension method for NancyConventions
+        /// 
+        /// conventions.Static((File, Directory) =>
+        /// {
+        ///     File["/page.js"] = "page.js";
+        ///    Directory["/images"] = "images";
+        /// });
+        /// </summary>
+        /// <param name="staticConventions">The callback method allows you to describe the static content</param>
+        public static void Static(this NancyConventions conventions, Action<StaticFileContent, StaticDirectoryContent> staticConventions)
+        {
+            staticConventions(new StaticFileContent(conventions), new StaticDirectoryContent(conventions));
+        }
+    }
+}
---
+++ src/Nancy/Conventions/StaticDirectoryContent.cs
@@ -0,0 +1,31 @@
+﻿namespace Nancy.Conventions
+{
+    /// <summary>
+    /// Nancy static directory convention helper
+    /// </summary>
+    public class StaticDirectoryContent
+    {
+        NancyConventions conventions;
+
+        /// <summary>
+        /// Creates a new instance of StaticDirectoryContent
+        /// </summary>
+        /// <param name="conventions">NancyConventions, to wich static directories get added</param>
+        public StaticDirectoryContent(NancyConventions conventions)
+        {
+            this.conventions = conventions;
+        }
+
+        /// <summary>
+        /// Adds a new static directory to the nancy conventions
+        /// </summary>
+        /// <param name="requestFile">The route of the file</param>
+        public string this[string requestDirectory]
+        {
+            set
+            {
+                conventions.StaticContentsConventions.Add(StaticContentConventionBuilder.AddDirectory(requestDirectory, value));
+            }
+        }
+    }
+}
