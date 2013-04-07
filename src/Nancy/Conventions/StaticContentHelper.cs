f6b677cc155324b179671893dd6bb61e3be6744f
davepermen <davepermen@hotmail.com>
2013-04-08 02:19:56 +0300

    Update StaticContentHelper.cs

--- src/Nancy/Conventions/StaticContentHelper.cs
+++ src/Nancy/Conventions/StaticContentHelper.cs
@@ -13,13 +13,13 @@
         /// conventions.Static((File, Directory) =>
         /// {
         ///     File["/page.js"] = "page.js";
-        ///    Directory["/images"] = "images";
+        ///     Directory["/images"] = "images";
         /// });
         /// </summary>
         /// <param name="staticConventions">The callback method allows you to describe the static content</param>
         public static void Static(this NancyConventions conventions, Action<StaticFileContent, StaticDirectoryContent> staticConventions)
         {
-            staticConventions(new StaticFileContent(conventions), new StaticDirectoryContent(conventions));
+            this.staticConventions(new StaticFileContent(conventions), new StaticDirectoryContent(conventions));
         }
     }
 }
