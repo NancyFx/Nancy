849b1b1107e20f0449affa21da09b89b956651d0
Steven Robbins <ste.robbins@gmail.com>
2013-06-26 12:00:48 +0100

    Consolidated travis script

--- travis.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo Running on Mono 2.x
-rake mono
-echo Running on Mono 3.x
-sudo tar xk -C "/opt" -f /opt/mono-3.0.12-bin.tar.bz2
-export PATH="/opt/mono/bin:$PATH"
-export LD_LIBRARY_PATH="/opt/mono/bin"
-rake mono
