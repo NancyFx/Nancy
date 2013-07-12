0731946e5fc6016a3fdb234b6615d507751d64de
James R Sconfitto <james@exele.com>
2013-07-12 08:53:32 -0400

    Forgot DotLiquid ApplicationRegistration class

---
+++ src/Nancy.ViewEngines.DotLiquid/DotLiquidApplicationRegistrations.cs
@@ -0,0 +1,13 @@
+﻿namespace Nancy.ViewEngines.DotLiquid
+{
+    using Nancy.Bootstrapper;
+    using global::DotLiquid.NamingConventions;
+
+    public class DotLiquidApplicationRegistrations : ApplicationRegistrations
+    {
+        public DotLiquidApplicationRegistrations()
+        {
+            this.RegisterWithDefault<INamingConvention>(typeof(RubyNamingConvention));
+        }
+    }
+}
