eb630dc925f2089447ff9a21f3d42ddbcab6f557
James R Sconfitto <james@exele.com>
2013-07-12 09:39:24 -0400

    Document `DotLiquidApplicationRegistrations`

--- src/Nancy.ViewEngines.DotLiquid/DotLiquidApplicationRegistrations.cs
+++ src/Nancy.ViewEngines.DotLiquid/DotLiquidApplicationRegistrations.cs
@@ -3,6 +3,12 @@
     using Nancy.Bootstrapper;
     using global::DotLiquid.NamingConventions;
 
+    /// <summary>
+    /// Used to register DotLiquid's RubyNamingConvention as the default for the ViewEngine. The naming convention affect DotLiquid Drops and filters.
+    /// See <a href="https://github.com/formosatek/dotliquid/wiki/DotLiquid-Drops#tips">DotLiquid's documentation</a> on the subject for more information.
+    ///
+    /// This can be overridden in a bootstrapper.
+    /// </summary>
     public class DotLiquidApplicationRegistrations : ApplicationRegistrations
     {
         public DotLiquidApplicationRegistrations()
