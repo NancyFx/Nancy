ae72420c443031a4454bb7d55d7f87100fa7afe2
Damian Hickey <dhickey@gmail.com>
2013-09-16 09:05:33 +0100

    Add failing test to check that nancy appends Set-Cookie response header

--- src/Nancy.Owin.Tests/NancyOwinHostFixture.cs
+++ src/Nancy.Owin.Tests/NancyOwinHostFixture.cs
@@ -145,6 +145,27 @@ namespace Nancy.Owin.Tests
             (respHeaders["Set-Cookie"][1] == "test1=testvalue1; path=/").ShouldBeTrue();
         }
 
+        [Fact]
+        public void Should_append_setcookie_headers()
+        {
+            //Given
+            var respHeaders = Get<IDictionary<string, string[]>>(this.environment, "owin.ResponseHeaders");
+            respHeaders.Add("Set-Cookie", new[] { "other=othervalue; path=/" });
+
+            var fakeResponse = new Response { StatusCode = HttpStatusCode.OK };
+            fakeResponse.AddCookie("test", "testvalue");
+            var fakeContext = new NancyContext { Response = fakeResponse };
+            this.SetupFakeNancyCompleteCallback(fakeContext);
+
+            //When
+            this.host.Invoke(this.environment).Wait();
+
+            //Then
+            respHeaders["Set-Cookie"].Length.ShouldEqual(2);
+            (respHeaders["Set-Cookie"][0] == "other=othervalue; path=/").ShouldBeTrue();
+            (respHeaders["Set-Cookie"][1] == "test1=testvalue1; path=/").ShouldBeTrue();
+        }
+
         /// <summary>
         /// Sets the fake nancy engine to execute the complete callback with the given context
         /// </summary>
