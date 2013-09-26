f8221a52cd75d0608bff59d4bd1c4f2bb0b21f41
Kristian Hellang <kristian@hellang.com>
2013-09-27 01:13:08 +0200

    Made serializer in BrowserResponseBodyWrapper extensions pluggable

--- src/Nancy.Testing/BrowserResponseBodyWrapperExtensions.cs
+++ src/Nancy.Testing/BrowserResponseBodyWrapperExtensions.cs
@@ -1,12 +1,12 @@
 namespace Nancy.Testing
 {
-    using System;
     using System.IO;
     using System.Linq;
     using System.Text;
     using System.Xml;
-    using System.Xml.Serialization;
-    using Nancy.Json;
+
+    using Nancy.ModelBinding;
+    using Nancy.ModelBinding.DefaultBodyDeserializers;
 
     /// <summary>
     /// Extension method for formatting the contents of a <see cref="BrowserResponseBodyWrapper"/>.
@@ -40,51 +40,51 @@ namespace Nancy.Testing
         /// <value>A <see cref="XmlDocument"/> representation of the HTTP response body.</value>
         public static XmlDocument AsXmlDocument(this BrowserResponseBodyWrapper bodyWrapper)
         {
-            var document =
-                new XmlDocument();
+            var document = new XmlDocument();
+
             document.LoadXml(bodyWrapper.AsString());
 
             return document;
         }
 
         /// <summary>
-        /// Gets the deserialized representation of the JSON in the response body.
+        /// Gets the deserialized representation of the JSON in the response body using the default XML body deserializer.
         /// </summary>
         /// <typeparam name="TModel">The type that the JSON response body should be deserialized to.</typeparam>
         /// <param name="bodyWrapper">An instance of the <see cref="BrowserResponseBodyWrapper"/> that the extension should be invoked on.</param>
         /// <value>A <typeparamref name="TModel"/> instance representation of the HTTP response body.</value>
         public static TModel DeserializeJson<TModel>(this BrowserResponseBodyWrapper bodyWrapper)
         {
-            var serializer = 
-                new JavaScriptSerializer();
+            var bodyDeserializer = new JsonBodyDeserializer();
 
-            return bodyWrapper.DeserializeJson<TModel>(serializer);
+            return bodyWrapper.Deserialize<TModel>(bodyDeserializer);
         }
 
         /// <summary>
-        /// Gets the deserialized representation of the JSON in the response body.
-        /// </summary>
-        /// <typeparam name="TModel">The type that the JSON response body should be deserialized to.</typeparam>
-        /// <param name="bodyWrapper">An instance of the <see cref="BrowserResponseBodyWrapper"/> that the extension should be invoked on.</param>
-        /// <param name="serializer">An instance of <see cref="JavaScriptSerializer"/>, controlling the deserialization.</param>
-        /// <value>A <typeparamref name="TModel"/> instance representation of the HTTP response body.</value>
-        public static TModel DeserializeJson<TModel>(this BrowserResponseBodyWrapper bodyWrapper, JavaScriptSerializer serializer)
-        {
-            return serializer.Deserialize<TModel>(bodyWrapper.AsString());
-        }
-
-        /// <summary>
-        /// Gets the deserialized representation of the XML in the response body.
+        /// Gets the deserialized representation of the XML in the response body using the default XML body deserializer.
         /// </summary>
         /// <typeparam name="TModel">The type that the XML response body should be deserialized to.</typeparam>
         /// <param name="bodyWrapper">An instance of the <see cref="BrowserResponseBodyWrapper"/> that the extension should be invoked on.</param>
         /// <value>A <typeparamref name="TModel"/> instance representation of the HTTP response body.</value>
         public static TModel DeserializeXml<TModel>(this BrowserResponseBodyWrapper bodyWrapper)
         {
-            var serializer =
-                new XmlSerializer(typeof(TModel));
+            var bodyDeserializer = new XmlBodyDeserializer();
 
-            return (TModel)serializer.Deserialize(bodyWrapper.AsStream());
+            return bodyWrapper.Deserialize<TModel>(bodyDeserializer);
+        }
+
+        /// <summary>
+        /// Gets the deserialized representation of the response body using the specified body deserializer.
+        /// </summary>
+        /// <typeparam name="TModel">The type that the response body should be deserialized to.</typeparam>
+        /// <param name="bodyWrapper">An instance of the <see cref="BrowserResponseBodyWrapper"/> that the extension should be invoked on.</param>
+        /// <param name="bodyDeserializer">An instance of the <see cref="IBodyDeserializer"/> that should be used to deserialize the response body.</param>
+        /// <value>A <typeparamref name="TModel"/> instance representation of the HTTP response body.</value>
+        public static TModel Deserialize<TModel>(this BrowserResponseBodyWrapper bodyWrapper, IBodyDeserializer bodyDeserializer)
+        {
+            var bindingContext = new BindingContext { DestinationType = typeof(TModel) };
+
+            return (TModel)bodyDeserializer.Deserialize(bodyWrapper.ContentType, bodyWrapper.AsStream(), bindingContext);
         }
     }
 }
