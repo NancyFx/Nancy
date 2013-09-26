a5937ce9a0a659d6f2109af1b81d0d638087fbf3
marcusoft <marcusoft.net@gmail.com>
2013-09-26 20:56:46 +0200

    Added TestingViewFactory and some basic unit tests

---
+++ src/Nancy.Testing.Tests/TestingViewFactoryGetModelTests.cs
@@ -0,0 +1,98 @@
+﻿namespace Nancy.Testing.Tests
+{
+    using Xunit;
+
+    public class TestingViewFactoryGetModelTests
+    {
+        private readonly BrowserResponse response;
+        private readonly Browser _browser;
+
+        public TestingViewFactoryGetModelTests()
+        {
+            this._browser = new Browser(with => 
+            {
+                with.Module<TestingViewFactoryTestModule>();
+                with.ViewFactory<TestingViewFactory>();
+            });
+
+            this.response = this._browser.Get("/testingViewFactory");
+        }
+
+        [Fact]
+        public void should_return_model_of_correct_type()
+        {
+            Assert.IsType<ViewFactoryTestModel>(this.response.GetModel<ViewFactoryTestModel>());
+        }
+
+        [Fact]
+        public void should_set_model()
+        {
+            Assert.NotNull(this.response.GetModel<ViewFactoryTestModel>());
+        }
+
+        [Fact]
+        public void should_set_values_correct_on_the_model()
+        {
+            var model = this.response.GetModel<ViewFactoryTestModel>();
+            Assert.Equal("A value", model.AString);
+            Assert.Equal("Another value", model.ComplexModel.AnotherString);
+        }
+
+        [Fact]
+        public void should_set_the_view_name()
+        {
+            Assert.Equal("ViewFactoryTest.sshtml", response.GetViewName());
+        }
+
+        [Fact]
+        public void should_set_the_view_name_to_empty_when_no_view_name_is_given()
+        {
+            var response = this._browser.Get("/testingViewFactoryNoViewName");
+            Assert.Equal(string.Empty, response.GetViewName());
+        }
+
+        [Fact]
+        public void should_set_the_module_name()
+        {
+            Assert.Equal("TestingViewFactoryTest", response.GetModuleName());
+        }
+
+        [Fact]
+        public void should_set_the_module_path_to_empty_for_modules_in_root_catalog()
+        {
+            Assert.Equal(string.Empty, response.GetModulePath());
+        }
+    }
+
+    // Test module for TestingViewFactory
+
+    public class TestingViewFactoryTestModule : NancyModule
+    {
+        public TestingViewFactoryTestModule()
+        {
+            Get["/testingViewFactory"] = _ => this.View["ViewFactoryTest.sshtml", GetModel()];
+            Get["/testingViewFactoryNoViewName"] = _ => this.View[GetModel()];
+        }
+
+        private static ViewFactoryTestModel GetModel()
+        {
+            var model = new ViewFactoryTestModel
+            {
+                AString = "A value",
+                ComplexModel = new CompositeTestModel {AnotherString = "Another value"}
+            };
+            return model;
+        }
+    }
+
+    public class ViewFactoryTestModel
+    {
+        public string AString { get; set; }
+        public CompositeTestModel ComplexModel { get; set; }
+    }
+
+    public class CompositeTestModel
+    {
+        public string AnotherString { get; set; }
+    }
+}
---
+++ src/Nancy.Testing/TestingViewFactory.cs
@@ -0,0 +1,94 @@
+﻿namespace Nancy.Testing
+{
+    using Nancy.ViewEngines;
+
+    /// <summary>
+    /// A view factory decorator, aimed for test,
+    /// that exposes some interesting properties about the generated view
+    /// </summary>
+    public class TestingViewFactory : IViewFactory
+    {
+        private readonly DefaultViewFactory defaultViewFactory;
+
+        /// <summary>
+        /// Creates the view based on the view factory sent to the constructor
+        /// </summary>
+        /// <param name="viewFactory">the view factory that is decorated</param>
+        public TestingViewFactory(DefaultViewFactory viewFactory)
+        {
+            this.defaultViewFactory = viewFactory;
+        }
+
+        /// <summary>
+        /// Renders the view and then call into the viewfactory 
+        /// that the TestingViewFactory is decorating
+        /// </summary>
+        /// <param name="viewName">The name of the view to render.</param>
+        /// <param name="model">The module path of the module that is rendering the view.</param>
+        /// <param name="viewLocationContext">A <see cref="ViewLocationContext"/> instance, containing information about the context for which the view is being rendered.</param>
+        /// <returns>A response.</returns>
+        public Response RenderView(string viewName, dynamic model, ViewLocationContext viewLocationContext)
+        {
+            // Intercept and store interesting stuff
+            viewLocationContext.Context.Items["###ViewModel###"] = model;
+            viewLocationContext.Context.Items["###ViewName###"] = viewName;
+            viewLocationContext.Context.Items["###ModuleName###"] = viewLocationContext.ModuleName;
+            viewLocationContext.Context.Items["###ModulePath###"] = viewLocationContext.ModulePath;
+
+            return this.defaultViewFactory.RenderView(viewName, model, viewLocationContext);
+        }
+    }
+
+    /// <summary>
+    /// Extension methods for easy access of the properties
+    /// stored in the view context by the testing view factory
+    /// </summary>
+    public static class TestingViewBrowserResponseExtensions
+    {
+        /// <summary>
+        /// Get the model on the view
+        /// </summary>
+        /// <typeparam name="TType">the type of the model</typeparam>
+        /// <param name="response">The <see cref="BrowserResponse"/> that the assert should be made on.</param>
+        /// <returns>a model of the <typeparam name="TType">type</typeparam></returns>
+        public static TType GetModel<TType>(this BrowserResponse response)
+        {
+            return (TType)response.Context.Items["###ViewModel###"];
+        }
+
+        /// <summary>
+        /// Returns the name of the view
+        /// </summary>
+        /// <param name="response">The <see cref="BrowserResponse"/> that the assert should be made on.</param>
+        /// <returns>the name of the view</returns>
+        public static string GetViewName(this BrowserResponse response)
+        {
+            var value = (string)response.Context.Items["###ViewName###"];
+            return string.IsNullOrEmpty(value) ? string.Empty : value;
+        }
+
+        /// <summary>
+        /// Returns the name of the module
+        /// </summary>
+        /// <param name="response">The <see cref="BrowserResponse"/> that the assert should be made on.</param>
+        /// <returns>the name of the module</returns>
+        public static string GetModuleName(this BrowserResponse response)
+        {
+            var value = (string)response.Context.Items["###ModuleName###"];
+            return string.IsNullOrEmpty(value) ? string.Empty : value;
+        }
+
+        /// <summary>
+        /// Returns the name of the module
+        /// </summary>
+        /// <param name="response">The <see cref="BrowserResponse"/> that the assert should be made on.</param>
+        /// <returns>the name of the module</returns>
+        public static string GetModulePath(this BrowserResponse response)
+        {
+            var value = (string)response.Context.Items["###ModulePath###"];
+            return string.IsNullOrEmpty(value) ? string.Empty : value;
+        }
+
+    }
+
+}
