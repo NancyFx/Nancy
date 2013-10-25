687ccb487979dc3559ef08ea13606b33bdc7ba5b
Steven Robbins <ste.robbins@gmail.com>
2013-10-25 07:58:25 +0000

    Added comments

--- src/Nancy/Session/Session.cs
+++ src/Nancy/Session/Session.cs
@@ -4,6 +4,9 @@ namespace Nancy.Session
     using System.Collections;
     using System.Collections.Generic;
 
+    /// <summary>
+    /// Session implementation
+    /// </summary>
     public class Session : ISession
     {
         private readonly IDictionary<string, object> dictionary;
@@ -15,11 +18,17 @@ namespace Nancy.Session
             this.dictionary = dictionary;
         }
 
+        /// <summary>
+        /// Gets the number of items stored
+        /// </summary>
         public int Count
         {
             get { return dictionary.Count; }
         }
 
+        /// <summary>
+        /// Deletes all items
+        /// </summary>
         public void DeleteAll()
         {
             if (Count > 0)
@@ -30,11 +39,20 @@ namespace Nancy.Session
             dictionary.Clear();            
         }
 
+        /// <summary>
+        /// Delete an item with the given key
+        /// </summary>
+        /// <param name="key">Key to delete</param>
         public void Delete(string key)
         {
             if (dictionary.Remove(key)) { MarkAsChanged(); }            
         }
 
+        /// <summary>
+        /// Gets or sets values
+        /// </summary>
+        /// <param name="key">The key whos value to get or set</param>
+        /// <returns>The value, or null or the key didn't exist</returns>
         public object this[string key]
         {
             get { return dictionary.ContainsKey(key) ? dictionary[key] : null; }
@@ -52,15 +70,33 @@ namespace Nancy.Session
             }
         }
 
+        /// <summary>
+        /// Gets whether the session has changed
+        /// </summary>
         public bool HasChanged
         {
             get { return this.hasChanged; }
         }
 
+        /// <summary>
+        /// Returns an enumerator that iterates through a collection.
+        /// </summary>
+        /// <returns>
+        /// An <see cref="T:System.Collections.IEnumerator"/> object that can be used to iterate through the collection.
+        /// </returns>
+        /// <filterpriority>2</filterpriority>
         IEnumerator IEnumerable.GetEnumerator()
         {
             return this.GetEnumerator();
         }
+
+        /// <summary>
+        /// Returns an enumerator that iterates through the collection.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="T:System.Collections.Generic.IEnumerator`1"/> that can be used to iterate through the collection.
+        /// </returns>
+        /// <filterpriority>1</filterpriority>
         public IEnumerator<KeyValuePair<string, object>> GetEnumerator()
         {
             return dictionary.GetEnumerator();
