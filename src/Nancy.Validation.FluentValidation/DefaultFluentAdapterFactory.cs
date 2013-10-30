6e5537e8a61b07710b6986c1bd35f522553dc054
Andreas Hakansson <andreas@selfinflicted.org>
2013-10-30 21:49:33 +0100

    Removed unused using statement

--- src/Nancy.Validation.FluentValidation/DefaultFluentAdapterFactory.cs
+++ src/Nancy.Validation.FluentValidation/DefaultFluentAdapterFactory.cs
@@ -2,8 +2,6 @@ namespace Nancy.Validation.FluentValidation
 {
     using System.Collections.Generic;
     using System.Linq;
-
-    using global::FluentValidation.Internal;
     using global::FluentValidation.Validators;
 
     /// <summary>
