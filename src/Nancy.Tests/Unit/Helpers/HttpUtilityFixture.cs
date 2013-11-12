dd4b292585f79b434266d4c7817118e7528a902d
Anders Fjeldstad <anders.fjeldstad@gmail.com>
2013-11-13 00:35:56 +0100

    Make Request.ParseFormData respect StaticConfiguration.CaseSensitive.

--- src/Nancy.Tests/Unit/Helpers/HttpUtilityFixture.cs
+++ src/Nancy.Tests/Unit/Helpers/HttpUtilityFixture.cs
@@ -34,5 +34,35 @@
             collection["key"].ShouldEqual("value");
             collection["KEY"].ShouldBeNull();
         }
+
+        [Fact]
+        public void ParseQueryString_handles_duplicate_keys_when_case_insensitive()
+        {
+            // Given
+            StaticConfiguration.CaseSensitive = false;
+            var query = "key=value&key=value&KEY=VALUE";
+
+            // When
+            var collection = HttpUtility.ParseQueryString(query);
+
+            // Then
+            collection["key"].ShouldEqual("value,value,VALUE");
+            collection["KEY"].ShouldEqual("value,value,VALUE");
+        }
+
+        [Fact]
+        public void ParseQueryString_handles_duplicate_keys_when_case_sensitive()
+        {
+            // Given
+            StaticConfiguration.CaseSensitive = true;
+            var query = "key=value&key=value&KEY=VALUE";
+
+            // When
+            var collection = HttpUtility.ParseQueryString(query);
+
+            // Then
+            collection["key"].ShouldEqual("value,value");
+            collection["KEY"].ShouldEqual("VALUE");
+        }
     }
 }
