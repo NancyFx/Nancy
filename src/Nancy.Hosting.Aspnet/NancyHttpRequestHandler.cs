d1d55712b5e900ef5ee7f234e7b5f3be7e176825
Jason Fistner <jason.fistner@gmail.com>
2013-12-11 17:32:51 -0500

    change to string.Concat

--- src/Nancy.Hosting.Aspnet/NancyHttpRequestHandler.cs
+++ src/Nancy.Hosting.Aspnet/NancyHttpRequestHandler.cs
@@ -37,7 +37,7 @@ namespace Nancy.Hosting.Aspnet
             if (configurationBootstrapperType != null)
             {
                 var bootstrapperType =
-                    Type.GetType(configurationBootstrapperType.Name + ", " + configurationBootstrapperType.Assembly);
+                    Type.GetType(string.Concat(configurationBootstrapperType.Name, ", ", configurationBootstrapperType.Assembly));
 
                 return Activator.CreateInstance(bootstrapperType) as INancyBootstrapper;
             }
