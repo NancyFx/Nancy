737b7ed16a06b5fffbcd15c3b7564f5a1fa0a6b4
Łukasz Wiśniewski <l.wisniewski@xfer.pl>
2013-12-12 22:14:24 +0100

    Added 'Given, When, Then' and changed asserts to shoulds in tests.

--- src/Nancy.Tests/Unit/ErrorPipelineFixture.cs
+++ src/Nancy.Tests/Unit/ErrorPipelineFixture.cs
@@ -22,31 +22,37 @@
         [Fact]
         public void PlusEquals_with_func_adds_item_to_end_of_pipeline()
         {
-        	pipeline += (ctx, ex) => null;
-        	pipeline += (ctx, ex) => null;
+            // Given, When
+            pipeline += (ctx, ex) => null;
+            pipeline += (ctx, ex) => null;
 
-            Assert.Equal(2, pipeline.PipelineDelegates.Count());
+            // Then
+            pipeline.PipelineDelegates.ShouldHaveCount(2);
         }
 
         [Fact]
         public void PlusEquals_with_another_pipeline_adds_those_pipeline_items_to_end_of_pipeline()
         {
+            // Given
             pipeline.AddItemToEndOfPipeline((ctx, ex) => null);
             pipeline.AddItemToEndOfPipeline((ctx, ex) => null);
             var pipeline2 = new ErrorPipeline();
             pipeline2.AddItemToEndOfPipeline((ctx, ex) => null);
             pipeline2.AddItemToEndOfPipeline((ctx, ex) => null);
 
+            // When
             pipeline += pipeline2;
 
-            Assert.Equal(4, pipeline.PipelineItems.Count());
-            Assert.Same(pipeline2.PipelineDelegates.ElementAt(0), pipeline.PipelineDelegates.ElementAt(2));
-            Assert.Same(pipeline2.PipelineDelegates.ElementAt(1), pipeline.PipelineDelegates.Last());
+            // Then
+            pipeline.PipelineItems.ShouldHaveCount(4);
+            pipeline2.PipelineDelegates.ElementAt(0).ShouldBeSameAs(pipeline.PipelineDelegates.ElementAt(2));
+            pipeline2.PipelineDelegates.ElementAt(1).ShouldBeSameAs(pipeline.PipelineDelegates.Last());
         }
 
         [Fact]
         public void When_cast_to_func_and_invoked_members_are_invoked()
         {
+            // Given
             var item1Called = false;
             Func<NancyContext, Exception, Response> item1 = (ctx, ex) => { item1Called = true; return null; };
             var item2Called = false;
@@ -57,28 +63,32 @@
             pipeline.AddItemToEndOfPipeline(item2);
             pipeline.AddItemToEndOfPipeline(item3);
 
-            Func<NancyContext, Exception, Response> func = (ctx, ex) => null;
-            pipeline += func;
-
+            // When
             pipeline.Invoke(CreateContext(), new Exception());
 
-            Assert.True(item1Called);
-            Assert.True(item2Called);
-            Assert.True(item3Called);
+            // Then
+            item1Called.ShouldBeTrue();
+            item2Called.ShouldBeTrue();
+            item3Called.ShouldBeTrue();
         }
 
         [Fact]
         public void When_cast_from_func_creates_a_pipeline_with_one_item()
         {
+            // Given
             var castPipeline = new ErrorPipeline();
+            
+            // When
             castPipeline += (ctx, ex) => null;
 
-            Assert.Equal(1, castPipeline.PipelineDelegates.Count());
+            // Then
+            castPipeline.PipelineDelegates.ShouldHaveCount(1);
         }
 
         [Fact]
         public void Pipeline_containing_another_pipeline_will_invoke_items_in_both_pipelines()
         {
+            // Given
             var item1Called = false;
             Func<NancyContext, Exception, Response> item1 = (ctx, ex) => { item1Called = true; return null; };
             var item2Called = false;
@@ -93,13 +103,15 @@
             subPipeline += item3;
             subPipeline += item4;
 
+            // When
             pipeline.AddItemToEndOfPipeline(subPipeline);
             pipeline.Invoke(CreateContext(), new Exception());
 
-            Assert.True(item1Called);
-            Assert.True(item2Called);
-            Assert.True(item3Called);
-            Assert.True(item4Called);
+            // Then
+            item1Called.ShouldBeTrue();
+            item2Called.ShouldBeTrue();
+            item3Called.ShouldBeTrue();
+            item4Called.ShouldBeTrue();
         }
     }
 }
