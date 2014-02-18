404eb10faddef307c2f5188a46b3fa907d29a2fd
Damian Hickey <dhickey@gmail.com>
2014-02-18 12:17:20 +0100

    The test file.

---
+++ src/Nancy.Tests.Functional/Tests/AsyncExceptionTests.cs
@@ -0,0 +1,48 @@
+﻿namespace Nancy.Tests.Functional.Tests
+{
+    using System;
+    using System.Threading.Tasks;
+
+    using Nancy.Testing;
+
+    using Xunit;
+
+    public class AsyncExceptionTests
+    {
+        private readonly Browser browser;
+
+        public AsyncExceptionTests()
+        {
+            this.browser = new Browser(config =>
+            {
+                config.Module<MyModule>();
+                config.StatusCodeHandlers(new Type[] {});
+            });
+        }
+
+
+        [Fact] // Works as expected
+        public void When_get_sync_then_should_throw()
+        {
+            Assert.Throws<Exception>(() => this.browser.Get("/sync"));
+        }
+
+        [Fact] // Hangs indefinitely
+        public void When_get_async_then_should_throw()
+        {
+            Assert.Throws<Exception>(() => this.browser.Get("/async"));
+        }
+    }
+
+    public class MyModule : NancyModule
+    {
+        public MyModule()
+        {
+            this.Get["/sync"] = _ => { throw new Exception("Derp"); };
+
+            this.Get["/async", true] = (_, __) =>
+                Task.Factory.StartNew(() => { })
+                    .ContinueWith<dynamic>(t => { throw new Exception("Derp"); });
+        }
+    }
+}
