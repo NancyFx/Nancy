319933dc4cbb640486e05ff77f4ceb87a86f80cc
Andreas Hakansson <andreas@selfinflicted.org>
2014-02-18 21:22:39 +0100

    Updated SharedAssemblyInfo to v0.22.2

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -6,5 +6,5 @@ using System.Reflection;
 [assembly: AssemblyCompany("Nancy")]
 [assembly: AssemblyProduct("Nancy")]
 [assembly: AssemblyCopyright("Copyright (C) Andreas Hakansson, Steven Robbins and contributors")]
-[assembly: AssemblyVersion("0.22.1")]
-[assembly: AssemblyInformationalVersion("0.22.1")]
+[assembly: AssemblyVersion("0.22.2")]
+[assembly: AssemblyInformationalVersion("0.22.2")]
