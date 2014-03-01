9ae0a5494bc335c3d940d730ae5d5f18c1018836
Jeff Doolittle <jeffdoolittle@gmail.com>
2014-02-28 18:43:18 -0800

    Token authentication and authorization implementation.

---
+++ src/Nancy.Authentication.Token/TokenAuthenticationConfiguration.cs
@@ -0,0 +1,29 @@
+﻿namespace Nancy.Authentication.Token
+{
+    using System;
+
+    /// <summary>
+    /// Configuration options for token authentication
+    /// </summary>
+    public class TokenAuthenticationConfiguration
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TokenAuthenticationConfiguration"/> class.
+        /// </summary>
+        /// <param name="tokenizer">A valid instance of <see cref="ITokenizer"/> class</param>
+        public TokenAuthenticationConfiguration(ITokenizer tokenizer)
+        {
+            if (tokenizer == null)
+            {
+                throw new ArgumentNullException("tokenizer");
+            }
+
+            this.Tokenizer = tokenizer;
+        }
+
+        /// <summary>
+        /// Gets the token validator
+        /// </summary>
+        public ITokenizer Tokenizer { get; private set; }
+    }
+}
