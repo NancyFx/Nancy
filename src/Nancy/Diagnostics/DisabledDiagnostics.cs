a8323a975ad22fc103e869f1ee8d1f9e0a3a1586
Andreas Hakansson <andreas@selfinflicted.org>
2014-03-04 12:46:43 +0100

    Added XML comments to DisabledDiagnostics

--- src/Nancy/Diagnostics/DisabledDiagnostics.cs
+++ src/Nancy/Diagnostics/DisabledDiagnostics.cs
@@ -1,9 +1,16 @@
-using Nancy.Bootstrapper;
-
 namespace Nancy.Diagnostics
 {
+    using Nancy.Bootstrapper;
+
+    /// <summary>
+    /// Implementation of the <see cref="IDiagnostics"/> interface that does nothing.
+    /// </summary>
     public class DisabledDiagnostics : IDiagnostics
     {
+        /// <summary>
+        /// Initialise diagnostics
+        /// </summary>
+        /// <param name="pipelines">Application pipelines</param>
         public void Initialize(IPipelines pipelines)
         {
             // Do nothing :-)
