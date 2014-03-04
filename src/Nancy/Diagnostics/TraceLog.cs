eb8c5c881331ce95390f8482701d3a0ce4259003
Andreas Hakansson <andreas@selfinflicted.org>
2014-03-04 09:00:04 +0100

    Added missing XML comments to ITraceLog implementations

--- src/Nancy/Diagnostics/TraceLog.cs
+++ src/Nancy/Diagnostics/TraceLog.cs
@@ -3,15 +3,25 @@ namespace Nancy.Diagnostics
     using System;
     using System.Text;
 
+    /// <summary>
+    /// Implementation of the <see cref="ITraceLog"/> interface that writes to a buffer.
+    /// </summary>
     public class TraceLog : ITraceLog
     {
         private readonly StringBuilder log;
 
+        /// <summary>
+        /// Creates a new instance of the <see cref="TraceLog"/> class.
+        /// </summary>
         public TraceLog()
         {
             this.log = new StringBuilder();
         }
 
+        /// <summary>
+        /// Write to the log
+        /// </summary>
+        /// <param name="logDelegate">Log writing delegate</param>
         public void WriteLog(Action<StringBuilder> logDelegate)
         {
             if (this.log != null)
@@ -20,6 +30,12 @@ namespace Nancy.Diagnostics
             }
         }
 
+        /// <summary>
+        /// Returns a string that represents the current object.
+        /// </summary>
+        /// <returns>
+        /// A string that represents the current object.
+        /// </returns>
         public override string ToString()
         {
             return this.log != null ? this.log.ToString() : string.Empty;
