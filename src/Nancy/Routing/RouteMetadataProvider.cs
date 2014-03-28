7236777f3382e545284902707f6c1ce403e71cf5
Kristian Hellang <kristian@hellang.com>
2014-03-28 09:56:54 +0100

    Added missing XML docs

--- src/Nancy/Routing/RouteMetadataProvider.cs
+++ src/Nancy/Routing/RouteMetadataProvider.cs
@@ -8,11 +8,21 @@ namespace Nancy.Routing
     /// <typeparam name="TMetadata">The metadata type.</typeparam>
     public abstract class RouteMetadataProvider<TMetadata> : IRouteMetadataProvider
     {
+        /// <summary>
+        /// Gets the <see cref="Type" /> of the metadata that is created by the provider.
+        /// </summary>
+        /// <value>A <see cref="Type" /> instance.</value>
         public Type MetadataType
         {
             get { return typeof(TMetadata); }
         }
 
+        /// <summary>
+        /// Gets the metadata for the provided route.
+        /// </summary>
+        /// <param name="module">The <see cref="INancyModule" /> instance that the route is declared in.</param>
+        /// <param name="routeDescription">A <see cref="RouteDescription" /> for the route.</param>
+        /// <returns>An instance of <see cref="MetadataType" />.</returns>
         public object GetMetadata(INancyModule module, RouteDescription routeDescription)
         {
             return this.GetRouteMetadata(module, routeDescription);
