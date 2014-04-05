378a8f2eba619acd861c26165710b1ed9a13ed5f
Steven Robbins <ste.robbins@gmail.com>
2014-04-05 15:47:15 +0100

    Added a MaterialisingResponse
    It executes the deferred body immediately and stores the
    results in memory to be sent on later.
    
    Wired it into the ViewProcessor to catch template execution
    errors if error traces are on.

---
+++ src/Nancy/Responses/MaterialisingResponse.cs
@@ -0,0 +1,31 @@
+﻿namespace Nancy.Responses
+{
+    using System.IO;
+
+    /// <summary>
+    /// Takes an existing response and materialises the body.
+    /// Can be used as a wrapper to force execution of the deferred body for
+    /// error checking etc.
+    /// Copies the existing response into memory, so use with caution.
+    /// </summary>
+    public class MaterialisingResponse : Response
+    {
+        private readonly byte[] oldResponseOutput;
+
+        public MaterialisingResponse(Response sourceResponse)
+        {
+            this.ContentType = sourceResponse.ContentType;
+            this.Headers = sourceResponse.Headers;
+            this.StatusCode = sourceResponse.StatusCode;
+            this.ReasonPhrase = sourceResponse.ReasonPhrase;
+
+            using (var memoryStream = new MemoryStream())
+            {
+                sourceResponse.Contents.Invoke(memoryStream);
+                this.oldResponseOutput = memoryStream.ToArray();
+            }
+
+            this.Contents = stream => stream.Write(this.oldResponseOutput, 0, this.oldResponseOutput.Length);
+        }
+    }
+}
