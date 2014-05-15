4f8b67d75f9135977d8ba03511fd5f1117daf8d7
robb <rob.beal@7digital.com>
2014-05-15 11:48:58 +0100

    Added XmlSettings (equivalent of JsonSettings)
    The XmlSettings is to allow setting default options when serializing xml. In my use-case, setting the Encoding.
    
    I've added a boolean property EncodingEnabled and defaulted it to false so as not to break any existing usages. Other than that DefaultEncoding is set to utf-8 just like JsonSettings.

--- src/Nancy/Responses/XmlResponse.cs
+++ src/Nancy/Responses/XmlResponse.cs
@@ -3,6 +3,8 @@
     using System;
     using System.IO;
 
+    using Nancy.Xml;
+
     public class XmlResponse<TModel> : Response
     {
         public XmlResponse(TModel model, string contentType, ISerializer serializer)
@@ -17,9 +19,22 @@
             this.StatusCode = HttpStatusCode.OK;
         }
 
+        private static string contentType
+        {
+            get
+            {
+                return string.Concat("application/xml", GetEncoding());
+            }
+        }
+
+        private static string GetEncoding()
+        {
+            return (!XmlSettings.EncodingEnabled ? string.Empty : string.Concat("; charset=", XmlSettings.DefaultEncoding.WebName));
+        }
+
         private static Action<Stream> GetXmlContents(TModel model, ISerializer serializer)
         {
-            return stream => serializer.Serialize("application/xml", model, stream);
+            return stream => serializer.Serialize(contentType, model, stream);
         }
     }
 }
