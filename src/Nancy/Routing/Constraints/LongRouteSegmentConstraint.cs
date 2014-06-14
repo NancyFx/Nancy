81b0959c90a5b7730effc3b613f7bd41ff914204
Phillip Haydon <phillip.haydon@gmail.com>
2014-06-14 16:31:29 +0800

    added new long constraint and made int.. an int

---
+++ src/Nancy/Routing/Constraints/LongRouteSegmentConstraint.cs
@@ -0,0 +1,20 @@
+﻿namespace Nancy.Routing.Constraints
+{
+    using System.Globalization;
+
+    /// <summary>
+    /// Constraint for <see cref="long"/> route segments.
+    /// </summary>
+    public class LongRouteSegmentConstraint : RouteSegmentConstraintBase<long>
+    {
+        public override string Name
+        {
+            get { return "long"; }
+        }
+
+        protected override bool TryMatch(string constraint, string segment, out long matchedValue)
+        {
+            return long.TryParse(segment, NumberStyles.Integer, CultureInfo.InvariantCulture, out matchedValue);
+        }
+    }
+}
