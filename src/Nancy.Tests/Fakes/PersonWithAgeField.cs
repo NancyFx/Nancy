d979de01d59b26fab97012add987db062661f999
Iker Celorrio <icelorrio@kiroljokoa.com>
2014-07-08 10:35:22 +0200

    Adding Tests to new behaviour
    Test that View/Partial works with Model Property (old behaviour
    maintained)
    Test that View/Partial works with Model Field (new behaviour)

---
+++ src/Nancy.Tests/Fakes/PersonWithAgeField.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nancy.Tests.Fakes
+{
+    public class PersonWithAgeField : Person
+    {
+        public int Age;
+    }
+}
