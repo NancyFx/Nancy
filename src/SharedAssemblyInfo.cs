568fa0b522b59b31d84006f6086e5446f5e0c9d7
Steven Robbins <ste.robbins@gmail.com>
2015-01-23 15:01:15 +0000

    Updated SharedAssemblyInfo to v1.0.0

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Reflection;
 [assembly: AssemblyCompany("Nancy")]
 [assembly: AssemblyProduct("Nancy")]
 [assembly: AssemblyCopyright("Copyright (C) Andreas Hakansson, Steven Robbins and contributors")]
-[assembly: AssemblyVersion("0.23.2")]
-[assembly: AssemblyInformationalVersion("0.23.2")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
 
 [assembly: InternalsVisibleTo("Nancy.Tests")]
