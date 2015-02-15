4c6f09e782e69c3f5d15c6b8c759e09032cb6b98
Jonathan Channon <jonathan.channon@gmail.com>
2015-02-15 16:36:49 +0000

    added Before hook to make Url https if forwarded header present

---
+++ src/Nancy/Security/SSLProxy.cs
@@ -0,0 +1,38 @@
+﻿namespace Nancy.Security
+{
+    using System;
+    using System.Linq;
+
+    using Nancy.Bootstrapper;
+
+    /// <summary>
+    /// Allows a BeforeRequest hook to change Url to HTTPS if X-Forwarded-Proto header present
+    /// </summary>
+    public class SSLProxy
+    {
+        public static void Enable(IPipelines pipelines)
+        {
+            pipelines.BeforeRequest += (ctx) =>
+            {
+                //X-Forwarded-Proto: https
+                if (ctx.Request.Headers.Keys.Any(x => x.Equals("X-Forwarded-Proto", StringComparison.OrdinalIgnoreCase)))
+                {
+                    ctx.Request.Url.Scheme = "https";
+                }
+
+                //RFC7239
+                if (ctx.Request.Headers.Keys.Any(x => x.Equals("Forwarded", StringComparison.OrdinalIgnoreCase)))
+                {
+                    var forwardedHeader = ctx.Request.Headers["Forwarded"];
+                    var protoValue = forwardedHeader.FirstOrDefault(x => x.StartsWith("proto", StringComparison.OrdinalIgnoreCase));
+                    if (protoValue != null && protoValue.Equals("proto=https", StringComparison.OrdinalIgnoreCase))
+                    {
+                        ctx.Request.Url.Scheme = "https";
+                    }
+                }
+
+                return null;
+            };
+        }
+    }
+}
