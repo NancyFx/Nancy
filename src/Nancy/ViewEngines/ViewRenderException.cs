9ab3b6742c756c8bb7968db3261d7a5efa58bb4e
Adrian D. Alvarez <adrian@adriandalvarez.com>
2015-03-04 20:26:15 -0500

    Tabs to Spaces

--- src/Nancy/ViewEngines/ViewRenderException.cs
+++ src/Nancy/ViewEngines/ViewRenderException.cs
@@ -1,27 +1,27 @@
 namespace Nancy.ViewEngines.Razor
 {
-	using System;
+    using System;
 
-	/// <summary>
-	/// An exception that indicates the view could not be rendered
-	/// </summary>
-	public class ViewRenderException : Exception
-	{
-		/// <summary>
-		/// Create an instance of <see cref="ViewRenderException"/>
-		/// </summary>
-		/// <param name="msg">A description of the rendering problem</param>
-		public ViewRenderException(string msg) : base(msg)
-		{
-		}
+    /// <summary>
+    /// An exception that indicates the view could not be rendered
+    /// </summary>
+    public class ViewRenderException : Exception
+    {
+        /// <summary>
+        /// Create an instance of <see cref="ViewRenderException"/>
+        /// </summary>
+        /// <param name="msg">A description of the rendering problem</param>
+        public ViewRenderException(string msg) : base(msg)
+        {
+        }
 
-		/// <summary>
-		/// Create an instance of <see cref="ViewRenderException"/>
-		/// </summary>
-		/// <param name="msg">A description of the rendering problem</param>
-		/// <param name="innerException">The exception that is the cause of the current exception.</param>
-		public ViewRenderException(string msg, Exception innerException) : base(msg, innerException)
-		{
-		}
-	}
+        /// <summary>
+        /// Create an instance of <see cref="ViewRenderException"/>
+        /// </summary>
+        /// <param name="msg">A description of the rendering problem</param>
+        /// <param name="innerException">The exception that is the cause of the current exception.</param>
+        public ViewRenderException(string msg, Exception innerException) : base(msg, innerException)
+        {
+        }
+    }
 }
