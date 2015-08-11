2b1a73f6cada84927ee11c3f8745668d4597d5f9
Andreas Håkansson <andreas@thecodejunkie.com>
2015-08-11 14:10:13 +0200

    Added missing XML comments and cleaned up the code

--- src/Nancy/Diagnostics/DiagnosticsConfiguration.cs
+++ src/Nancy/Diagnostics/DiagnosticsConfiguration.cs
@@ -1,15 +1,14 @@
 ﻿namespace Nancy.Diagnostics
 {
-    using Nancy.Configuration;
     using Nancy.Cryptography;
 
     /// <summary>
-    /// Settings for the diagnostics dashboard
+    /// Settings for the diagnostics dashboard.
     /// </summary>
     public class DiagnosticsConfiguration
     {
         /// <summary>
-        /// 
+        /// A default instance of the <see cref="DiagnosticsConfiguration"/> class.
         /// </summary>
         public static DiagnosticsConfiguration Default = new DiagnosticsConfiguration
         {
@@ -27,11 +26,11 @@
         /// <summary>
         /// Initializes a new instance of the <see cref="DiagnosticsConfiguration"/> class
         /// </summary>
-        /// <param name="password"></param>
-        /// <param name="path"></param>
-        /// <param name="cookieName"></param>
-        /// <param name="slidingTimeout"></param>
-        /// <param name="cryptographyConfiguration"></param>
+        /// <param name="password">Password used to secure the dashboard.</param>
+        /// <param name="path">Relative path of the dashboard.</param>
+        /// <param name="cookieName">Name of the cookie to store diagnostics information.</param>
+        /// <param name="slidingTimeout">Number of minutes that expiry of the diagnostics dashboard.</param>
+        /// <param name="cryptographyConfiguration">Cryptography config to use for securing the dashboard.</param>
         public DiagnosticsConfiguration(string password, string path, string cookieName, int slidingTimeout, CryptographyConfiguration cryptographyConfiguration)
         {
             this.Password = password ?? Default.Password;
@@ -55,8 +54,8 @@
 
         /// <summary>
         /// Gets or sets password for accessing the diagnostics screen.
-        /// This should be secure :-)
         /// </summary>
+        /// <remarks>The default value is <see langword="null" />.</remarks>
         public string Password { get; private set; }
 
         /// <summary>
@@ -76,96 +75,4 @@
             return (!path.StartsWith("/")) ? string.Concat("/", path) : path;
         }
     }
-
-    public static class DiagnosticsConfigurationExtensions
-    {
-        public static void Diagnostics(this INancyEnvironment environment, string password, string path = "/_Nancy", string cookieName = "__ncd", int slidingTimeout = 15, CryptographyConfiguration cryptographyConfiguration = null)
-        {
-            environment.AddValue(new DiagnosticsConfiguration(
-                password,
-                path,
-                cookieName,
-                slidingTimeout,
-                cryptographyConfiguration));
-        }
-    }
-
-    ///// <summary>
-    ///// Settings for the diagnostics dashboard
-    ///// </summary>
-    //public class DiagnosticsConfiguration
-    //{
-    //    private string path;
-
-    //    /// <summary>
-    //    /// Initializes a new instance of the <see cref="DiagnosticsConfiguration"/> class,
-    //    /// using the <see cref="CryptographyConfiguration.Default"/> cryptographic
-    //    /// configuration.
-    //    /// </summary>
-    //    public DiagnosticsConfiguration()
-    //        : this(CryptographyConfiguration.Default)
-    //    {
-    //    }
-
-    //    /// <summary>
-    //    /// Initializes a new instance of the <see cref="DiagnosticsConfiguration"/> class,
-    //    /// using the <paramref name="cryptographyConfiguration"/> cryptographic
-    //    /// configuration.
-    //    /// </summary>
-    //    /// <param name="cryptographyConfiguration">The <see cref="CryptographyConfiguration"/> to use with diagnostics.</param>
-    //    public DiagnosticsConfiguration(CryptographyConfiguration cryptographyConfiguration)
-    //    {
-    //        this.CookieName = "__ncd";
-    //        this.CryptographyConfiguration = cryptographyConfiguration;
-    //        this.Path = "/_Nancy";
-    //        this.SlidingTimeout = 15;
-    //    }
-
-    //    /// <summary>
-    //    /// Gets or sets the name of the cookie used by the diagnostics dashboard.
-    //    /// </summary>
-    //    /// <remarks>The default is __ncd</remarks>
-    //    public string CookieName { get; set; }
-
-    //    /// <summary>
-    //    /// Gets or sets the cryptography config to use for securing the diagnostics dashboard
-    //    /// </summary>
-    //    public CryptographyConfiguration CryptographyConfiguration { get; set; }
-
-    //    /// <summary>
-    //    /// Gets or sets password for accessing the diagnostics screen.
-    //    /// This should be secure :-)
-    //    /// </summary>
-    //    public string Password { get; set; }
-
-    //    /// <summary>
-    //    /// Gets or sets the path that the diagnostics dashboard will be accessible on.
-    //    /// </summary>
-    //    /// <remarks>The default is /_Nancy. The path should always start with a forward slash.</remarks>
-    //    public string Path
-    //    {
-    //        get { return this.path; }
-    //        set { this.path = (!value.StartsWith("/")) ? string.Concat("/", value) : value; }
-    //    }
-
-    //    /// <summary>
-    //    /// The number of minutes that expiry of the diagnostics dashboard will be extended each time it is used.
-    //    /// </summary>
-    //    /// <remarks>The default is 15 minutes.</remarks>
-    //    public int SlidingTimeout { get; set; }
-
-    //    /// <summary>
-    //    /// Gets a value indicating whether the configuration is valid
-    //    /// </summary>
-    //    public bool Valid
-    //    {
-    //        get
-    //        {
-    //            return !string.IsNullOrWhiteSpace(this.Password) &&
-    //                !string.IsNullOrWhiteSpace(this.CookieName) &&
-    //                !string.IsNullOrWhiteSpace(this.Path) &&
-    //                this.SlidingTimeout != 0;
-    //        }
-    //    }
-    //}
 }
---
+++ src/Nancy/Diagnostics/DiagnosticsConfigurationExtensions.cs
@@ -0,0 +1,30 @@
+﻿namespace Nancy.Diagnostics
+{
+    using Nancy.Configuration;
+    using Nancy.Cryptography;
+
+    /// <summary>
+    /// Contains diagnostics configurtion extensions for <see cref="INancyEnvironment"/>.
+    /// </summary>
+    public static class DiagnosticsConfigurationExtensions
+    {
+        /// <summary>
+        /// Configures diagnostics.
+        /// </summary>
+        /// <param name="environment"><see cref="INancyEnvironment"/> that should be configured.</param>
+        /// <param name="password">Password used to secure the dashboard.</param>
+        /// <param name="path">Relative path of the dashboard.</param>
+        /// <param name="cookieName">Name of the cookie to store diagnostics information.</param>
+        /// <param name="slidingTimeout">Number of minutes that expiry of the diagnostics dashboard.</param>
+        /// <param name="cryptographyConfiguration">Cryptography config to use for securing the dashboard.</param>
+        public static void Diagnostics(this INancyEnvironment environment, string password, string path = null, string cookieName = null, int slidingTimeout = 15, CryptographyConfiguration cryptographyConfiguration = null)
+        {
+            environment.AddValue(new DiagnosticsConfiguration(
+                password,
+                path,
+                cookieName,
+                slidingTimeout,
+                cryptographyConfiguration));
+        }
+    }
+}
