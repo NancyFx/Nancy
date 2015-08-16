855d0d1f3d8ed03f7fd48ad34771a666a8df51cc
Andreas Håkansson <andreas@thecodejunkie.com>
2015-08-16 14:09:21 +0200

    Added DiagnosticsDefaultConfigurationProvider

---
+++ src/Nancy/Diagnostics/DiagnosticsDefaultConfigurationProvider.cs
@@ -0,0 +1,20 @@
+﻿namespace Nancy.Diagnostics
+{
+    using Nancy.Configuration;
+
+    /// <summary>
+    /// Provides the default configuration for diagnostics.
+    /// </summary>
+    public class DiagnosticsDefaultConfigurationProvider : INancyDefaultConfigurationProvider
+    {
+        /// <summary>
+        /// Gets the default configuration instance to register in the <see cref="INancyEnvironment"/>.
+        /// </summary>
+        /// <returns>The configuration instance</returns>
+        /// <remarks>Will return <see cref="DiagnosticsConfiguration.Default"/></remarks>
+        public object GetDefaultConfiguration()
+        {
+            return DiagnosticsConfiguration.Default;
+        }
+    }
+}
