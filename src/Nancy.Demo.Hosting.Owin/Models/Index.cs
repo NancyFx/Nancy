1cd73789ed06dc60644bce03678e87b28f05d3bc
Andrew Young <andrew.young@carefusion.com>
2015-09-08 12:40:44 -0700

    Add Owin demo into sln.

--- src/Nancy.Demo.Hosting.Owin/Models/Index.cs
+++ src/Nancy.Demo.Hosting.Owin/Models/Index.cs
@@ -3,12 +3,5 @@ namespace Nancy.Demo.Hosting.Owin.Models
     public class Index
     {
         public string Name { get; set; }
-
-        public string Posted { get; set; }
-
-        public Index()
-        {
-            this.Posted = "Nothing :-(";
-        }
     }
 }
