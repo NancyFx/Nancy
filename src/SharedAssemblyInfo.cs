45860c82e4df8e2d380997ddf1d19d61400fb145
Steven Robbins <ste.robbins@gmail.com>
2015-09-25 12:18:16 +0100

    Updated SharedAssemblyInfo to v1.3.0

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -1,3 +1,4 @@
+using System.Runtime.InteropServices;
 using System.Runtime.CompilerServices;
 using System.Reflection;
 
@@ -6,8 +7,8 @@ using System.Reflection;
 [assembly: AssemblyCompany("Nancy")]
 [assembly: AssemblyProduct("Nancy")]
 [assembly: AssemblyCopyright("Copyright (C) Andreas Hakansson, Steven Robbins and contributors")]
-[assembly: AssemblyVersion("1.2.0")]
-[assembly: AssemblyInformationalVersion("1.2.0")]
+[assembly: AssemblyVersion("1.3.0")]
+[assembly: AssemblyInformationalVersion("1.3.0")]
 
 [assembly: InternalsVisibleTo("Nancy.Tests")]
 [assembly: InternalsVisibleTo("Nancy.Hosting.Self.Tests")]
