1ec15dff836e55bb65a3a27e3b84bae1d459eec9
Joseph Lennox <jlennox@gmail.com>
2015-10-03 11:57:00 -0700

    Escape ConfigurationName in razor view engine post build command. Resolves NancyFx/Nancy#2068

--- src/Nancy.ViewEngines.Razor/GetNancyRazorBuildProviderPostBuildCmd.ps1
+++ src/Nancy.ViewEngines.Razor/GetNancyRazorBuildProviderPostBuildCmd.ps1
@@ -5,7 +5,7 @@ $BuildProvidersDir = Join-Path $path "BuildProviders"
 $ViewEngineDir = Join-Path $path "lib\Net40"
 
 $NancyRazorBuildProviderPostBuildCmd = "
-if `$(ConfigurationName) == Debug (
+if `"`$(ConfigurationName)`" == Debug (
 xcopy /s /y /R `"$BuildProvidersDir\Nancy.ViewEngines.Razor.BuildProviders.dll`" `"`$(ProjectDir)bin\`"
 xcopy /s /y /R `"$ViewEngineDir\Nancy.ViewEngines.Razor.dll`" `"`$(ProjectDir)bin\`"
 )"
