b9f80da944da6012a8345b36cff1bd86fd9924ee
Andreas Håkansson <andreas@thecodejunkie.com>
2015-11-22 14:44:45 +0100

    Updated to use Lazy<bool>

--- src/Nancy/DefaultRuntimeEnvironmentInformation.cs
+++ src/Nancy/DefaultRuntimeEnvironmentInformation.cs
@@ -1,5 +1,6 @@
 namespace Nancy
 {
+    using System;
     using System.Diagnostics;
     using System.Linq;
     using Nancy.Bootstrapper;
@@ -9,7 +10,7 @@ namespace Nancy
     /// </summary>
     public class DefaultRuntimeEnvironmentInformation : IRuntimeEnvironmentInformation
     {
-        private bool? isDebug;
+        private readonly Lazy<bool> isDebug = GetDebugMode();
 
         /// <summary>
         /// Gets a value indicating if the application is running in debug mode.
@@ -17,24 +18,27 @@ namespace Nancy
         /// <returns><see langword="true"/> if the application is running in debug mode, otherwise <see langword="false"/>.</returns>
         public virtual bool IsDebug
         {
-            get { return this.isDebug ?? (bool)(this.isDebug = GetDebugMode()); }
+            get { return this.isDebug.Value; }
         }
 
-        private static bool GetDebugMode()
+        private static Lazy<bool> GetDebugMode()
         {
-            try
+            return new Lazy<bool>(() =>
             {
-                var assembliesInDebug = AppDomainAssemblyTypeScanner
-                    .TypesOf<INancyModule>(ScanMode.ExcludeNancy)
-                    .Select(x => x.Assembly.GetCustomAttributes(typeof(DebuggableAttribute), true))
-                    .Where(x => x.Length != 0);
+                try
+                {
+                    var assembliesInDebug = AppDomainAssemblyTypeScanner
+                        .TypesOf<INancyModule>(ScanMode.ExcludeNancy)
+                        .Select(x => x.Assembly.GetCustomAttributes(typeof(DebuggableAttribute), true))
+                        .Where(x => x.Length != 0);
 
-                return assembliesInDebug.Any(d => ((DebuggableAttribute)d[0]).IsJITTrackingEnabled);
-            }
-            catch
-            {
-                return false;
-            }
+                    return assembliesInDebug.Any(d => ((DebuggableAttribute)d[0]).IsJITTrackingEnabled);
+                }
+                catch
+                {
+                    return false;
+                }
+            });
         }
     }
 }
