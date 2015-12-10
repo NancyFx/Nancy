ef70351a03866a4d43c3ed62c62a623feefe9f39
Dan Barua <danbarua@gmail.com>
2015-12-10 17:22:09 +0000

    SwitchoverStream does not work when %TEMP% is full Removed problematic Path.GetTempFileName() call, uses Guid.NewGuid() for filename instead. See comments for citations.s

--- src/Nancy/IO/RequestStream.cs
+++ src/Nancy/IO/RequestStream.cs
@@ -357,9 +357,21 @@
 
         private static FileStream CreateTemporaryFileStream()
         {
-            var filePath = Path.GetTempFileName();
+            // we could use Path.GetTempFilePath() but this is problematic on Windows and more so on Mono
+            // symptoms will show when this method has been called > 65k times
+            // see docs: https://msdn.microsoft.com/en-us/library/system.io.path.gettempfilename(v=vs.110).aspx
+            // comments on Win32 implementation: https://msdn.microsoft.com/en-us/library/windows/desktop/aa364991(v=vs.85).aspx
+            // mono implementation: https://github.com/mono/mono/blob/master/mcs/class/corlib/System.IO/Path.cs#L490
 
-            return new FileStream(filePath, FileMode.Create, FileAccess.ReadWrite, FileShare.None, 8192, StaticConfiguration.AllowFileStreamUploadAsync);
+            var filePath = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString("N") + ".tmp");
+
+            return new FileStream(
+                filePath,
+                FileMode.Create,
+                FileAccess.ReadWrite,
+                FileShare.None,
+                8192,
+                StaticConfiguration.AllowFileStreamUploadAsync);         
         }
 
         private Stream CreateDefaultMemoryStream(long expectedLength)
