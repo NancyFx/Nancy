d2d0240327efe8875025c4a3c68c093ac9bfb877
Steven Robbins <ste.robbins@gmail.com>
2015-12-21 12:00:06 +0000

    Fixed build issue with claims

--- src/Nancy.ViewEngines.Razor/HtmlHelpers.cs
+++ src/Nancy.ViewEngines.Razor/HtmlHelpers.cs
@@ -3,7 +3,7 @@
     using System;
     using System.IO;
     using System.Linq;
-    using System.Security.Claims;
+    using Nancy.Security;
 
     /// <summary>
     /// Helpers to generate html content.
