b220b4df2cdb5285913dacf879d95ebbb3ca6e62
Steven Robbins <ste.robbins@gmail.com>
2015-12-21 11:54:51 +0000

    Updated version

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Reflection;
 [assembly: AssemblyProduct("Nancy")]
 [assembly: AssemblyCopyright("Copyright (C) Andreas Hakansson, Steven Robbins and contributors")]
 [assembly: AssemblyVersion("1.4.2")]
-[assembly: AssemblyInformationalVersion("1.4.2")]
+[assembly: AssemblyInformationalVersion("1.4.3")]
 
 [assembly: InternalsVisibleTo("Nancy.Tests")]
 [assembly: InternalsVisibleTo("Nancy.Hosting.Self.Tests")]
