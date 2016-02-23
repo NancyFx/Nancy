b1324b3bd7879927ee95cd8651bd487905b67574
Andreas Håkansson <andreas@thecodejunkie.com>
2016-02-23 14:39:33 +0100

    Added overload to Diagnostics which implicilty sets enabled to true

--- src/Nancy/Diagnostics/DiagnosticsConfigurationExtensions.cs
+++ src/Nancy/Diagnostics/DiagnosticsConfigurationExtensions.cs
@@ -12,6 +12,28 @@
         /// Configures diagnostics.
         /// </summary>
         /// <param name="environment"><see cref="INancyEnvironment"/> that should be configured.</param>
+        /// <param name="password">Password used to secure the dashboard.</param>
+        /// <param name="path">Relative path of the dashboard.</param>
+        /// <param name="cookieName">Name of the cookie to store diagnostics information.</param>
+        /// <param name="slidingTimeout">Number of minutes that expiry of the diagnostics dashboard.</param>
+        /// <param name="cryptographyConfiguration">Cryptography config to use for securing the dashboard.</param>
+        /// <remarks>This will implicitly enable diagnostics. If you need control, please explicitly set enabled to either <see langword="true"/> or <see langword="false"/>.</remarks>
+        public static void Diagnostics(this INancyEnvironment environment, string password, string path = null, string cookieName = null, int slidingTimeout = 15, CryptographyConfiguration cryptographyConfiguration = null)
+        {
+            Diagnostics(
+                environment,
+                enabled: true,
+                password: password,
+                path: path,
+                cookieName: cookieName,
+                slidingTimeout: slidingTimeout,
+                cryptographyConfiguration: cryptographyConfiguration);
+        }
+
+        /// <summary>
+        /// Configures diagnostics.
+        /// </summary>
+        /// <param name="environment"><see cref="INancyEnvironment"/> that should be configured.</param>
         /// <param name="enabled"><see langword="true"/> if diagnostics should be enabled, otherwise <see langword="false"/>.</param>
         /// <param name="password">Password used to secure the dashboard.</param>
         /// <param name="path">Relative path of the dashboard.</param>
