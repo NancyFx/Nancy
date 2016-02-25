99cc7504f0ae9194a2fb318861676bfbe7e09903
Jonathan Channon <jonathan.channon@gmail.com>
2016-02-25 13:28:33 +0000

    added kestrel demo project

---
+++ samples/Nancy.Demo.Hosting.Kestrel/HomeModule.cs
@@ -0,0 +1,14 @@
+﻿namespace Nancy.Demo.Hosting.Kestrel
+{
+    using System.Threading.Tasks;
+
+    public class HomeModule : NancyModule
+    {
+        public HomeModule()
+        {
+            Get["/"] = (o, token) => Task.FromResult<dynamic>("Hello from Nancy running on CoreCLR");
+
+            Get["/conneg/{name}"] = (parameters, token) => Task.FromResult<dynamic>(new Person() { Name = parameters.name });
+        }
+    }
+}
---
+++ samples/Nancy.Demo.Hosting.Kestrel/Startup.cs
@@ -0,0 +1,16 @@
+﻿namespace Nancy.Demo.Hosting.Kestrel
+{
+    using Microsoft.AspNet.Builder;
+    using Microsoft.AspNet.Hosting;
+    using Nancy.Owin;
+
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseOwin(x => x.UseNancy());
+        }
+
+        public static void Main(string[] args) => WebApplication.Run<Startup>(args);
+    }
+}
