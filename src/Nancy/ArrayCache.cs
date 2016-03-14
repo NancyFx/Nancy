750f1c724f8e843b6ced81e0419753ef2297eb97
Kristian Hellang <kristian@hellang.com>
2016-03-14 18:21:13 +0100

    Added ArrayCache

---
+++ src/Nancy/ArrayCache.cs
@@ -0,0 +1,22 @@
+﻿namespace Nancy
+{
+    /// <summary>
+    /// A cache for empty arrays.
+    /// </summary>
+    public class ArrayCache
+    {
+        /// <summary>
+        /// Gets a cached, empty array of the specified type.
+        /// </summary>
+        /// <typeparam name="T">the type of array to get.</typeparam>
+        public static T[] Empty<T>()
+        {
+            return EmptyArray<T>.Value;
+        }
+
+        private static class EmptyArray<T>
+        {
+            public static readonly T[] Value = new T[0];
+        }
+    }
+}
