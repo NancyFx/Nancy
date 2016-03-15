145a2863ee329b449ed93852c2947aa49b0d21b3
Andreas Håkansson <andreas@thecodejunkie.com>
2016-03-15 11:56:51 +0100

    Optimization of field name convertion

--- src/Nancy/ModelBinding/DefaultFieldNameConverter.cs
+++ src/Nancy/ModelBinding/DefaultFieldNameConverter.cs
@@ -1,11 +1,23 @@
 namespace Nancy.ModelBinding
 {
+    using System.Collections.Concurrent;
+
     /// <summary>
     /// Default field name converter
     /// Converts camel case to pascal case
     /// </summary>
     public class DefaultFieldNameConverter : IFieldNameConverter
     {
+        private readonly ConcurrentDictionary<string, string> cache;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DefaultFieldNameConverter"/> class.
+        /// </summary>
+        public DefaultFieldNameConverter()
+        {
+            this.cache = new ConcurrentDictionary<string, string>();
+        }
+
         /// <summary>
         /// Converts a field name to a property name
         /// </summary>
@@ -18,15 +30,15 @@ namespace Nancy.ModelBinding
                 return fieldName;
             }
 
-            var first = fieldName.Substring(0, 1);
-            var capitalFirst = first.ToUpperInvariant();
-
-            if (first.Equals(capitalFirst))
+            return this.cache.GetOrAdd(fieldName, name =>
             {
-                return fieldName;
-            }
+                if (name.Length > 1)
+                {
+                    return char.ToUpperInvariant(name[0]) + name.Substring(1);
+                }
 
-            return string.Format("{0}{1}", capitalFirst, fieldName.Substring(1));
+                return name.ToUpperInvariant();
+            });
         }
     }
 }
