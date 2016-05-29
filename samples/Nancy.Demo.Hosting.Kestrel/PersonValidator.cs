6f74b42440552b867bfcf94708c27e63874df264
Jonathan Channon <jonathan.channon@gmail.com>
2016-05-29 09:24:32 +0100

    using FV alpha package to get Nancy.FluentValidation working

---
+++ samples/Nancy.Demo.Hosting.Kestrel/PersonValidator.cs
@@ -0,0 +1,12 @@
+﻿namespace Nancy.Demo.Hosting.Kestrel
+{
+    using FluentValidation;
+    
+    public class PersonValidator : AbstractValidator<Person>
+    {
+        public PersonValidator()
+        {
+            this.RuleFor(x => x.Name).NotEmpty();
+        }
+    }
+}
