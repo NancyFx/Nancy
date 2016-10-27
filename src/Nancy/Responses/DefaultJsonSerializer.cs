4ef98996619ecbb41d934efdaf8e154a809c7486
Kristian Hellang <kristian@hellang.com>
2016-10-27 12:43:14 +0200

    Made Json class public, reuse utility to check media type

--- src/Nancy/Responses/DefaultJsonSerializer.cs
+++ src/Nancy/Responses/DefaultJsonSerializer.cs
@@ -36,7 +36,7 @@
         /// <returns>True if supported, false otherwise</returns>
         public bool CanSerialize(MediaRange mediaRange)
         {
-            return IsJsonType(mediaRange);
+            return Json.IsJsonContentType(mediaRange);
         }
 
         /// <summary>
@@ -76,30 +76,6 @@
                     }
                 }
             }
-        }
-
-        /// <summary>
-        /// Attempts to detect if the content type is JSON.
-        /// Supports:
-        ///   application/json
-        ///   text/json
-        ///   application/vnd[something]+json
-        /// Matches are case insentitive to try and be as "accepting" as possible.
-        /// </summary>
-        /// <param name="contentType">Request content type</param>
-        /// <returns>True if content type is JSON, false otherwise</returns>
-        private static bool IsJsonType(string contentType)
-        {
-            if (string.IsNullOrEmpty(contentType))
-            {
-                return false;
-            }
-
-            var contentMimeType = contentType.Split(';')[0];
-
-            return contentMimeType.StartsWith("application/json", StringComparison.OrdinalIgnoreCase)
-                || contentMimeType.Equals("text/json", StringComparison.OrdinalIgnoreCase)
-                || contentMimeType.EndsWith("+json", StringComparison.OrdinalIgnoreCase);
         }
     }
 }
