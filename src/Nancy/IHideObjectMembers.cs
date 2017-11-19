27c58221f3c6e078c9695b5b87c7e48cc519ef9b
Andreas Hakansson <andreas@selfinflicted.org>
2017-11-19 15:51:04 +0100

    Updated the XML comments for IHideObjectMembers
    Removed blog link as it is likely to either change or become stale over
    time.
    
    Fixed #2571

--- src/Nancy/IHideObjectMembers.cs
+++ src/Nancy/IHideObjectMembers.cs
@@ -7,7 +7,6 @@ namespace Nancy
     /// Helper interface used to hide the base <see cref="object"/>  members from the fluent API to make it much cleaner
     /// in Visual Studio intellisense.
     /// </summary>
-    /// <remarks>Created by Daniel Cazzulino http://www.clariusconsulting.net/blogs/kzu/archive/2008/03/10/58301.aspx</remarks>
     [EditorBrowsable(EditorBrowsableState.Never)]
     public interface IHideObjectMembers
     {
